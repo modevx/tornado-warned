9c856ae27c37592ace2dabab821b34f1d983ab32
Ephraim <admin@ephraimsmith.dev>
2022-01-21 07:25:47 -0600

    define init 3rd party API adapters

--- adapters/national-weather-service/index.ts
+++ adapters/national-weather-service/index.ts
@@ -1,4 +1,43 @@
-import { getNWS } from "../xhr";
+import Axios from "axios";
 
-export const getSevereThunderStormWarningAlerts = () =>
-	getNWS("/alerts/active?event=Severe Thunderstorm Warning");
+const returnAxiosInstance = () =>
+	// instance.method default: "GET"
+	Axios.create({ baseURL: "https://api.weather.gov" });
+
+const getAlertsEndpoint = (url?: string, configs?: any) => {
+	const axios = returnAxiosInstance();
+	return axios.get(url ? `alerts/${url}` : "alerts", configs);
+};
+
+export const getAlerts = () => {
+	return getAlertsEndpoint();
+};
+
+export const getAlertTypes = () => {
+	return getAlertsEndpoint("/types");
+};
+
+export const getAlertById = (id: string) => {
+	return getAlertsEndpoint(id);
+};
+
+export const getActiveAlerts = () => {
+	return getAlertsEndpoint("/active");
+};
+
+export const getActiveAlertsCount = () => {
+	return getAlertsEndpoint("/active/count");
+};
+
+// const getNWSAlertsByEvent = (eventType: string) => {
+// 	const axiosEventAlerts = getNWSAlerts();
+// 	return axiosEventAlerts.get({ url: eventType });
+// };
+
+// export const getActiveAlertsCountByEvent = (event: string) => getNWS();
+
+// export const getSevereThunderstormWarnings = () =>
+// 	getNWSAlertsByEvent("Severe Thunderstorm Warning");
+
+// export const getTornadoWatches = () => getNWSAlertsByEvent("Tornado Watch");
+// export const getTornadoWarnings = () => getNWSAlertsByEvent("Tornado Warning");
--- adapters/severe-weather-data-inventory/index.ts
+++ adapters/severe-weather-data-inventory/index.ts
@@ -1,5 +1,19 @@
-import { getSWDI } from "../xhr";
+// SEVERE WEATHER DATABASE
+// baseURL: https://www.ncdc.noaa.gov/swdiws/{outputFormat}/{dataset}/{dateRange}
+import Axios from "axios";
 
-export const getTornadoVortexSignaturesInRange = (dateRange: string) => {
-	getSWDI(`/json/nx3tvs/${dateRange}`);
+const returnSWDIAxiosInstance = () =>
+	Axios.create({ method: "get", baseURL: "https://www.ncdc.noaa.gov/swdiws" });
+
+export const getSWDI = (url: string) => {
+	const axios = returnSWDIAxiosInstance();
+	return axios.get(url);
 };
+
+const getSWDIasJSON = (dateRange: string) => {
+	return getSWDI(`/json/nx3tvs/${dateRange}`);
+};
+
+// export const getTornadoVortexSignatures = () => {
+// 	return getSWDITornadoVortexSignaturesInRange();
+// };
