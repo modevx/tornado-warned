591711768b5e92ab343c3ea46e5baa7fa3e6954a
Ephraim <admin@ephraimsmith.dev>
2022-02-22 12:30:58 -0600

    layout warning & watch cards on HomeScreen

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -11,15 +13,15 @@ import axios from "axios";
 const Home = () => {
 	const [alerts, setAlerts] = useState([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-			console.log(res.data);
-			setAlerts(res.data);
-		};
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+	// 		console.log(res.data);
+	// 		setAlerts(res.data);
+	// 	};
 
-		fetchData();
-	}, []);
+	// 	fetchData();
+	// }, []);
 
 	return (
 		<div
@@ -31,7 +33,36 @@ const Home = () => {
 				<Row className='h-100'>
 					<Col className='d-flex flex-column'>
 						<Navbar />
-						<main className='h-100'></main>
+						<Container className='h-100 d-flex align-items-center justify-content-evenly '>
+							<Row className='w-100'>
+								<Col className='d-flex justify-content-center'>
+									<Card style={{ width: "18rem" }}>
+										<Card.Img variant='top' src='holder.js/100px180' />
+										<Card.Body className='bg-dark'>
+											<Card.Title>Active Tornado Warnings</Card.Title>
+											<Card.Text>
+												Tornadoes verified by storm spotters or rotation
+												indicated on radar.
+											</Card.Text>
+											<Button variant='danger'>GO TO WARNINGS</Button>
+										</Card.Body>
+									</Card>
+								</Col>
+								<Col className='d-flex justify-content-center'>
+									<Card style={{ width: "18rem" }}>
+										<Card.Img variant='top' src='holder.js/100px180' />
+										<Card.Body className='bg-dark'>
+											<Card.Title>Active Tornado Watches</Card.Title>
+											<Card.Text>
+												Conditions are right for producing destructive weather
+												and tornadoes.
+											</Card.Text>
+											<Button variant='warning'>GO TO WATCHES</Button>
+										</Card.Body>
+									</Card>
+								</Col>
+							</Row>
+						</Container>
 						<Footer />
 					</Col>
 				</Row>
