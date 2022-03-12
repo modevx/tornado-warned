08cc0295461e74354e5ecab058fdce631e0250c5
Ephraim <admin@ephraimsmith.dev>
2022-03-11 18:18:28 -0600

    86 unused components

--- components/AlertIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { BsTornado } from "react-icons/bs";
-
-const AlertIcon = () => {
-	return <div>AlertIcon</div>;
-};
-
-export default AlertIcon;
--- components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type CARD = { children: any; bgColorClass: string };
-
-export const Card = ({ children, bgColorClass }: CARD) => {
-  return (
-    <div className={`w-1/4 from-${bgColorClass} to-white`}>{children}</div>
-  );
-};
