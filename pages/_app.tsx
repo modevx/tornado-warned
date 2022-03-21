fa30cbfae37e2c4eaf0de05e385bd94d22239378
Ephraim <admin@ephraimsmith.dev>
2022-03-21 05:29:59 -0500

    install reactprime deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
 import "../styles/globals.scss";
+import "primereact/resources/themes/saga-blue/theme.css"; // theme
+import "primereact/resources/primereact.min.css"; // core css
+import "primeicons/primeicons.css"; // icons
+import "primeflex/primeflex.css"; // css util classes
+
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+	const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Component {...pageProps} />
+		</QueryClientProvider>
+	);
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,52 @@
 import { AlertList, PageWrapper } from "../components";
 import {
-  useActiveAlerts,
-  useTestAlerts,
+	useActiveAlerts,
+	useTestAlerts,
 } from "../services/NationalWeatherService";
 
 const HomeScreen = () => {
-  const { isLoading, error, data } = useActiveAlerts();
-  // const { isLoading, error, data } = useTestAlerts();
-  const tornadoWarnings = [];
-  const tornadoWatches = [];
-  const stormWarnings = [];
-  const stormWatches = [];
+	const { isLoading, error, data } = useActiveAlerts();
+	// const { isLoading, error, data } = useTestAlerts();
+	const tornadoWarnings = [];
+	const tornadoWatches = [];
+	const stormWarnings = [];
+	const stormWatches = [];
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>ERROR: {error.message}</p>;
+	if (isLoading) return <p>Loading...</p>;
+	if (error) return <p>ERROR: {error.message}</p>;
 
-  if (data) {
-    data.forEach((alert) => {
-      if (alert.event === "Tornado Warning") tornadoWarnings.push(alert);
-      if (alert.event === "Tornado Watch") tornadoWatches.push(alert);
-      if (alert.event === "Severe Thunderstorm Warning")
-        stormWarnings.push(alert);
-      if (alert.event === "Severe Thunderstorm Watch") stormWatches.push(alert);
-    });
-  }
+	if (data) {
+		data.forEach(alert => {
+			if (alert.event === "Tornado Warning") tornadoWarnings.push(alert);
+			if (alert.event === "Tornado Watch") tornadoWatches.push(alert);
+			if (alert.event === "Severe Thunderstorm Warning")
+				stormWarnings.push(alert);
+			if (alert.event === "Severe Thunderstorm Watch") stormWatches.push(alert);
+		});
+	}
 
-  return (
-    <PageWrapper>
-      <div className="flex flex-col justify-around w-full">
-        <div>
-          {data ? (
-            <>
-              <h2 className="text-4xl">TORNADO WARNINGS</h2>
-              <AlertList alerts={tornadoWarnings} />
-              <h2 className="text-4xl">TORNADO WATCHES</h2>
-              <AlertList alerts={tornadoWatches} />
-              <h2 className="text-4xl">SEVERE THUNDERSTORM WARNINGS</h2>
+	return (
+		<PageWrapper>
+			<div className='flex flex-col justify-around w-full'>
+				<div>
+					{data ? (
+						<>
+							<h2 className='text-4xl'>TORNADO WARNINGS</h2>
+							<AlertList alerts={tornadoWarnings} />
+							<h2 className='text-4xl'>TORNADO WATCHES</h2>
+							<AlertList alerts={tornadoWatches} />
+							{/* <h2 className="text-4xl">SEVERE THUNDERSTORM WARNINGS</h2>
               <AlertList alerts={stormWarnings} />
               <h2 className="text-4xl">SEVERE THUNDERSTORM WATCHES</h2>
-              <AlertList alerts={stormWatches} />
-            </>
-          ) : (
-            <p>No ALERT to report...</p>
-          )}
-        </div>
-      </div>
-    </PageWrapper>
-  );
+              <AlertList alerts={stormWatches} /> */}
+						</>
+					) : (
+						<p>No ALERT to report...</p>
+					)}
+				</div>
+			</div>
+		</PageWrapper>
+	);
 };
 
 export default HomeScreen;
