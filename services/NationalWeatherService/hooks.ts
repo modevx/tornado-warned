ac3a23dee788cf52b0bca92149dde3e5d3a775e2
Ephraim <admin@ephraimsmith.dev>
2022-03-21 06:18:41 -0500

    import fetchAlerts

--- services/NationalWeatherService/hooks.ts
+++ services/NationalWeatherService/hooks.ts
@@ -1,10 +1,18 @@
 import { useQuery } from "react-query";
-import { fetchActiveAlerts, fetchTestAlerts } from "./requests";
+import { fetchAlerts, fetchActiveAlerts, fetchTestAlerts } from "./requests";
 
 export const useActiveAlerts = () => {
-  return useQuery(["activeAlerts"], () => fetchActiveAlerts());
+	return useQuery(["activeAlerts"], () => fetchActiveAlerts());
 };
-
+export const useTornadoWarnings = () => {
+	return useQuery(["tornadoWarnings"], () => fetchAlerts());
+};
+export const useTornadoWatches = () => {
+	return useQuery(["tornadoWatches"], () => fetchAlerts());
+};
+export const useCancelledAlerts = () => {
+	return useQuery(["cancelledAlerts"], () => fetchAlerts());
+};
 export const useTestAlerts = () => {
-  return useQuery(["testAlerts"], () => fetchTestAlerts());
+	return useQuery(["testAlerts"], () => fetchTestAlerts());
 };
