7f54f84c99b69c0f2f60e2803e97a7d1da3af943
Ephraim <admin@ephraimsmith.dev>
2022-03-21 06:19:10 -0500

    add alert-specific request hooks

--- services/NationalWeatherService/requests.ts
+++ services/NationalWeatherService/requests.ts
@@ -1,33 +1,35 @@
 import { AXIOS_CONFIG } from "./config";
-import { NWS_API_PATHS } from "./paths";
+import { NWS_ENDPOINTS } from "./paths";
+
+export const fetchAlerts = async () => {};
 
 export const fetchActiveAlerts = async () => {
-  const raw = await AXIOS_CONFIG.get(NWS_API_PATHS.activeAlerts);
-  const features = await raw.data.features;
-  return await features.map((alert) => {
-    const { areaDesc, event, status, messageType, headline, instruction } =
-      alert.properties;
-    return { event, status, messageType, areaDesc, headline, instruction };
-  });
+	const raw = await AXIOS_CONFIG.get(NWS_ENDPOINTS.tornadoWarnings);
+	const features = await raw.data.features;
+	return await features.map(alert => {
+		const { areaDesc, event, status, messageType, headline, instruction } =
+			alert.properties;
+		return { event, status, messageType, areaDesc, headline, instruction };
+	});
 };
 
 export const fetchTestAlerts = async () => {
-  const raw = await AXIOS_CONFIG.get(NWS_API_PATHS.testAlerts);
-  const features = await raw.data.features;
-  return await features.map((alert) => {
-    const { areaDesc, event, status, messageType, headline, instruction } =
-      alert.properties;
-    return { event, status, messageType, areaDesc, headline, instruction };
-  });
+	const raw = await AXIOS_CONFIG.get(NWS_ENDPOINTS.testAlerts);
+	const features = await raw.data.features;
+	return await features.map(alert => {
+		const { areaDesc, event, status, messageType, headline, instruction } =
+			alert.properties;
+		return { event, status, messageType, areaDesc, headline, instruction };
+	});
 
-  // return features.map((alert) => {
-  //   const { areaDesc, event } = alert;
+	// return features.map((alert) => {
+	//   const { areaDesc, event } = alert;
 
-  //   return {
-  //     areaDesc,
-  //     event,
-  //   };
-  // });
+	//   return {
+	//     areaDesc,
+	//     event,
+	//   };
+	// });
 };
 
 // {
