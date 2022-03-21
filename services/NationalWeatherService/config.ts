e75e8893e36183822dc0e41b7b5e1b3326a832b7
Ephraim <admin@ephraimsmith.dev>
2022-03-21 06:18:21 -0500

    update endpoints path

--- services/NationalWeatherService/config.ts
+++ services/NationalWeatherService/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { NWS_API_PATHS } from "./paths";
+import { NWS_ENDPOINTS } from "./paths";
 
 export const AXIOS_CONFIG = axios.create({
-  baseURL: NWS_API_PATHS.baseUrl,
+	baseURL: NWS_ENDPOINTS.nwsBaseUrl,
 });
