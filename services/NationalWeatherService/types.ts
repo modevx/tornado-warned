9342c54afb9d734a6f924cb9d5b20b347eca48e5
Ephraim <admin@ephraimsmith.dev>
2022-03-21 06:17:39 -0500

    add hook response-based types

--- services/NationalWeatherService/types.ts
+++ services/NationalWeatherService/types.ts
@@ -1,8 +1,12 @@
-export interface ALERT {
-  areaDesc: string;
-  sent: string;
-  expires: string;
-  messageType: string;
-  event: string;
-  senderName: string;
+export interface TORNADO_WARNING {
+	areaDesc: string;
+	sent: string;
+	expires: string;
+	messageType: string;
+	event: string;
+	senderName: string;
 }
+
+export interface TORNADO_WATCH {}
+
+export interface CANCELLED_ALERT {}
