e3a9c3e1aa5881f85dedecb63803094734644383
Ephraim <admin@ephraimsmith.dev>
2022-03-24 05:58:00 -0500

    add hero & features components

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,16 @@ import { QUERY_PARAMS as NWS_PARAMS } from "../services/NationalWeatherService";
 import TornadoWarningWindow from "../components/TornadoWarningWindow";
 import TornadoWatchesWindow from "../components/TornadoWatchesWindow";
 import CancelledAlertsWindow from "../components/CancelledAlertsWindow";
+import Hero from "../components/Hero";
+import FeaturesList from "../components/FeaturesList";
 
 const HomeScreen = () => {
 	return (
 		<PageWrapper>
 			<div className='flex flex-col justify-around w-full'>
 				<div>
+					<Hero />
+					<FeaturesList />
 					<TornadoWarningWindow />
 					<TornadoWatchesWindow />
 					<CancelledAlertsWindow />
