f2017132d9da60b8b7820820ee30c1517a7f95c5
Ephraim <admin@ephraimsmith.dev>
2022-04-03 19:41:50 -0500

    86 TS

--- pages/index.js
+++ pages/index.js
@@ -8,86 +8,103 @@ dayjs.extend(localized);
 // -----
 import { Hero, Features, Header, PageWrapper } from "../components";
 import {
-	useTornadoWarnings,
-	useTornadoWatches,
-	useCancelledAlerts,
+  useTornadoWarnings,
+  useTornadoWatches,
+  useCancelledAlerts,
 } from "../hooks";
 // -----
+import axios from "axios";
 
 const SiteFeaturesSection = () => {};
 
 const AlertList = ({ alertArray }) => {
-	if (!alertArray) return null;
+  if (!alertArray) return null;
 
-	return (
-		<div>
-			{alertArray.map(alert => (
-				<AlertItem alert={alert} key={alert.id} />
-			))}
-		</div>
-	);
+  return (
+    <div>
+      {alertArray.map((alert) => (
+        <AlertItem alert={alert} key={alert.id} />
+      ))}
+    </div>
+  );
 };
 
 const AlertItem = ({ alert }) => {
-	const { event, messageType, effective, expires, areaDesc } = alert.properties;
+  const { event, messageType, effective, expires, areaDesc } = alert.properties;
 
-	const EVENT_COLOR_MAP = {
-		TornadoWarning: "from-red-500",
-		TornadoWatch: "from-yellow-500",
-	};
+  const EVENT_COLOR_MAP = {
+    TornadoWarning: "from-red-500",
+    TornadoWatch: "from-yellow-500",
+  };
 
-	const color =
-		messageType.toLowerCase() === "cancel"
-			? "from-neutral-500"
-			: EVENT_COLOR_MAP[`${event.split(" ").join("")}`];
+  const color =
+    messageType.toLowerCase() === "cancel"
+      ? "from-neutral-500"
+      : EVENT_COLOR_MAP[`${event.split(" ").join("")}`];
 
-	return (
-		// -- [ CARD ]
-		<div
-			className={`bg-gradient-to-r ${color} to-neutral-900 my-3 p-2 text-xs`}
-		>
-			{/* -- [ HEADLINE ] */}
-			<div className='flex items-center justify-between py-2'>
-				{/* -- [ ALERT type ] + [ EFFECTIVE time ] */}
-				<div className='flex items-center'>
-					<BsTornado className='mr-1' size={30} />
+  return (
+    // -- [ CARD ]
+    <div
+      className={`bg-gradient-to-r ${color} to-neutral-900 my-3 p-2 text-xs`}
+    >
+      {/* -- [ HEADLINE ] */}
+      <div className="flex items-center justify-between py-2">
+        {/* -- [ ALERT type ] + [ EFFECTIVE time ] */}
+        <div className="flex items-center">
+          <BsTornado className="mr-1" size={30} />
 
-					<span className='font-bold inline-block  text-lg text-white uppercase'>
-						{event.split(" ")[1]}
-					</span>
-				</div>
+          <span className="font-bold inline-block  text-lg text-white uppercase">
+            {event.split(" ")[1]}
+          </span>
+        </div>
 
-				{/* -- [ MESSAGE type ] + [ EXPIRATION time ] */}
-				<div>
-					<span>{dayjs(effective).format("LT")}</span>&nbsp; - &nbsp;
-					<span>{dayjs(expires).format("LT")}</span>
-				</div>
-			</div>
-			{/* -- Impacted Areas Box */}
-			<div className='bg-neutral-700 px-2 py-3'>
-				<p>{areaDesc}</p>
-			</div>
-		</div>
-	);
+        {/* -- [ MESSAGE type ] + [ EXPIRATION time ] */}
+        <div>
+          <span>{dayjs(effective).format("LT")}</span>&nbsp; - &nbsp;
+          <span>{dayjs(expires).format("LT")}</span>
+        </div>
+      </div>
+      {/* -- Impacted Areas Box */}
+      <div className="bg-neutral-700 px-2 py-3">
+        <p>{areaDesc}</p>
+      </div>
+    </div>
+  );
 };
 
 const HomeScreen = () => {
-	const { data: warnings } = useTornadoWarnings();
-	const { data: watches } = useTornadoWatches();
-	const { data: cancels } = useCancelledAlerts();
+  const { data: warnings } = useTornadoWarnings();
+  const { data: watches } = useTornadoWatches();
+  const { data: cancels } = useCancelledAlerts();
 
-	return (
-		<PageWrapper>
-			<div className='flex flex-col justify-around w-full'>
-				<Header />
-				{/* <Hero />
-				<Features /> */}
-				<AlertList alertArray={warnings} color='red' />
-				<AlertList alertArray={watches} color='neutral' />
-				<AlertList alertArray={cancels} color='yellow' />
-			</div>
-		</PageWrapper>
-	);
+  const getFakeData = async () => {
+    const res = await axios
+      .get("../fake_data/test_alerts.json")
+      .then((res) => res.data.features)
+      .catch((err) => console.log(err));
+  };
+
+  const { features } = getFakeData();
+  const [fakeStormWarnings, fakeTornadoWatches, fakeTornadoWarnings] = [
+    features,
+  ];
+
+  if (warnings) {
+    console.log(warnings);
+  }
+
+  return (
+    <PageWrapper>
+      <div className="flex flex-col justify-around w-full">
+        <Header />
+        <Hero />
+        <Features />
+        <AlertList alertArray={warnings} color="red" />
+        <AlertList alertArray={watches} color="neutral" />
+        <AlertList alertArray={cancels} color="yellow" />
+      </div>
+    </PageWrapper>
+  );
 };
 
 export default HomeScreen;
