3d1c41ed27a32a9022f6ce91531b45f5c2089442
Ephraim <admin@ephraimsmith.dev>
2022-04-08 16:34:58 -0500

    refactor Alert value check

--- components/Alerts/AlertsSection.jsx
+++ components/Alerts/AlertsSection.jsx
@@ -2,15 +2,14 @@ import { AlertList } from ".";
 import { BsTornado } from "react-icons/bs";
 
 export const AlertsSection = ({ alerts }) => {
-	// console.log("AlertsSection >>\n", alerts[0]);
+	// if (alerts) {
+	// 	const { isLoading, error, data } = alerts;
+	// 	console.log("AlertsSection >>\n", isLoading, error, alerts);
+	// }
 
 	return (
 		<section>
-			{alerts === null || alerts === undefined || alerts.length < 1 ? (
-				<p className='mt-4 text-xl text-green-400'>All clear.</p>
-			) : (
-				<AlertList alertArray={alerts} />
-			)}
+			<AlertList alertArray={alerts} />
 		</section>
 	);
 };
