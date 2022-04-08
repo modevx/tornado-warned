99a221d3a7f42a80ab9bd9bbdbe9606452630df2
Ephraim <admin@ephraimsmith.dev>
2022-04-08 16:34:37 -0500

    reformat Alert components

--- components/Alerts/AlertList.jsx
+++ components/Alerts/AlertList.jsx
@@ -7,7 +7,7 @@ export const AlertList = ({ alertArray }) => {
 
 	return (
 		// <ul className='p-4 flex flex-col flex-wrap gap-4 mx-auto'>
-		<ul className='p-4 sm:grid sm:grid-cols-2 gap-6'>
+		<ul className='p-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-6'>
 			{alertArray.map(alert => (
 				<AlertListItem alert={alert} key={alert.id} />
 			))}
--- components/Alerts/Alerts.jsx
+++ components/Alerts/Alerts.jsx
@@ -13,10 +13,24 @@ export const Alerts = () => {
 	// const { data: tornado_watches } = useTornadoWatches();
 	const { tornado_warnings, tornado_watches } = FAKE_ALERTS;
 
+	const warnings =
+		tornado_warnings.length > 0 ? (
+			<AlertsSection alerts={tornado_warnings} />
+		) : (
+			<p className='mt-4 text-xl text-green-400'>No Active Tornado Warnings.</p>
+		);
+
+	const watches =
+		tornado_watches.length > 0 ? (
+			<AlertsSection alerts={tornado_watches} />
+		) : (
+			<p className='mt-4 text-xl text-green-400'>No Active Tornado Watches.</p>
+		);
+
 	return (
 		<>
-			<AlertsSection alerts={tornado_warnings} />
-			<AlertsSection alerts={tornado_watches} />
+			{warnings}
+			{watches}
 		</>
 	);
 };
