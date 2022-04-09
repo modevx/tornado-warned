87855ab3c65ebfd1fb65808ab797e4d85ef47a02
Ephraim <admin@ephraimsmith.dev>
2022-04-09 08:21:09 -0500

    create func to list alert areas by state

---
+++ components/Alerts/utils/getAreaDescStringByState.js
@@ -0,0 +1,35 @@
+import { getUniqueStatesSet } from "./getUniqueStatesSet";
+
+export const getAffectedAreasStringByState = alertAreaDescSTR => {
+	if (alertAreaDescSTR === null || alertAreaDescSTR === undefined) return;
+	// --------------------------------------------------
+	// unique state abbreviations Set
+	// --------------------------------------------------
+	const areaDescSET = getUniqueStatesSet(alertAreaDescSTR);
+	// --------------------------------------------------
+
+	const areaDescARR = alertAreaDescSTR.split(/(?:;\s)/gm);
+	const areaDescMAP = new Map();
+
+	areaDescARR.forEach(affectedArea => {
+		const [alertArea, state] = affectedArea.split(/\,\s/);
+
+		if (areaDescMAP.get(state) === undefined) {
+			areaDescMAP.set(state, new Array(alertArea));
+		} else {
+			areaDescMAP.set(state, [...areaDescMAP.get(state), alertArea]);
+		}
+	});
+
+	// *********************************
+	// __TESTING__
+	// *********************************
+
+	console.log("///////////////////////////////////////////");
+	for (const state of areaDescMAP.keys()) {
+		console.log(state);
+		console.log(areaDescMAP.get(state));
+	}
+	console.log("///////////////////////////////////////////");
+	return areaDescARR;
+};
