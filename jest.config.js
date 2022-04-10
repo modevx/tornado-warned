45be3f24556782625ff5fd0df6fd3e620e635a18
Ephraim <admin@ephraimsmith.dev>
2022-04-10 15:13:38 -0500

    add configs to use custom matchers

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,11 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
 	// Add more setup options before each test is run
-	// setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	// import custom matchers from @testing-library/jest-dom
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-	moduleDirectories: ["node_modules", "<rootDir>/"],
-	testEnvironment: "jest-environment-jsdom",
+	// moduleDirectories: ["node_modules", "<rootDir>/"],
+	// testEnvironment: "jest-environment-jsdom",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
