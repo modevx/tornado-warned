43ec172bff94cf950e9d96066e009631adc787fd
Ephraim <admin@ephraimsmith.dev>
2022-04-11 14:55:01 -0500

    add alerts page link

--- components/Header/Navbar.jsx
+++ components/Header/Navbar.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export const NavBar = () => {
 	return (
-		<nav className='p-4 flex justify-between'>
-			<span className='text-lg font-bold'>
-				TORNADO <span className='text-red-500'>ACTION</span>
-			</span>
+		<nav data-testid='navbar' className='p-4 flex justify-between'>
+			<ul>
+				<Link href='/alerts'>
+					<a>alerts</a>
+				</Link>
+			</ul>
 		</nav>
 	);
 };
