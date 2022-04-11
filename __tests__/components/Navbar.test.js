8477a7d609a0585d7475ddef00ef89cb2d51e267
Ephraim <admin@ephraimsmith.dev>
2022-04-11 14:54:30 -0500

    add init Navbar tests

---
+++ __tests__/components/Navbar.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { NavBar } from "../../components/Header";
+
+describe("Navbar Tests", () => {
+	test("displays <Navbar/>", () => {});
+
+	test("contains ALERTS page link", () => {});
+});
