90740e38c1345d8f52c7061d0f9aa4f087ab62b4
Ephraim <admin@ephraimsmith.dev>
2022-04-11 08:12:56 -0500

    move alert hooks to Home comp for jest dep inj

--- components/Alerts/Alerts.jsx
+++ components/Alerts/Alerts.jsx
@@ -1,36 +1,25 @@
 import React from "react";
 import { AlertsSection } from "./AlertsSection";
-import {
-	useTornadoWarnings,
-	useTornadoWatches,
-	useTestAlerts,
-} from "../../hooks/nws-alerts";
-import FAKE_ALERTS from "../../constants/fake-alerts";
 
-export const Alerts = () => {
-	// console.log("Alerts >>\n", tornado_warnings);
-	// const { data: tornado_warnings } = useTornadoWarnings();
-	// const { data: tornado_watches } = useTornadoWatches();
-	const { tornado_warnings, tornado_watches } = FAKE_ALERTS;
-
-	const warnings =
-		tornado_warnings.length > 0 ? (
-			<AlertsSection alerts={tornado_warnings} />
+export const Alerts = ({ warnings, watches }) => {
+	const warningAlerts =
+		warnings.length > 0 ? (
+			<AlertsSection alerts={warnings} />
 		) : (
 			<p className='mt-4 text-xl text-green-400'>No Active Tornado Warnings.</p>
 		);
 
-	const watches =
-		tornado_watches.length > 0 ? (
-			<AlertsSection alerts={tornado_watches} />
+	const watchAlerts =
+		watches.length > 0 ? (
+			<AlertsSection alerts={watches} />
 		) : (
 			<p className='mt-4 text-xl text-green-400'>No Active Tornado Watches.</p>
 		);
 
 	return (
-		<>
-			{warnings}
-			{watches}
-		</>
+		<div id='alerts'>
+			{warningAlerts}
+			{watchAlerts}
+		</div>
 	);
 };
