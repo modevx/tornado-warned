887a6bd6e1eca039cac7ae232d599acddb3fbd51
Ephraim <admin@ephraimsmith.dev>
2022-04-11 14:54:51 -0500

    move logo text to Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,8 +7,13 @@ export const Header = () => {
 			data-testid='header'
 			className='flex justify-between items-center text-xs'
 		>
+			<div>
+				<div className='text-lg font-bold'>
+					TORNADO <span className='text-red-500'>ACTION</span>
+				</div>
+				<Clock />
+			</div>
 			<NavBar />
-			<Clock />
 		</header>
 	);
 };
