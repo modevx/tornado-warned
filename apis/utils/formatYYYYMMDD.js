1107886240d45693af2f8655563c363b7f8dc0c0
Ephraim <admin@ephraimsmith.dev>
2022-04-12 13:35:51 -0500

    simplify folder structure

--- apis/utils/formatYYYYMMDD.js
+++
@@ -1,3 +0,0 @@
-export const formatYYYYMMDD = date => {
-	return date.toISOString().slice(0, 10).split("-").join("");
-};
--- apis/utils/parseGeoJsonCoords.js
+++
@@ -1,7 +0,0 @@
-export const parseGeoJsonCoords = (arrWithGeoJson) => {
-	const geoJsonCoordsArr = arrWithGeoJson.map((tvs) => {
-		return tvs.geometry.coordinates;
-	});
-
-	return geoJsonCoordsArr;
-};
