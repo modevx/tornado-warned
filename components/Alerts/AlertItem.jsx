3d3f0d688a0d631b66985eac971477a171bf2730
Ephraim <admin@ephraimsmith.dev>
2022-04-12 21:41:16 -0500

    build out new Alert components

--- components/Alerts/AlertItem.jsx
+++ components/Alerts/AlertItem.jsx
@@ -31,103 +31,58 @@ export const AlertItem = ({ activeAlert }) => {
 	const color = EVENT_COLOR_MAP[event.split(" ").join("")];
 	const italicWarn = event === "Tornado Warning" ? "italic" : "";
 	const areaDescMAP = getAreaDescMAP(areaDesc);
-
 	const areaDescMapARR = Array.from(areaDescMAP.entries());
-	const impactedAreas = areaDescMapARR.map(([state, areas]) => {
-		const joinedAreaDescSTR = areas.join(", ");
 
-		return (
-			<div key={state} className='mb-2'>
-				<h4 className='text-xl font-medium mb-2 uppercase'>{STATES[state]}</h4>
-				<p>{joinedAreaDescSTR}</p>
-			</div>
-		);
-	});
-
-	// 	const alertPropsToDisplay = [headline, impactedAreas, instruction];
-
-	// 	const alertCardElements = alertPropsToDisplay.map(alertProp => {
-	// 		return (
-	// 			<div className='bg-neutral-700 p-4 mb-4 rounded'>
-	// 				<p className='text-xs'>{alertProp}</p>
-	// 			</div>
-	// 		);
-	// 	});
-
-	// 	return (
-	// 		<li
-	// 			className={`bg-gradient-to-b ${color} to-stone-800 ${color} mb-6 sm:m-0 sm:h-fit p-4 rounded shadow-black shadow-md`}
-	// 		>
-	// 			<div className='flex items-center mb-4 w-full'>
-	// 				<Image src='/nws-logo.png' height={50} width={50} />
-	// 				<div className='flex-1 ml-2 font-medium text-right'>
-	// 					<p className={`${italicWarn} text-xl uppercase w-full`}>{event}</p>
-	// 					<p className='text-md font-light w-full'>
-	// 						{formatSenderNameSTR(senderName)}
-	// 					</p>
-	// 				</div>
-	// 			</div>
-
-	// 			<div className='flex items-center justify-between bg-neutral-700 p-4 mb-4 rounded'>
-	// 				<p className='text-center'>{dayjs(effective).format("LT")}</p>
-	// 				<span>until</span>
-	// 				<p className='text-center'>{dayjs(expires).format("LT")}</p>
-	// 			</div>
-
-	// <div className='bg-neutral-700 p-4 mb-4 rounded'>
-	// 	<p className='text-xs'>{areaDesc}</p>
-	// 	{impactedAreas}
-	// </div>
-
-	// {instruction !== null ? (
-	// 	<div className='bg-neutral-700 p-4 mb-4 rounded'>
-	// 		<p className='text-xs'>{instruction}</p>
-	// 	</div>
-	// ) : (
-	// 	""
-	// )}
-
-	// 			<div className='bg-neutral-700 p-4 mb-4 rounded'>
-	// 				<p className='font-bold italic text-xl mb-2'>Description</p>
-	// 				{description.split(/\n(?:\n)?/gm).map(copySection => (
-	// 					<p className='text-xs'>{copySection}</p>
-	// 				))}
-	// 			</div>
-	// 		</li>
-	// 	);
-
-	// ****************************************
-	// ** DISCLOSURE
-	// ****************************************
 	return (
 		<div
-			className={`bg-gradient-to-r ${color} to-neutral-800 w-full max-w-md shadow-md shadow-stone-900 mb-4 mx-auto rounded-md p-2`}
+			className={`flex flex-col bg-gradient-to-b ${color} to-neutral-400 w-full max-w-md shadow-md shadow-stone-900 mb-6 mx-auto rounded-md p-2`}
 		>
-			<div className='flex items-center mb-4 w-full'>
-				<Image src='/nws-logo.png' height={50} width={50} />
-				<div className='flex-1 ml-2 font-medium text-right'>
-					<p className={` text-xl uppercase w-full`}>
-						{formatSenderNameSTR(senderName)}
-					</p>
-					<p className={`${italicWarn} text-md font-light w-full`}>{event}</p>
+			<div className='flex flex-col items-center mb-4 w-full'>
+				<p
+					className={`${italicWarn} text-3xl text-center font-medium w-full mb-2`}
+				>
+					{event}
+				</p>
+
+				<div className='flex items-center'>
+					<Image src='/nws-logo.png' height={30} width={30} />
+					<span className='ml-2'>{formatSenderNameSTR(senderName)}</span>
 				</div>
 			</div>
 
-			{impactedAreas}
+			<div className='bg-neutral-700 px-4 py-2 mb-4 rounded'>
+				<p className='text-xs text-right'>
+					Expires: {dayjs(expires).format("LT")}
+				</p>
+				{areaDescMapARR.map(([state, areas]) => {
+					const joinedAreaDescSTR = areas.join(", ");
+
+					return (
+						<div key={state}>
+							<h4 className='text-lg font-medium mb-2 uppercase'>
+								{STATES[state]}
+							</h4>
+							<p className='text-sm mb-2'>{joinedAreaDescSTR}</p>
+						</div>
+					);
+				})}
+			</div>
 
 			{instruction !== null ? (
-				<div className='bg-neutral-700 p-4 mb-4 rounded'>
-					<p className='text-xs'>{instruction}</p>
+				<div className='bg-neutral-700 px-4 py-2 mb-4 rounded'>
+					<p className='text-sm italic'>{instruction}</p>
 				</div>
 			) : (
 				""
 			)}
 
+			<div className='flex-1'></div>
+
 			<Disclosure>
 				{({ open }) => (
 					<>
 						<Disclosure.Button
-							className={`flex justify-between w-full px-4 py-2 text-sm font-medium rounded-md bg-stone-900`}
+							className={`flex justify-between w-full shadow-lg shadow-black px-4 py-2 mb-2 text-sm font-medium rounded-md bg-blue-600 active:scale-95 active:shadow-sm`}
 						>
 							<span>Details</span>
 							<ChevronUpIcon
@@ -136,9 +91,10 @@ export const AlertItem = ({ activeAlert }) => {
 								} w-5 h-5 text-white`}
 							/>
 						</Disclosure.Button>
-						<Disclosure.Panel className='px-4 pt-4 pb-2 text-sm text-gray-500'>
-							If you're unhappy with your purchase for any reason, email us
-							within 90 days and we'll refund you in full, no questions asked.
+						<Disclosure.Panel className='px-4 pt-4 pb-2 text-sm text-white'>
+							{description.split(/\n(?:\n)?/).map(text => (
+								<p className='text-xs mb-2'>{text}</p>
+							))}
 						</Disclosure.Panel>
 					</>
 				)}
