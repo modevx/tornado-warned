b47c6834ada277a8b8cab2d19c49477482b7d161
Ephraim <admin@ephraimsmith.dev>
2022-04-13 08:11:01 -0500

    dadd generic 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { PageWrapper } from "../components/PageWrapper";
 
 export default function Custom404() {
-	return <h1>404 - Page Not Found</h1>;
+	return (
+		<PageWrapper>
+			<h1 className='sr-only'>404 - Page Not Found</h1>
+			<p className='font-bold text-3xl'>
+				sorry. couldn&apos;t find THAT page. smash the BACK button to get back
+				on track.
+			</p>
+		</PageWrapper>
+	);
 }
