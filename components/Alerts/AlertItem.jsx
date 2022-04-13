719aad8431f774f4ce9062aac330cadaefb87d57
Ephraim <admin@ephraimsmith.dev>
2022-04-12 19:52:20 -0500

    re-structure Alert components

--- components/Alerts/AlertItem.jsx
+++ components/Alerts/AlertItem.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import Image from "next/image";
 // --
 import { Disclosure } from "@headlessui/react";
-import { BsTornado } from "react-icons/bs";
+import { ChevronUpIcon } from "@heroicons/react/solid";
 // --
-import { getAreaDescMAP } from "./utils/getAreaDescByState";
-import { formatSenderNameSTR } from "./utils/formatNWSStationNameSTR";
-import { STATES } from "../../constants";
+import { getAreaDescMAP, formatSenderNameSTR } from "./utils";
+import { STATE_ABBREVIATION_MAP as STATES } from "../states";
 // --
 import LocalizedFormat from "dayjs/plugin/localizedFormat";
 import dayjs from "dayjs";
 dayjs.extend(LocalizedFormat);
 
-export const AlertItem = ({ alert }) => {
-	// console.log("AlertListItem >>\n", alert);
+export const AlertItem = ({ activeAlert }) => {
+	// console.log("AlertListItem >>\n", activeAlert.properties.event);
 	const {
 		event,
 		messageType,
@@ -24,7 +23,7 @@ export const AlertItem = ({ alert }) => {
 		description,
 		instruction,
 		senderName,
-	} = alert;
+	} = activeAlert.properties;
 	const EVENT_COLOR_MAP = {
 		TornadoWarning: "from-red-500",
 		TornadoWatch: "from-yellow-600",
@@ -32,58 +31,90 @@ export const AlertItem = ({ alert }) => {
 	const color = EVENT_COLOR_MAP[event.split(" ").join("")];
 	const italicWarn = event === "Tornado Warning" ? "italic" : "";
 	const areaDescMAP = getAreaDescMAP(areaDesc);
-	// [...[state, areas]]
+
 	const areaDescMapARR = Array.from(areaDescMAP.entries());
 	const impactedAreas = areaDescMapARR.map(([state, areas]) => {
 		const joinedAreaDescSTR = areas.join(", ");
 
 		return (
 			<div key={state} className='mb-2'>
-				<h4 className='text-2xl font-medium mb-2 uppercase'>{STATES[state]}</h4>
+				<h4 className='text-xl font-medium mb-2 uppercase'>{STATES[state]}</h4>
 				<p>{joinedAreaDescSTR}</p>
 			</div>
 		);
 	});
 
-	const alertPropsToDisplay = [headline, impactedAreas, instruction];
+	// 	const alertPropsToDisplay = [headline, impactedAreas, instruction];
 
-	const alertCardElements = alertPropsToDisplay.map(alertProp => {
-		return (
-			<div className='bg-neutral-700 p-4 mb-4 rounded'>
-				<p className='text-xs'>{alertProp}</p>
-			</div>
-		);
-	});
+	// 	const alertCardElements = alertPropsToDisplay.map(alertProp => {
+	// 		return (
+	// 			<div className='bg-neutral-700 p-4 mb-4 rounded'>
+	// 				<p className='text-xs'>{alertProp}</p>
+	// 			</div>
+	// 		);
+	// 	});
 
+	// 	return (
+	// 		<li
+	// 			className={`bg-gradient-to-b ${color} to-stone-800 ${color} mb-6 sm:m-0 sm:h-fit p-4 rounded shadow-black shadow-md`}
+	// 		>
+	// 			<div className='flex items-center mb-4 w-full'>
+	// 				<Image src='/nws-logo.png' height={50} width={50} />
+	// 				<div className='flex-1 ml-2 font-medium text-right'>
+	// 					<p className={`${italicWarn} text-xl uppercase w-full`}>{event}</p>
+	// 					<p className='text-md font-light w-full'>
+	// 						{formatSenderNameSTR(senderName)}
+	// 					</p>
+	// 				</div>
+	// 			</div>
+
+	// 			<div className='flex items-center justify-between bg-neutral-700 p-4 mb-4 rounded'>
+	// 				<p className='text-center'>{dayjs(effective).format("LT")}</p>
+	// 				<span>until</span>
+	// 				<p className='text-center'>{dayjs(expires).format("LT")}</p>
+	// 			</div>
+
+	// <div className='bg-neutral-700 p-4 mb-4 rounded'>
+	// 	<p className='text-xs'>{areaDesc}</p>
+	// 	{impactedAreas}
+	// </div>
+
+	// {instruction !== null ? (
+	// 	<div className='bg-neutral-700 p-4 mb-4 rounded'>
+	// 		<p className='text-xs'>{instruction}</p>
+	// 	</div>
+	// ) : (
+	// 	""
+	// )}
+
+	// 			<div className='bg-neutral-700 p-4 mb-4 rounded'>
+	// 				<p className='font-bold italic text-xl mb-2'>Description</p>
+	// 				{description.split(/\n(?:\n)?/gm).map(copySection => (
+	// 					<p className='text-xs'>{copySection}</p>
+	// 				))}
+	// 			</div>
+	// 		</li>
+	// 	);
+
+	// ****************************************
+	// ** DISCLOSURE
+	// ****************************************
 	return (
-		<li
-			className={`bg-gradient-to-b ${color} to-stone-800 ${color} mb-6 sm:m-0 sm:h-fit p-4 rounded shadow-black shadow-md`}
+		<div
+			className={`bg-gradient-to-r ${color} to-neutral-800 w-full max-w-md shadow-md shadow-stone-900 mb-4 mx-auto rounded-md p-2`}
 		>
-			{/* -- BANNER -- */}
 			<div className='flex items-center mb-4 w-full'>
 				<Image src='/nws-logo.png' height={50} width={50} />
 				<div className='flex-1 ml-2 font-medium text-right'>
-					<p className={`${italicWarn} text-xl uppercase w-full`}>{event}</p>
-					<p className='text-md font-light w-full'>
+					<p className={` text-xl uppercase w-full`}>
 						{formatSenderNameSTR(senderName)}
 					</p>
+					<p className={`${italicWarn} text-md font-light w-full`}>{event}</p>
 				</div>
 			</div>
 
-			{/* -- TIMES -- */}
-			<div className='flex items-center justify-between bg-neutral-700 p-4 mb-4 rounded'>
-				<p className='text-center'>{dayjs(effective).format("LT")}</p>
-				<span>until</span>
-				<p className='text-center'>{dayjs(expires).format("LT")}</p>
-			</div>
+			{impactedAreas}
 
-			{/* -- AREAS -- */}
-			<div className='bg-neutral-700 p-4 mb-4 rounded'>
-				{/* <p className='text-xs'>{areaDesc}</p> */}
-				{impactedAreas}
-			</div>
-
-			{/* -- INSTRUCTIONS -- */}
 			{instruction !== null ? (
 				<div className='bg-neutral-700 p-4 mb-4 rounded'>
 					<p className='text-xs'>{instruction}</p>
@@ -92,12 +123,26 @@ export const AlertItem = ({ alert }) => {
 				""
 			)}
 
-			{/* <div className='bg-neutral-700 p-4 mb-4 rounded'>
-				<p className='font-bold italic text-xl mb-2'>Description</p>
-				{description.split(/\n(?:\n)?/gm).map(copySection => (
-					<p className='text-xs'>{copySection}</p>
-				))}
-			</div> */}
-		</li>
+			<Disclosure>
+				{({ open }) => (
+					<>
+						<Disclosure.Button
+							className={`flex justify-between w-full px-4 py-2 text-sm font-medium rounded-md bg-stone-900`}
+						>
+							<span>Details</span>
+							<ChevronUpIcon
+								className={`${
+									open ? "" : "transform rotate-180"
+								} w-5 h-5 text-white`}
+							/>
+						</Disclosure.Button>
+						<Disclosure.Panel className='px-4 pt-4 pb-2 text-sm text-gray-500'>
+							If you're unhappy with your purchase for any reason, email us
+							within 90 days and we'll refund you in full, no questions asked.
+						</Disclosure.Panel>
+					</>
+				)}
+			</Disclosure>
+		</div>
 	);
 };
--- components/Alerts/AlertSection.jsx
+++ components/Alerts/AlertSection.jsx
@@ -1,15 +1,27 @@
-// import { AlertList } from "./";
-import {
-	useTornadoWarnings,
-	useTornadoWatches,
-	useTestAlerts,
-} from "../../hooks";
+import { AlertList } from "./";
+import { useAllTestAlerts, useAllTornadoAlerts } from "../../hooks";
 
 export const AlertSection = () => {
-	return (
-		<section>
-			{/* <AlertList />
-			<AlertList /> */}
-		</section>
-	);
+	// const { isLoading, error, isSuccess, data } = useAllTornadoAlerts();
+	const { isLoading, error, isSuccess, data } = useAllTestAlerts();
+	let warnings, watches;
+
+	if (data) {
+		return (
+			<section>
+				<AlertList
+					activeAlerts={data.filter(
+						alert => alert.properties.event === "Tornado Warning"
+					)}
+				/>
+				<AlertList
+					activeAlerts={data.filter(
+						alert => alert.properties.event === "Tornado Watch"
+					)}
+				/>
+			</section>
+		);
+	}
+
+	return <p>Loading...</p>;
 };
