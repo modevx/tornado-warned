d1dfbcf8d00ae9206e078550de11b28dc6c3b595
Ephraim <admin@ephraimsmith.dev>
2022-04-12 21:40:57 -0500

    change Alert component breakpoint to md

--- components/Alerts/AlertList.jsx
+++ components/Alerts/AlertList.jsx
@@ -5,7 +5,7 @@ import { useAllTestAlerts, useAllTornadoAlerts } from "../../hooks";
 export const AlertList = ({ activeAlerts }) => {
 	if (activeAlerts.length > 1) {
 		return (
-			<ul className='p-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+			<ul className='p-4 md:grid md:grid-cols-2 xl:grid-cols-3 gap-6'>
 				{activeAlerts.map(alert => (
 					<AlertItem activeAlert={alert} key={alert.id} />
 				))}
