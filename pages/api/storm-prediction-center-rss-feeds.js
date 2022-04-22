2071af5499f7bac9de585c35bce9a8236ee46971
Ephraim <admin@ephraimsmith.dev>
2022-04-22 14:36:52 -0500

    build API route that handle all SPC RSS feeds

---
+++ pages/api/storm-prediction-center-rss-feeds.js
@@ -0,0 +1,32 @@
+import Cors from "cors";
+import RSSParser from "rss-parser";
+import { STORM_PREDICTION_CENTER as SPC } from "../../services/constants/endpoints";
+
+export default async function handler(req, res) {
+	let rss_parser = new RSSParser();
+
+	await runMiddleware(req, res, corsMiddleware);
+
+	await rss_parser.parseURL(req.body.rssFeedUrl, (error, feed) => {
+		if (error) throw error;
+		res.status(200).json(feed.items);
+	});
+}
+
+const corsMiddleware = Cors({
+	origin: true,
+	methods: ["POST", "OPTIONS"],
+	preflightContinue: true,
+});
+
+function runMiddleware(req, res, middleware) {
+	return new Promise((resolve, reject) => {
+		middleware(req, res, result => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
