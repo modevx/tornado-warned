20922de8e41d2807da722046f027007315b88a00
Ephraim <admin@ephraimsmith.dev>
2022-04-22 16:58:08 -0500

    add Features template

--- components/FeaturesSection/FeaturesSection.jsx
+++ components/FeaturesSection/FeaturesSection.jsx
@@ -1,54 +1,75 @@
+/* This example requires Tailwind CSS v2.0+ */
+import {
+	AnnotationIcon,
+	GlobeAltIcon,
+	LightningBoltIcon,
+	ScaleIcon,
+} from "@heroicons/react/outline";
+
 export const FeaturesSection = () => {
 	return (
-		<div className='surface-0 text-center'>
-			<div className='mb-3 font-bold text-2xl'>
-				<span className='text-900'>One Product, </span>
-				<span className='text-blue-600'>Many Solutions</span>
-			</div>
-			<div className='text-700 text-sm mb-6'>
-				Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna.
-			</div>
-			<div className='grid'>
-				<div className='col-12 md:col-4 mb-4 px-5'>
-					<span
-						className='p-3 shadow-2 mb-3 inline-block'
-						style={{ borderRadius: "10px" }}
-					>
-						<i className='pi pi-desktop text-4xl text-blue-500'></i>
-					</span>
-					<div className='text-900 mb-3 font-medium'>Built for Developers</div>
-					<span className='text-700 text-sm line-height-3'>
-						Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur.
-					</span>
+		<div className='py-12 bg-white'>
+			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+				<div className='lg:text-center'>
+					<h2 className='text-base text-indigo-600 font-semibold tracking-wide uppercase'>
+						Transactions
+					</h2>
+					<p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+						A better way to send money
+					</p>
+					<p className='mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto'>
+						Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+						voluptatum cupiditate veritatis in accusamus quisquam.
+					</p>
 				</div>
-				<div className='col-12 md:col-4 mb-4 px-5'>
-					<span
-						className='p-3 shadow-2 mb-3 inline-block'
-						style={{ borderRadius: "10px" }}
-					>
-						<i className='pi pi-lock text-4xl text-blue-500'></i>
-					</span>
-					<div className='text-900 mb-3 font-medium'>End-to-End Encryption</div>
-					<span className='text-700 text-sm line-height-3'>
-						Risus nec feugiat in fermentum posuere urna nec. Posuere
-						sollicitudin aliquam ultrices sagittis.
-					</span>
-				</div>
-				<div className='col-12 md:col-4 mb-4 px-5'>
-					<span
-						className='p-3 shadow-2 mb-3 inline-block'
-						style={{ borderRadius: "10px" }}
-					>
-						<i className='pi pi-check-circle text-4xl text-blue-500'></i>
-					</span>
-					<div className='text-900 mb-3 font-medium'>Easy to Use</div>
-					<span className='text-700 text-sm line-height-3'>
-						Ornare suspendisse sed nisi lacus sed viverra tellus. Neque volutpat
-						ac tincidunt vitae semper.
-					</span>
+
+				<div className='mt-10'>
+					<dl className='space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10'>
+						{features.map(feature => (
+							<div key={feature.name} className='relative'>
+								<dt>
+									<div className='absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
+										<feature.icon className='h-6 w-6' aria-hidden='true' />
+									</div>
+									<p className='ml-16 text-lg leading-6 font-medium text-gray-900'>
+										{feature.name}
+									</p>
+								</dt>
+								<dd className='mt-2 ml-16 text-base text-gray-500'>
+									{feature.description}
+								</dd>
+							</div>
+						))}
+					</dl>
 				</div>
 			</div>
 		</div>
 	);
 };
+
+const features = [
+	{
+		name: "Competitive exchange rates",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+		icon: GlobeAltIcon,
+	},
+	{
+		name: "No hidden fees",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+		icon: ScaleIcon,
+	},
+	{
+		name: "Transfers are instant",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+		icon: LightningBoltIcon,
+	},
+	{
+		name: "Mobile notifications",
+		description:
+			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+		icon: AnnotationIcon,
+	},
+];
