12a23bd86e1983cc5b80af5ff19f4da10d4764b0
Ephraim <admin@ephraimsmith.dev>
2022-04-22 10:30:08 -0500

    clean up SPC RSS feed test code

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,24 @@
-import Axios from "axios";
 import React from "react";
 
 import { PageWrapper } from "../components/PageWrapper";
 import { AlertSection } from "../components/Alerts";
 
+import { useSpcRssFeeds } from "../hooks";
+
 const HomeScreen = () => {
-	const [feed, setFeed] = React.useState();
-	const [testFeed, setTestFeed] = React.useState();
-	let rssFeedDisplay, testRssFeedDisplay;
+	const { data, error, isLoading } = useSpcRssFeeds();
+	let rssFeedDisplay;
 
-	// React.useEffect(async () => {
-	// 	let rssFeed = await axios.get("/api/cors/spc-rss-feed");
-	// 	setFeed(rssFeed.data.items);
-	// 	console.log(Object.entries(rssFeed.data.items));
-	// }, []);
+	if (error) {
+		rssFeedDisplay = <p>ERROR: Storm Prediction Center - RSS feed</p>;
+	}
 
-	React.useEffect(async () => {
-		let feed = await Axios.get(
-			"https://test-www-spc.woc.noaa.gov/products/spcrss.xml"
-		);
-		// setTestFeed(testFeed.data.items);
-		console.log(feed);
-		// console.log(Object.entries(testFeed.data.items));
-	}, []);
+	if (isLoading) {
+		rssFeedDisplay = <p>Storm Prediction Center RSS feed loading...</p>;
+	}
 
-	// if (feed) {
-	// 	rssFeedDisplay = feed.map(item => (
+	// if (data) {
+	// 	rssFeedDisplay = data.map(item => (
 	// 		<div key={item.link} className='mb-4 bg-black p-4'>
 	// 			{Object.entries(item).map(([key, value]) => (
 	// 				<>
@@ -36,31 +29,14 @@ const HomeScreen = () => {
 	// 			))}
 	// 		</div>
 	// 	));
-	// } else {
-	// 	rssFeedDisplay = <p>No RSS feed...</p>;
 	// }
 
-	// if (testFeed) {
-	// 	testRssFeedDisplay = testFeed.map(item => (
-	// 		<div key={item.link} className='mb-4 bg-black p-4'>
-	// 			{Object.entries(item).map(([key, value]) => (
-	// 				<>
-	// 					<span className='font-bold italic text-red-400'>{key}: </span>
-	// 					<span>{value}</span>
-	// 					<br />
-	// 				</>
-	// 			))}
-	// 		</div>
-	// 	));
-	// } else {
-	// 	testRssFeedDisplay = <p>No RSS testFeed...</p>;
-	// }
+	if (data) console.log(data);
 
 	return (
 		<PageWrapper>
 			<AlertSection />
-			{/* {rssFeedDisplay} */}
-			{/* {testRssFeedDisplay} */}
+			{rssFeedDisplay}
 		</PageWrapper>
 	);
 };
