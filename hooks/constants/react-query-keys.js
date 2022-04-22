bb1aacb9ad85b58931ce77d98ba184193788d743
Ephraim <admin@ephraimsmith.dev>
2022-04-22 10:29:10 -0500

    add react-query-keys

--- hooks/constants/react-query-keys.js
+++ hooks/constants/react-query-keys.js
@@ -1,12 +1,15 @@
 const NATIONAL_WEATHER_SERVICE = Object.freeze({
-	active_tornado_alerts: "all_tornado_alerts",
-	active_tornado_warnings: "active_warnings",
-	active_tornado_watches: "active_watches",
+	active_tornado_alerts: "nws_all_tornado_alerts",
+	active_tornado_warnings: "nws_active_tornado_warnings",
+	active_tornado_watches: "nws_active_tornado_watches",
 });
 const SEVERE_WEATHER_DATA_INVENTORY = Object.freeze({
-	tornado_vortex_signatures: "tornadoVortexSignatures",
+	tornado_vortex_signatures: "swdi_tornado_vortex_signatures",
 });
-const STORM_PREDICTION_CENTER = Object.freeze({});
+const STORM_PREDICTION_CENTER = Object.freeze({
+	all_rss_feeds: "spc_all_rss_feeds",
+	test_all_rss_feeds: "spc_test_all_rss_feeds",
+});
 
 export {
 	NATIONAL_WEATHER_SERVICE,
