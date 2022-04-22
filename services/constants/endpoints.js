b2eb4a3f067775f6dc39a2b3fe5b7d0872016330
Ephraim <admin@ephraimsmith.dev>
2022-04-22 10:28:36 -0500

    add spc rss feed api routes

--- services/constants/endpoints.js
+++ services/constants/endpoints.js
@@ -25,12 +25,24 @@ const SEVERE_WEATHER_DATA_INVENTORY = Object.freeze({
 	storm_cell_structure_information_json: "/json/nx3structure",
 });
 const STORM_PREDICTION_CENTER = Object.freeze({
-	spcrss: "/spcrss.xml",
-	spcwwrss: "/spcwwrss.xml",
-	spcpdswwrss: "/spcpdswwrss.xml",
-	spcmdrss: "/spcmdrss.xml",
-	spcacrss: "/spcacrss.xml",
-	spcmbrss: "/spcmbrss.xml",
+	api_rss_all_feeds: "/api/rss-storm-prediction-center-all-feeds",
+	api_rss_convective_outlook:
+		"/api/rss-storm-prediction-center-convective-outlook",
+	api_rss_dangerous_situations:
+		"/api/rss-storm-prediction-center-dangerous-situations",
+	api_rss_mesoscale_discussions:
+		"/api/rss-storm-prediction-center-mesoscale-discussions",
+	api_rss_severe_wx_media_briefs:
+		"/api/rss-storm-prediction-center-severe-wx-xmedia-briefings",
+	api_rss_status_reports: "/api/rss-storm-prediction-center-status-reports",
+	base_url: "http://www.spc.noaa.gov",
+	convective_outlook_rss_feed: "/products/spcacrss.xml",
+	mesoscale_discussions_rss_feed: "/products/spcmdrss.xml",
+	particulary_dangerous_situation_rss_feed: "/products/spcpdswwrss.xml",
+	rss_all_feeds: "/products/spcrss.xml",
+	severe_wx_outlook_rss_feed: "/products/spcmbrss.xml",
+	test_base_url: "http://test-www-spc.woc.noaa.gov",
+	tornado_thunderstorm_rss_feed: "/products/spcwwrss.xml",
 });
 
 export {
