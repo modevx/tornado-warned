fd3b34a3337f8856978012536b877302cf2a9718
Ephraim <admin@ephraimsmith.dev>
2022-04-22 10:28:51 -0500

    update import paths

--- hooks/storm-prediction-center.js
+++ hooks/storm-prediction-center.js
@@ -1,1 +1,7 @@
 import { useQuery } from "react-query";
+import { STORM_PREDICTION_CENTER as KEYS } from "./constants/react-query-keys";
+import { fetchAllRSSFeeds } from "../services/storm-prediction-center";
+
+export const useSpcRssFeeds = () => {
+	return useQuery(KEYS.all_rss_feeds, fetchAllRSSFeeds);
+};
