e2b9387f9d7d5a461aeef711a3c96e0ef6624d83
Ephraim <admin@ephraimsmith.dev>
2022-04-22 04:29:17 -0500

    create react-query-keys file

---
+++ hooks/constants/query-keys.js
@@ -0,0 +1,15 @@
+const NATIONAL_WEATHER_SERVICE = Object.freeze({
+	active_tornado_alerts: "all_tornado_alerts",
+	active_tornado_warnings: "active_warnings",
+	active_tornado_watches: "active_watches",
+});
+const SEVERE_WEATHER_DATA_INVENTORY = Object.freeze({
+	tornado_vortex_signatures: "tornadoVortexSignatures",
+});
+const STORM_PREDICTION_CENTER = Object.freeze({});
+
+export {
+	NATIONAL_WEATHER_SERVICE,
+	SEVERE_WEATHER_DATA_INVENTORY,
+	STORM_PREDICTION_CENTER,
+};
