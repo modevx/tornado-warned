bb3c083f0a2f55df959e34fbb8398689e2e00440
Ephraim <admin@ephraimsmith.dev>
2022-04-23 13:18:27 -0500

    replace Navbar component with resp. Nav code from Hero

--- components/Header/Navbar.jsx
+++ components/Header/Navbar.jsx
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Menu, Popover, Transition } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import { RiTornadoLine } from "react-icons/ri";
 
@@ -9,139 +9,120 @@ function classNames(...classes) {
 }
 
 const navigation = [
-	{
-		label: "alerts",
-		href: "/alerts",
-		isCurrent: false,
-		isDisabled: false,
-	},
-	{
-		label: "outlooks",
-		href: "/outlooks",
-		isCurrent: false,
-		isDisabled: true,
-	},
+	{ name: "alerts", href: "/alerts" },
+	{ name: "outlooks", href: "/outlooks" },
+	{ name: "be ready", href: "/safety" },
 	// {
-	// 	label: "safety",
+	// 	name: "safety",
 	// 	href: "/safety",
-	// isCurrent: false,
-	// 	isDisabled: true
 	// },
 	// {
-	// 	label: "storm chasing",
+	// 	name: "storm chasing",
 	// 	href: "/storm chasing",
-	// isCurrent: false,
-	// 	isDisabled: true
 	// },
 	// {
-	// 	label: "developers",
+	// 	name: "developers",
 	// 	href: "/developers",
-	// isCurrent: false,
-	// isDisabled: true
 	// },
 	// {
-	// 	label: "images",
+	// 	name: "images",
 	// 	href: "/images",
-	// isCurrent: false,
-	// isDisabled: true
 	// },
 	// {
-	// 	label: "interactive",
+	// 	name: "interactive",
 	// 	href: "/interactive",
-	// isCurrent: false,
-	// isDisabled: true
 	// },
 	// {
-	// 	label: "pop culture",
+	// 	name: "pop culture",
 	// 	href: "/pop culture",
-	// isCurrent: false,
-	// isDisabled: true
 	// },
 	// {
-	// 	label: "radar",
+	// 	name: "radar",
 	// 	href: "/radar",
-	// isCurrent: false,
-	// isDisabled: true
 	// },
 	// {
-	// 	label: "stats",
+	// 	name: "stats",
 	// 	href: "/stats",
-	// isCurrent: false,
-	// isDisabled: true
 	// },
 ];
 
 export const Navbar = () => {
 	return (
-		<Disclosure as='nav'>
-			{({ open }) => (
-				<>
-					<div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
-						<div className='relative flex items-center justify-between h-16'>
-							<div className='absolute inset-y-0 right-0 flex items-center sm:hidden'>
-								{/* Mobile menu button*/}
-								<Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+		<Popover>
+			<div className='relative pt-6 px-4 sm:px-6 lg:px-8'>
+				<nav
+					className='relative flex items-center justify-between sm:h-10 lg:justify-start'
+					aria-name='Global'
+				>
+					<div className='flex items-center flex-grow flex-shrink-0 lg:flex-grow-0'>
+						<div className='flex items-center justify-between w-full md:w-auto'>
+							<a href='/'>
+								<span className='sr-only'>Tornado Action</span>
+								<RiTornadoLine color='red' className='h-8 w-auto sm:h-10' />
+							</a>
+							<div className='-mr-2 flex items-center md:hidden'>
+								<Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
 									<span className='sr-only'>Open main menu</span>
-									{open ? (
-										<XIcon className='block h-6 w-6' aria-hidden='true' />
-									) : (
-										<MenuIcon className='block h-6 w-6' aria-hidden='true' />
-									)}
-								</Disclosure.Button>
-							</div>
-							<div className='flex-1 flex items-center justify-between sm:items-stretch'>
-								<div className='flex-shrink-0 flex items-center'>
-									<RiTornadoLine
-										alt='tornado icon'
-										className='block h-12 w-auto'
-										color='limegreen'
-									/>
-								</div>
-								<div className='hidden sm:block sm:ml-6'>
-									<div className='flex space-x-4'>
-										{navigation.map(item => (
-											<a
-												key={item.label}
-												href={item.href}
-												className={classNames(
-													item.current
-														? "bg-gray-900 text-white"
-														: "text-gray-300 hover:bg-gray-700 hover:text-white",
-													"px-3 py-2 rounded-md text-sm font-medium"
-												)}
-												aria-current={item.current ? "page" : undefined}
-											>
-												{item.label}
-											</a>
-										))}
-									</div>
-								</div>
+									<MenuIcon className='h-6 w-6' aria-hidden='true' />
+								</Popover.Button>
 							</div>
 						</div>
 					</div>
-
-					<Disclosure.Panel className='sm:hidden'>
+					<div className='hidden md:block md:ml-10 md:pr-4 md:space-x-8'>
+						{navigation.map(item => (
+							<a
+								key={item.name}
+								href={item.href}
+								className='font-medium text-gray-500 hover:text-gray-900'
+							>
+								{item.name}
+							</a>
+						))}
+					</div>
+				</nav>
+			</div>
+			{/* ************************************************* */}
+			{/* ** HERO/mobile-menu-btn */}
+			{/* ************************************************* */}
+			<Transition
+				as={Fragment}
+				enter='duration-150 ease-out'
+				enterFrom='opacity-0 scale-95'
+				enterTo='opacity-100 scale-100'
+				leave='duration-100 ease-in'
+				leaveFrom='opacity-100 scale-100'
+				leaveTo='opacity-0 scale-95'
+			>
+				<Popover.Panel
+					focus
+					className='absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden'
+				>
+					<div className='rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden'>
+						<div className='px-5 pt-4 flex items-center justify-between'>
+							<div>
+								<RiTornadoLine color='red' className='h-8 w-auto' />
+							</div>
+							<div className='-mr-2'>
+								<Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+									<span className='sr-only'>Close main menu</span>
+									<XIcon className='h-6 w-6' aria-hidden='true' />
+								</Popover.Button>
+							</div>
+						</div>
 						<div className='px-2 pt-2 pb-3 space-y-1'>
 							{navigation.map(item => (
-								<Disclosure.Button
-									key={item.label}
-									as='a'
+								<a
+									key={item.name}
 									href={item.href}
-									className={classNames(
-										item.current
-											? "bg-gray-900 text-white"
-											: "text-gray-300 hover:bg-gray-700 hover:text-white",
-										"block px-3 py-2 rounded-md text-base font-medium"
-									)}
-									aria-current={item.current ? "page" : undefined}
+									className='block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50'
 								>
-									{item.label}
-								</Disclosure.Button>
+									{item.name}
+								</a>
 							))}
 						</div>
-					</Disclosure.Panel>
-				</>
-			)}
-		</Disclosure>
+					</div>
+				</Popover.Panel>
+			</Transition>
+		</Popover>
 	);
 };
