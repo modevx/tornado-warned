86d0e50e355410ad3f5ec12f13fe072e27a451e0
Ephraim <admin@ephraimsmith.dev>
2022-04-23 15:02:36 -0500

    update page import paths

--- pages/outlooks/OutlooksPage.jsx
+++ pages/outlooks/OutlooksPage.jsx
@@ -1,7 +1,7 @@
 import { PageWrapper } from "../../components/PageWrapper";
 import { Navbar } from "../../components/Header/Navbar";
-import { useSPC_RSS_Feed } from "../hooks";
-import { STORM_PREDICTION_CENTER as SPC_EP } from "../services/constants/endpoints";
+import { useSPC_RSS_Feed } from "../../hooks";
+import { STORM_PREDICTION_CENTER as SPC_EP } from "../../services/constants/endpoints";
 
 const OutlooksPage = () => {
 	const { data, error, isLoading } = useSPC_RSS_Feed(SPC_EP.rss_feed_all);
@@ -17,7 +17,7 @@ const OutlooksPage = () => {
 	}
 
 	if (data) {
-		rssFeedDisplay = data.data.map(item => (
+		rssFeedDisplay = data.data.map((item) => (
 			<div key={item.link} className='mb-4 bg-black p-4'>
 				{Object.entries(item).map(([itemKey, value]) => (
 					<div key={itemKey}>
