b4e18ec875e39352c5b997a0f393016078e60a72
Ephraim <admin@ephraimsmith.dev>
2022-04-23 09:50:00 -0500

    add Tornado icon

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -2,12 +2,11 @@
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { RiTornadoLine } from "react-icons/ri";
 
 const navigation = [
-	{ name: "Product", href: "#" },
-	{ name: "Features", href: "#" },
-	{ name: "Marketplace", href: "#" },
-	{ name: "Company", href: "#" },
+	{ name: "alerts", href: "/alerts" },
+	{ name: "outlooks", href: "/outlooks" },
 ];
 
 export const Hero = () => {
@@ -15,6 +14,7 @@ export const Hero = () => {
 		<div className='relative bg-white overflow-hidden'>
 			<div className='max-w-7xl mx-auto'>
 				<div className='relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32'>
+					{/* <RiTornadoLine color='red' className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" size={45} /> */}
 					<svg
 						className='hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2'
 						fill='currentColor'
@@ -34,11 +34,16 @@ export const Hero = () => {
 								<div className='flex items-center flex-grow flex-shrink-0 lg:flex-grow-0'>
 									<div className='flex items-center justify-between w-full md:w-auto'>
 										<a href='#'>
-											<span className='sr-only'>Workflow</span>
-											<img
+											<span className='sr-only'>Tornado Action</span>
+											<RiTornadoLine
+												color='red'
+												className='h-8 w-auto sm:h-10'
+												size={45}
+											/>
+											{/* <img
 												className='h-8 w-auto sm:h-10'
 												src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
-											/>
+											/> */}
 										</a>
 										<div className='-mr-2 flex items-center md:hidden'>
 											<Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
@@ -58,12 +63,6 @@ export const Hero = () => {
 											{item.name}
 										</a>
 									))}
-									<a
-										href='#'
-										className='font-medium text-indigo-600 hover:text-indigo-500'
-									>
-										Log in
-									</a>
 								</div>
 							</nav>
 						</div>
@@ -122,15 +121,14 @@ export const Hero = () => {
 					<main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
 						<div className='sm:text-center lg:text-left'>
 							<h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
-								<span className='block xl:inline'>Data to enrich your</span>{" "}
+								<span className='block xl:inline'>TORNADO ACTION</span>{" "}
 								<span className='block text-indigo-600 xl:inline'>
-									online business
+									alerts. radar. stats.
 								</span>
 							</h1>
 							<p className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0'>
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-								lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-								fugiat aliqua.
+								Tornado Action is your personal command center for tracking
+								tornadoes past and present.
 							</p>
 							<div className='mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start'>
 								<div className='rounded-md shadow'>
@@ -138,7 +136,7 @@ export const Hero = () => {
 										href='#'
 										className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
 									>
-										Get started
+										Features
 									</a>
 								</div>
 								<div className='mt-3 sm:mt-0 sm:ml-3'>
@@ -146,7 +144,7 @@ export const Hero = () => {
 										href='#'
 										className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10'
 									>
-										Live demo
+										Look Around
 									</a>
 								</div>
 							</div>
