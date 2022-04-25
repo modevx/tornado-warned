c08df167ffe808489eafd502732750e7da99dc00
Ephraim <admin@ephraimsmith.dev>
2022-04-25 10:06:12 -0500

    add FeatureSection

--- components/Features/FeatureSection.jsx
+++ components/Features/FeatureSection.jsx
@@ -6,37 +6,39 @@ import {
 	ScaleIcon,
 } from "@heroicons/react/outline";
 
-export const FeaturesSection = () => {
+import { RiTornadoLine } from "react-icons/ri";
+import { ImWarning } from "react-icons/im";
+
+export const FeatureSection = ({ featureOBJ }) => {
+	const { benefits: benefitsObjARR } = featureOBJ;
+	// {titleSTR, taglineSTR, benefitsObjARR}
 	return (
 		<div className='py-12 bg-white'>
 			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
 				<div className='lg:text-center'>
-					<h2 className='text-base text-indigo-600 font-semibold tracking-wide uppercase'>
-						Transactions
+					<h2 className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+						{featureOBJ.name}
 					</h2>
-					<p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-						A better way to send money
-					</p>
 					<p className='mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto'>
-						Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-						voluptatum cupiditate veritatis in accusamus quisquam.
+						{featureOBJ.tagline}
 					</p>
 				</div>
 
 				<div className='mt-10'>
 					<dl className='space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10'>
-						{features.map(feature => (
-							<div key={feature.name} className='relative'>
+						{benefitsObjARR.map((benefitsOBJ) => (
+							// {features.map((feature) => (
+							<div key={benefitsOBJ.name} className='relative'>
 								<dt>
-									<div className='absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
-										<feature.icon className='h-6 w-6' aria-hidden='true' />
+									<div className='absolute flex items-center justify-center h-12 w-12 rounded-md bg-green-500 text-white'>
+										<benefitsOBJ.icon className='h-6 w-6' aria-hidden='true' />
 									</div>
 									<p className='ml-16 text-lg leading-6 font-medium text-gray-900'>
-										{feature.name}
+										{benefitsOBJ.name}
 									</p>
 								</dt>
 								<dd className='mt-2 ml-16 text-base text-gray-500'>
-									{feature.description}
+									{benefitsOBJ.description}
 								</dd>
 							</div>
 						))}
@@ -52,13 +54,13 @@ const features = [
 		name: "Competitive exchange rates",
 		description:
 			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-		icon: GlobeAltIcon,
+		icon: RiTornadoLine,
 	},
 	{
 		name: "No hidden fees",
 		description:
 			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-		icon: ScaleIcon,
+		icon: ImWarning,
 	},
 	{
 		name: "Transfers are instant",
