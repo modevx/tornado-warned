fe173c8a623858eb71b5e6a3c12d852b81436a26
Ephraim <admin@ephraimsmith.dev>
2022-04-25 14:28:33 -0500

    relo RSS logic to component and import

--- pages/outlooks/OutlooksPage.jsx
+++ pages/outlooks/OutlooksPage.jsx
@@ -1,38 +1,24 @@
-import { PageWrapper } from "../../components/PageWrapper";
 import { Navbar } from "../../components/Header/Navbar";
+import { RSSFeed } from "./RSSFeed";
+import { PageWrapper } from "../../components/PageWrapper";
 import { useSPC_RSS_Feed } from "../../hooks";
 import { STORM_PREDICTION_CENTER as SPC_EP } from "../../services/constants/endpoints";
 
 const OutlooksPage = () => {
-	const { data, error, isLoading } = useSPC_RSS_Feed(SPC_EP.rss_feed_all);
+	const all = useSPC_RSS_Feed(SPC_EP.rss_feed_all);
+	const pds = useSPC_RSS_Feed(SPC_EP.rss_feed_particulary_dangerous_situation);
+	const media_briefing = useSPC_RSS_Feed(SPC_EP.rss_feed_multi_media_briefing);
+	const severe = useSPC_RSS_Feed(SPC_EP.rss_feed_severe_wx);
+	const convective = useSPC_RSS_Feed(SPC_EP.rss_feed_convective_outlook);
 
-	let rssFeedDisplay;
-
-	if (error) {
-		rssFeedDisplay = <p>ERROR: Storm Prediction Center - RSS feed</p>;
-	}
-
-	if (isLoading) {
-		rssFeedDisplay = <p>Storm Prediction Center RSS feed loading...</p>;
-	}
-
-	if (data) {
-		rssFeedDisplay = data.data.map((item) => (
-			<div key={item.link} className='mb-4 bg-black p-4'>
-				{Object.entries(item).map(([itemKey, value]) => (
-					<div key={itemKey}>
-						<span className='font-bold italic text-red-400'>{itemKey}: </span>
-						<span>{value}</span>
-						<br />
-					</div>
-				))}
-			</div>
-		));
-	}
 	return (
 		<PageWrapper>
 			<Navbar />
-			{rssFeedDisplay}
+			<RSSFeed rssFeedObjARR={all} />
+			<RSSFeed rssFeedObjARR={pds} />
+			<RSSFeed rssFeedObjARR={media_briefing} />
+			<RSSFeed rssFeedObjARR={severe} />
+			<RSSFeed rssFeedObjARR={convective} />
 		</PageWrapper>
 	);
 };
