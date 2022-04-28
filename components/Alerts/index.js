ef6e329a55d38d753e863ee96da4a2a16ad7ac88
Ephraim <admin@ephraimsmith.dev>
2022-04-28 09:50:55 -0500

    consolidate alert components to single file

--- components/Alerts/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./AlertDefinitionItem";
-export * from "./AlertDefinitionList";
-export * from "./AlertDefinitionSection";
-export * from "./AlertItem";
-export * from "./AlertList";
-export * from "./AlertSection";
