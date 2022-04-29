c2bad9eef647d69f11787ca6ed7b33256812f48d
Ephraim <admin@ephraimsmith.dev>
2022-04-29 14:39:52 -0500

    install & start playing with arcgis

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
+import React from "react";
+import { useQuery } from "react-query";
 import { Clock, FeatureSection, Hero, Page } from "components";
 import { features } from "constants";
+// ----- testing ArcGIS + Next.js
+import axios from "axios";
 
 const HomeScreen = () => {
+	let display = "";
+
+	const { isLoading, isError, error, data } = useQuery("arcgis", () =>
+		axios.get("/api/tutorial-route")
+	);
+
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
+
+	if (isError) {
+		return <p>Error: {error.message}</p>;
+	}
+
 	return (
 		<Page>
-			<Hero />
+			{/* <Hero />
 			{features &&
 				features.map((feature) => {
 					return <FeatureSection featureOBJ={feature} key={feature.name} />;
-				})}
+				})} */}
+			{data && <p>{JSON.stringify(data)}</p>}
 		</Page>
 	);
 };
