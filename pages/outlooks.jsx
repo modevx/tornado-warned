22858f37af984e913efd2103a5f1a85d9e846c36
Ephraim <admin@ephraimsmith.dev>
2022-05-01 13:55:33 -0500

    update imported endpoints constant

--- pages/outlooks.jsx
+++ pages/outlooks.jsx
@@ -1,14 +1,18 @@
 import { Navbar, Page, RSSFeed } from "components";
 import { useSPC_RSS_Feed } from "hooks";
-import { STORM_PREDICTION_CENTER as SPC_EP } from "services";
+import { ENDPOINTS } from "services/storm-prediction-center";
 
 const OutlooksPage = () => {
-	const all = useSPC_RSS_Feed(SPC_EP.rss_feed_all);
-	const severe = useSPC_RSS_Feed(SPC_EP.rss_feed_severe_wx);
-	const meso = useSPC_RSS_Feed(SPC_EP.rss_feed_mesoscale_discussions);
-	const convective = useSPC_RSS_Feed(SPC_EP.rss_feed_convective_outlook);
-	const media_briefing = useSPC_RSS_Feed(SPC_EP.rss_feed_multi_media_briefing);
-	const pds = useSPC_RSS_Feed(SPC_EP.rss_feed_particulary_dangerous_situation);
+	const all = useSPC_RSS_Feed(ENDPOINTS.rss_feed_all);
+	const severe = useSPC_RSS_Feed(ENDPOINTS.rss_feed_severe_wx);
+	const meso = useSPC_RSS_Feed(ENDPOINTS.rss_feed_mesoscale_discussions);
+	const convective = useSPC_RSS_Feed(ENDPOINTS.rss_feed_convective_outlook);
+	const media_briefing = useSPC_RSS_Feed(
+		ENDPOINTS.rss_feed_multi_media_briefing
+	);
+	const pds = useSPC_RSS_Feed(
+		ENDPOINTS.rss_feed_particulary_dangerous_situation
+	);
 
 	return (
 		<Page>
