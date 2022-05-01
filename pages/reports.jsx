a9d32d3e218dc513109a9273a9d6cfda55b07c11
Ephraim <admin@ephraimsmith.dev>
2022-05-01 18:39:41 -0500

    display Public Info Statement text

--- pages/reports.jsx
+++ pages/reports.jsx
@@ -14,13 +14,16 @@ const AlertsPage = () => {
 		);
 
 	if (isSuccess) {
-		display = data.map((warning) => <p>{JSON.stringify(warning)}</p>);
+		display = data.map((warning) => (
+			<p>{JSON.stringify(warning.productText)}</p>
+		));
 	}
 
 	return (
 		<Page>
 			<Navbar />
 			<h1>Local Storm Reports and Damage Assessments</h1>
+			{data && display}
 		</Page>
 	);
 };
