872415b136fc96d901e8fd4d5d12f3a58e2979ba
Ephraim <admin@ephraimsmith.dev>
2022-05-01 12:43:08 -0500

    reorg service files

--- services/service-severe-weather-data-inventory.js
+++ services/service-severe-weather-data-inventory.js
@@ -1,10 +1,10 @@
-import { SEVERE_WEATHER_DATA_INVENTORY as AXIOS } from "./https-client-config";
+import { SEVERE_WEATHER_DATA_INVENTORY as CLIENT } from "./https-client-config";
 import { SEVERE_WEATHER_DATA_INVENTORY as ENDPOINTS } from "./endpoints";
-import { fetchFromService } from "./base-request";
+import { fetchFromService } from "./http-client-base-request";
 
 const fetchTornadoVortexSignatures = async (dateRange) => {
 	return await fetchFromService(
-		AXIOS,
+		CLIENT,
 		ENDPOINTS.tornado_vortx_signatures_json,
 		dateRange
 	);
