2c5f461591d0fdfa008ecb13ac76f2c513db9c6a
Ephraim <admin@ephraimsmith.dev>
2022-05-01 14:13:14 -0500

    add public info statement rq key

--- hooks/react-query-keys.js
+++ hooks/react-query-keys.js
@@ -2,6 +2,7 @@ const NATIONAL_WEATHER_SERVICE = Object.freeze({
 	active_tornado_alerts: "nws_all_tornado_alerts",
 	active_tornado_warnings: "nws_active_tornado_warnings",
 	active_tornado_watches: "nws_active_tornado_watches",
+	public_information_statements: "nws_public_information_statements",
 });
 const SEVERE_WEATHER_DATA_INVENTORY = Object.freeze({
 	tornado_vortex_signatures: "swdi_tornado_vortex_signatures",
