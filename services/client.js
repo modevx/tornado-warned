3e874bc692930bc5c39e4fc73ef625e7ffaebbb9
Ephraim <admin@ephraimsmith.dev>
2022-05-01 12:50:14 -0500

    rename client file

---
+++ services/client.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const DEFAULT_TIMEOUT = 5000;
+
+export const fetchFromService = async (httpClient, path = "", options = {}) => {
+	const response = await httpClient.get(path, options);
+	return await response.data;
+};
+
+export const ARCGIS = axios.create({
+	baseURL: "https://mapservices.weather.noaa.gov/vector/rest/services",
+	timeout: DEFAULT_TIMEOUT,
+});
+
+export const NEXT_JS_API = axios.create({
+	baseURL: "/api",
+	timeout: DEFAULT_TIMEOUT,
+});
+
+export const NATIONAL_WEATHER_SERVICE = axios.create({
+	baseURL: "https://api.weather.gov",
+	timeout: DEFAULT_TIMEOUT,
+});
+
+export const SEVERE_WEATHER_DATA_INVENTORY = axios.create({
+	baseURL: "https://www.ncdc.noaa.gov/swdiws",
+	timeout: DEFAULT_TIMEOUT,
+});
+
+export const STORM_PREDICTION_CENTER = {
+	base: axios.create({
+		baseURL: "http://www.spc.noaa.gov",
+		timeout: DEFAULT_TIMEOUT,
+	}),
+	cors: NEXT_JS_API,
+	test_base: axios.create({
+		baseURL: "http://test-www-spc.woc.noaa.gov",
+		timeout: DEFAULT_TIMEOUT,
+	}),
+};
