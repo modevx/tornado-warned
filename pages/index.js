cb6f1c8fef1800c0526014ef012bfc5bd2742e30
Ephraim <admin@ephraimsmith.dev>
2022-05-14 19:19:50 -0500

    generate outlook maps for all 8 days

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,26 @@ import { useQuery } from "react-query";
 import { Clock, FeatureSection, Hero, Page } from "components";
 import { features } from "constants";
 // ----- testing ArcGIS + Next.js
-const ArcGISMap = dynamic(() => import("../components/ArcGisSpcOutlookMap"), {
-	ssr: false,
-});
+const SpcOutlookMap = dynamic(
+	() => import("../components/SpcConvectiveOutlookMap"),
+	{
+		ssr: false,
+	}
+);
+import { MAP_SERVICE } from "services/storm-prediction-center/";
 
 const HomeScreen = () => {
+	const eightDayOutlookObjARR = Object.keys(MAP_SERVICE).map(
+		(key) => MAP_SERVICE[key].sub_layers
+	);
+	console.log("8-DAY OUTLOOKS", eightDayOutlookObjARR);
+
 	return (
 		<Page>
 			<Hero />
-			<ArcGISMap />
+			{eightDayOutlookObjARR.map((outlookLayers) => (
+				<SpcOutlookMap day_layers={outlookLayers} />
+			))}
 			{/* {features &&
 				features.map((feature) => {
 					return <FeatureSection featureOBJ={feature} key={feature.name} />;
