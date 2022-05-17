4c9a20f2210093df60170e3b6721a8b1e0f5a512
Ephraim <admin@ephraimsmith.dev>
2022-05-17 06:59:02 -0500

    update todos

--- components/SpcConvectiveOutlookMap.jsx
+++ components/SpcConvectiveOutlookMap.jsx
@@ -1,45 +1,39 @@
 import React from "react";
 
-import { MAP_SERVICE } from "services/storm-prediction-center/";
+import { MAP_SERVICE as SPC } from "services/storm-prediction-center/";
 
 import esriConfig from "@arcgis/core/config";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import GroupLayer from "@arcgis/core/layers/GroupLayer";
+
+// TODO:
+// [ ] use Esri Leaflet plugin (lighter/use on smaller apps)
+// [ ] display outlook map for each day
+// [ ] fix zoom level
+// [ ] make layers transparent
+// [ ] map labels
 
 const SpcConvectiveOutlookMap = () => {
 	const mapRef = React.useRef(null);
-
-	const createFeatureLayerARR = (opacity, subLayersUrlOBJ) => {
-		const subLayersUrlArr = Object.values(subLayersUrlOBJ);
-		const featureLayerArr = subLayersUrlArr.map(
-			(url) => new FeatureLayer({ url, opacity })
-		);
-
-		return featureLayerArr;
-	};
+	let catLayer;
 
 	React.useEffect(() => {
 		if (mapRef.current) {
-			// ** create FeatureLayer array
-			const featureLayerARR = createFeatureLayerARR(
-				0.5,
-				Object.keys((key) => MAP_SERVICE[key].sub_layers)
-			);
+			catLayer = new FeatureLayer({
+				url: SPC.day1.sub_layers.categorical,
+				id: "catLayer",
+				opacity: 0.4,
+			});
 
-			// ** create GroupLayer
-			const groupLayer = new GroupLayer({ layers: featureLayerARR });
-
-			// ** create new Map
 			esriConfig.apiKey = process.env.NEXT_PUBLIC_ARCGIS_KEY;
 			const map = new Map({
 				basemap: "arcgis-navigation-night",
+				layers: [catLayer],
 			});
 
-			map.add(groupLayer);
-
-			// ** instantiate Map-rendering MapView
 			const mapView = new MapView({
 				map: map,
 				container: mapRef.current,
@@ -50,21 +44,10 @@ const SpcConvectiveOutlookMap = () => {
 	});
 
 	return (
-		<div className='h-96 bg-red-500'>
-			<h1>ArcGIS API for JavaScript Map</h1>
-			{/* <TabSelector /> */}
+		<div className='h-96'>
 			<div ref={mapRef} className='w-full h-96 bg-stone-400'></div>
 		</div>
 	);
 };
 
 export default SpcConvectiveOutlookMap;
-
-// TODO:
-// [x] display all 8 days of outlooks
-// [ ] give layers same transparency
-// [ ] disable zooming
-// [ ] access FeatureLayer data for labeling
-// [ ] add day selector
-// [ ] add FeatureLayer filtering
-// [ ] prevent uneccessary re-renders
