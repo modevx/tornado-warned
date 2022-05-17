43f616b46e63d2dbf654c5088377b10e1658c18b
Ephraim <admin@ephraimsmith.dev>
2022-05-17 18:39:31 -0500

    axe todo list

--- components/SpcConvectiveOutlookMap.jsx
+++ components/SpcConvectiveOutlookMap.jsx
@@ -9,39 +9,71 @@ import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import GroupLayer from "@arcgis/core/layers/GroupLayer";
 
-// TODO:
-// [ ] use Esri Leaflet plugin (lighter/use on smaller apps)
-// [ ] display outlook map for each day
-// [ ] fix zoom level
-// [ ] make layers transparent
-// [ ] map labels
-
 const SpcConvectiveOutlookMap = () => {
 	const mapRef = React.useRef(null);
 	let catLayer;
 
+	const testLayerFiltering = async () => {
+		const res = await fetch(
+			"https://mapservices.weather.noaa.gov/vector/rest/services/outlooks/SPC_wx_outlks/MapServer/layers?f=json"
+		);
+		const { layers } = await res.json();
+
+		// console.log("ALL LAYERS >>: ", layers);
+
+		const day_1_layers = await layers.filter(
+			(layer) => layer.name.includes("Day 1") && layer.type === "Feature Layer"
+		);
+		return day_1_layers;
+		// console.log("LAYERS >>: ", await day_1_layers);
+	};
+
+	async function loadPreBuiltMap(container, outlookLayers) {
+		const { buildMap } = await import("services/arcgis/map-builder");
+		return buildMap(container, outlookLayers);
+	}
+
 	React.useEffect(() => {
-		if (mapRef.current) {
-			catLayer = new FeatureLayer({
-				url: SPC.day1.sub_layers.categorical,
-				id: "catLayer",
-				opacity: 0.4,
-			});
+		let asyncCleanup;
 
-			esriConfig.apiKey = process.env.NEXT_PUBLIC_ARCGIS_KEY;
-			const map = new Map({
-				basemap: "arcgis-navigation-night",
-				layers: [catLayer],
-			});
+		const layers = async () => {
+			const outlookLayers = await testLayerFiltering();
 
-			const mapView = new MapView({
-				map: map,
-				container: mapRef.current,
-				center: [-97.29, 37.7],
-				zoom: 3,
-			});
-		}
-	});
+			if (mapRef.current && outlookLayers) {
+				asyncCleanup = loadPreBuiltMap(mapRef.current, outlookLayers);
+			}
+		};
+
+		layers();
+
+		return () => {
+			asyncCleanup && asyncCleanup.then((cleanup) => cleanup());
+		};
+	}, [mapRef]);
+
+	// React.useEffect(() => {
+	// 	if (mapRef.current) {
+	// 		catLayer = new FeatureLayer({
+	// 			url: SPC.day1.sub_layers.categorical,
+	// 			id: "catLayer",
+	// 			opacity: 0.4,
+	// 		});
+
+	// 		esriConfig.apiKey = process.env.NEXT_PUBLIC_ARCGIS_KEY;
+	// 		const map = new Map({
+	// 			basemap: "arcgis-navigation-night",
+	// 			layers: [catLayer],
+	// 		});
+
+	// 		const mapView = new MapView({
+	// 			map: map,
+	// 			container: mapRef.current,
+	// 			center: [-97.29, 37.7],
+	// 			zoom: 3,
+	// 			spatialReferenceLocked: true,
+	// 		});
+	// 	}
+	// });
 
 	return (
 		<div className='h-96'>
