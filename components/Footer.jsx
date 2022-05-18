08cabae636d8fd2f3a49ac8940b72dbcba619b0b
Ephraim <admin@ephraimsmith.dev>
2022-05-18 04:33:09 -0500

    update copyright

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ export const Footer = () => {
 	const year = new Date().getFullYear();
 
 	return (
-		<footer className='text-center'>
-			<p>&copy; {year} | tornadoaction.com | All Rights Reserved.</p>
+		<footer className='text-center text-xs mb-3'>
+			<p>&copy; {year} MODEVX, LLC. &nbsp; All Rights Reserved.</p>
 		</footer>
 	);
 };
