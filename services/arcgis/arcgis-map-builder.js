ca8d371330f38a5c0155d1f04e68c137672b1ecd
Ephraim <admin@ephraimsmith.dev>
2022-05-19 09:48:19 -0500

    added ArcGIS sample test url

--- services/arcgis/arcgis-map-builder.js
+++ services/arcgis/arcgis-map-builder.js
@@ -5,20 +5,23 @@ import Extent from "@arcgis/core/geometry/Extent";
 import Expand from "@arcgis/core/widgets/Expand";
 import config from "@arcgis/core/config";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import GroupLayer from "@arcgis/core/layers/GroupLayer";
 import LayerList from "@arcgis/core/widgets/LayerList"; // for filtering Layers
 import UniqueValueRenderer from "@arcgis/core/renderers/UniqueValueRenderer";
+import MapImageLayer from "@arcgis/core/layers/MapImageLayer";
 import { MAP_SERVICE as SPC } from "services/spc";
 
 config.apiKey = process.env.NEXT_PUBLIC_ARCGIS_KEY;
 
-// holds map values
 const app = {};
-// saves map Extent (x,y coords) anytime the map moves
-let mapExtentHandler;
-// SPC MapService base url
+let layer = {};
+let arcgisMap = {};
+let mapView = {};
+
 const BASE_URL =
 	"https://mapservices.weather.noaa.gov/vector/rest/services/outlooks/SPC_wx_outlks/MapServer";
-
+const TEST_URL =
+	"https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer";
 export const buildArcGISMap = async (container) => {
 	if (app.mapView) {
 		app.mapView.destroy();
@@ -42,51 +45,61 @@ export const buildArcGISMap = async (container) => {
 		},
 	};
 
-	const featureLayer = new FeatureLayer({
-		url: `${BASE_URL}`,
-		layerId: 1,
-		labelingInfo: [labelClass],
+	// ArcGIS MapImageLayer - Toggle Sublayer visibility Sample
+	layer = new MapImageLayer({
+		url: `${TEST_URL}`,
+		// sublayers: [
+		// 	{
+		// 		id: 0,
+		// 		visible: true,
+		// 	},
+		// 	{
+		// 		id: 1,
+		// 		visible: true,
+		// 	},
+		// 	{
+		// 		id: 2,
+		// 		visible: true,
+		// 	},
+		// 	{
+		// 		id: 3,
+		// 		visible: true,
+		// 	},
+		// ],
 	});
 
-	const arcgisMap = new Map({
+	// layer = new GroupLayer({
+	// 	url: `${BASE_URL}`,
+	// 	layerId: 0,
+	// 	subLayers: [{ id: 1 }],
+	// });
+
+	layer.when(() => console.log("ARCGIS MAP LAYER >> ", layer.toJSON()));
+	layer.when(() => console.log("ARCGIS MAP LAYER PROPERTIES >> ", layer.query));
+
+	arcgisMap = new Map({
 		basemap: "arcgis-dark-gray",
-		layers: featureLayer,
+		layers: layer,
 	});
 
-	const mapView = new MapView({
+	// arcgisMap.when(() => console.log("ARCGIS MAP >> ", arcgisMap.toJSON()));
+	// console.log("ARCGIS MAP >> ", arcgisMap);
+
+	mapView = new MapView({
 		map: arcgisMap,
 		center: [-98, 38],
 		container,
-		zoom: 3,
 	});
 
-	// sets the visible part of the map equal to the app coords (no animation)
-	// * for animated transition: use .goTo()
-	if (app.savedExtent) {
-		mapView.extent = Extent.fromJSON(app.savedExtent);
-	} else {
-		// if [app] doesn't have a saved Extent, set mapView to worldwide extent once the FeatureLayer loads
-		featureLayer.when(() => {
-			mapView.extent = featureLayer.fullExtent;
-		});
-	}
+	// mapView.when(() => console.log("ARCGIS MAP VIEW >> ", layer.toJSON()));
 
-	// save map coords [Extent] ANYTIME the map moves/changes
-	mapExtentHandler = watch(
-		() => mapView.stationary && mapView.extent,
-		() => {
-			app.savedExtent = mapView.extent.toJSON();
-		}
-	);
-
+	app.layer = layer;
 	app.map = arcgisMap;
-	app.featureLayer = featureLayer;
 	app.mapView = mapView;
 
 	return cleanup;
 };
 
 function cleanup() {
-	mapExtentHandler?.remove();
 	app.mapView?.destroy();
 }
