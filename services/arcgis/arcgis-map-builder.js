a6fc1e676771cd7afdf991cc823f25c6b421f760
Ephraim <admin@ephraimsmith.dev>
2022-05-21 15:49:55 -0500

    add WMSLayer

--- services/arcgis/arcgis-map-builder.js
+++ services/arcgis/arcgis-map-builder.js
@@ -1,68 +1,103 @@
 import { watch, when } from "@arcgis/core/core/reactiveUtils";
 import config from "@arcgis/core/config";
+import esriRequest from "@arcgis/core/request";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
+
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import GroupLayer from "@arcgis/core/layers/GroupLayer";
+import WMSLayer from "@arcgis/core/layers/WMSLayer";
+
 import LayerList from "@arcgis/core/widgets/LayerList";
-import { MAP_SERVICE as SPC } from "services/spc";
+import Legend from "@arcgis/core/widgets/Legend";
+// --
+import { MAP_SERVICE_URLS } from "services/spc";
 import {
 	disableViewNavigation,
 	setDefaultUiComponents,
 } from "services/arcgis/utils";
+
 config.apiKey = process.env.NEXT_PUBLIC_ARCGIS_KEY;
 
 const app = {};
-const labelClass = {};
+const labelClass = {
+	// autocasts as new LabelClass()
+	symbol: {
+		type: "text", // autocasts as new TextSymbol()
+		color: "green",
+		font: {
+			// autocast as new Font()
+			family: "Playfair Display",
+			size: 12,
+			weight: "bold",
+		},
+	},
+};
 
 export const buildArcGISMap = async (container) => {
 	if (app.mapView) {
 		app.mapView.destroy();
 	}
 
-	const layers = Object.values(SPC.day1.sub_layers).map(
-		(url) =>
-			new FeatureLayer({
-				url,
-				opacity: 0.2,
-				labelingInfo: [labelClass],
-			})
-	);
+	// all layers as JSON
+	// const spcGeoJSON = await esriRequest(MAP_SERVICE_URLS.layers, {
+	// 	responseType: "json",
+	// })
+	// 	.then((res) => {
+	// 		console.log("geoJSON RESPONSE >>\n", res.data);
+	// 	})
+	// 	.catch((err) => console.log("geoJSON FETCH ERROR >>\n", err));
 
-	const layer = new GroupLayer({});
-
-	layer.addMany(layers);
+	// call to WMS endpoint
+	const wmsLayer = new WMSLayer({
+		url: MAP_SERVICE_URLS.webmap_service,
+		opacity: 0.3,
+		// sublayers: [{}],
+	});
 
 	const map = new Map({
 		basemap: "arcgis-dark-gray",
-		layers: [layer],
+		// layers: [layer],
+		layers: [wmsLayer],
 	});
 
 	const view = new MapView({
 		map: map,
 		container,
 		extent: {
-			xmin: -128,
-			ymin: 18,
-			xmax: -60,
-			ymax: 64,
+			xmin: -109.03,
+			ymin: 19.75,
+			xmax: -79,
+			ymax: 62,
 			spatialReference: 4269,
 		},
 	});
 
-	app.layer = layer;
+	app.layer = wmsLayer;
 	app.map = map;
 	app.view = view;
 
-	app.view.when(() => {
+	app.view.when(async () => {
+		console.log(
+			"WMS LAYERS >>\n",
+			await app.layer.sublayers.toJSON()[0].toJSON()
+		);
+
+		app.view.extent = wmsLayer.fullExtent;
+
 		const layerList = new LayerList({
 			view,
+		});
+		const legend = new Legend({
+			view,
+			basemapLegendVisible: true,
 		});
 
 		disableViewNavigation(app.view);
 		setDefaultUiComponents(["attribution"], app.view);
 
-		view.ui.add(layerList, "top-left");
+		app.view.ui.add(layerList, "top-left");
+		// app.view.ui.add(legend, "bottom-left");
 	});
 
 	return cleanup;
