5bb9b8e5e88d43d45ab61617ade1d0bb261cfd8b
Ephraim <admin@ephraimsmith.dev>
2022-05-23 11:28:13 -0500

    add forwardRef components

--- components/ConvectiveOutlookMap.jsx
+++ components/ConvectiveOutlookMap.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export const ConvectiveOutlookMap = () => {
-	const [day, setDay] = React.useState(1);
+	const mapSelectorsRef = React.useRef(null);
 	const mapRef = React.useRef(null);
 
-	const handleCheckboxOnChange = (e) => {
+	const handleOutlookDaySelect = (e) => {
 		console.log("COMPONENT >>\n", e.target.value);
 	};
 
@@ -27,44 +27,40 @@ export const ConvectiveOutlookMap = () => {
 
 	return (
 		<div className='h-96 relative'>
-			<div
-				id='arcgis-map'
-				ref={mapRef}
-				className='w-screen h-[50vh] bg-stone-400 '
-			></div>
-			<div id='outlook-radio-btns' className='absolute top-0 left-0'>
-				<label>
-					<input
-						type='radio'
-						id='day-1'
-						name='outlook-day'
-						value='1'
-						defaultChecked
-						onChange={handleCheckboxOnChange}
-					/>
-					1
-				</label>
-				<label>
-					<input
-						type='radio'
-						id='day-2'
-						name='outlook-day'
-						value='2'
-						onChange={handleCheckboxOnChange}
-					/>
-					2
-				</label>
-				<label>
-					<input
-						type='radio'
-						id='day-3'
-						name='outlook-day'
-						value='3'
-						onChange={handleCheckboxOnChange}
-					/>
-					3
-				</label>
-			</div>
+			{/* <OutlookMapSelectors ref={mapSelectorsRef} /> */}
+			<ArcGISMap ref={mapRef} />
 		</div>
 	);
 };
+
+const ArcGISMap = React.forwardRef(function (props, ref) {
+	return (
+		<div
+			id='arcgis-map'
+			ref={ref}
+			className='w-screen h-[50vh] bg-stone-400 '
+		></div>
+	);
+});
+
+const OutlookMapSelectors = React.forwardRef(function (props, ref) {
+	return (
+		<div ref={ref}>
+			<h2>Categorical Outlooks</h2>
+			<div className='flex space-x-3'>
+				<div
+					className='border-2 border-white hover:bg-red-500/60 active:bg-red-500 px-4 py-2'
+					data-layer-id='1'
+				>
+					Day 1
+				</div>
+				<div
+					className='border-2 border-white hover:bg-red-500/60 active:bg-red-500 px-4 py-2'
+					data-layer-id='9'
+				>
+					Day 2
+				</div>
+			</div>
+		</div>
+	);
+});
