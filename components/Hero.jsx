18870f674b459b5535a9182e5146d4eeca8dc2b4
Ephraim <admin@ephraimsmith.dev>
2022-05-23 21:19:16 -0500

    strip down Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,53 +1,21 @@
 import Link from "next/link";
+import { FaChevronDown } from "react-icons/fa";
 
 export const Hero = () => {
 	return (
-		<div className='max-w-7xl mx-auto bg-gradient-to-r from-neutral-900/80 to-neutral-900/50'>
-			<div className='relative z-10 pb-8 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32'>
-				{/* ** HERO: responsive-mobile-nav */}
+		<div className='h-[50vh] flex flex-col items-center sm:text-center lg:text-left  border-b-2 border-red-500'>
+			<div className='grow flex flex-col justify-center'>
+				<h1 className='bg-gradient-to-br from-stone-500 to-red-500 text-transparent bg-clip-text uppercase inline-block text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
+					Tornado Warned
+				</h1>
 
-				{/* ** HERO: call-to-action */}
-				<main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
-					<div className='sm:text-center lg:text-left'>
-						{/* HERO: tag headline */}
-						<h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
-							<span className='block xl:inline text-neutral-200'>
-								Tornado Action
-							</span>{" "}
-							<span className='block xl:inline text-red-600'>
-								alerts. outlooks.
-							</span>
-						</h1>
-						{/* HERO: description */}
-						<p className='mt-3 text-base text-neutral-200 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0'>
-							View real-time tornado alerts and outlooks as they come in from
-							the National Weather Service and the Storm Prediction Center.
-						</p>
-						{/* HERO Buttons */}
-						<div className='mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start'>
-							<Link href='/alerts'>
-								<div className='rounded-md shadow'>
-									<a
-										href='#'
-										className='w-full flex items-center justify-center px-8 py-3 shadow border-2 border-red-600 text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 hover:border-red-700 md:py-4 md:text-lg md:px-10'
-									>
-										Check Alerts
-									</a>
-								</div>
-							</Link>
-							<Link href='/outlooks'>
-								<div className='mt-3 sm:mt-0 sm:ml-3'>
-									<a
-										href='\outlooks'
-										className='w-full flex items-center justify-center px-8 py-3 border-2 border-red-600 text-base font-medium rounded-md text-neutral-200 hover:bg-red-700 hover:border-red-700 md:py-4 md:text-lg md:px-10'
-									>
-										View Outlooks
-									</a>
-								</div>
-							</Link>
-						</div>
-					</div>
-				</main>
+				<p className='text-neutral-200 text-4xl'>stay ahead of the storm.</p>
+			</div>
+			<div className='mb-5'>
+				<div className='flex flex-col items-center justify-center mt-6'>
+					<FaChevronDown size={50} />
+					<FaChevronDown size={30} />
+				</div>
 			</div>
 		</div>
 	);
