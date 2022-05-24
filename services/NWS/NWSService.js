6b48389617058980abb8ad7df432fcaf4f1be468
Ephraim <admin@ephraimsmith.dev>
2022-05-24 15:05:14 -0500

    overhaul NWSService

--- services/NWS/NWSService.js
+++ services/NWS/NWSService.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const ENDPOINTS = Object.freeze({
-	active_alerts: {
-		count: `/alerts/active/count`,
-		tornadoes: `/alerts/active?event=Tornado%20Warning%2CTornado%20Watch&message_type=alert`,
-	},
-	alert_types: "/alerts/types",
-	glossary: "/glossary",
-	products: "/products",
-	radar: {
-		servers: `/radar/servers`,
-		stations: `/radar/stations`,
-	},
-	zones: "/zones",
+const BASE_URL = "https://api.weather.gov";
+
+const ENDPOINT = Object.freeze({
+	alert_count: `$${BASE_URL}/alerts/active/count`,
+	alert_tornadoes: `$${BASE_URL}/alerts/active?event=Tornado%20Warning%2CTornado%20Watch&message_type=alert`,
+	alert_types: `${BASE_URL}/alerts/types`,
+	glossary: `${BASE_URL}/glossary`,
+	products: `${BASE_URL}/products`,
+	radar_servers: `${BASE_URL}/radar/servers`,
+	radar_stations: `${BASE_URL}/radar/stations`,
+	zones: `${BASE_URL}/zones`,
 });
 
-const NWSClient = axios.create({
-	baseURL: "https://api.weather.gov",
-	timeout: 5000,
+const DEFAULT_TIMEOUT = 5000;
+
+const CLIENT = axios.create({
+	baseURL: BASE_URL,
+	timeout: DEFAULT_TIMEOUT,
 });
 
 export const getActiveTornadoAlerts = async () => {
-	const nwsResponse = NWSClient.get(ENDPOINTS.active_alerts.tornadoes);
-	return await nwsResponse.data.features;
+	const res = CLIENT.get(ENDPOINT.alert_tornadoes);
+	return await res.data.features;
 };
--- services/spc/SPCService.js
+++ services/spc/SPCService.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const BASE_URL = {
 	app_api: "/api",
 	map_serv:
