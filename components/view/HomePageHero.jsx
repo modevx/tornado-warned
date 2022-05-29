af933fe2acc14cc83ef084fe9d40038cafca5cc0
Ephraim <admin@ephraimsmith.dev>
2022-05-29 13:33:40 -0500

    add esri assets

--- components/view/HomePageHero.jsx
+++ components/view/HomePageHero.jsx
@@ -6,7 +6,7 @@ export const HomePageHero = () => {
 		<div className='h-[50vh] flex flex-col items-center sm:text-center lg:text-left  border-b-2 border-red-500'>
 			<div className='grow flex flex-col justify-center'>
 				<h1 className='bg-gradient-to-br from-stone-500 to-red-500 text-transparent bg-clip-text uppercase inline-block text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
-					Tornado Warned
+					Tornado Action
 				</h1>
 
 				<p className='text-neutral-200 text-4xl'>stay ahead of the storm.</p>
