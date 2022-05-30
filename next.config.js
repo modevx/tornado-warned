98af925e62acc6e1037842cc29a3d535fdff71dd
Ephraim <admin@ephraimsmith.dev>
2022-05-30 12:57:53 -0500

    display SPC outlook using Image src url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 
 module.exports = {
 	reactStrictMode: true,
+	images: {
+		domains: ["www.spc.noaa.gov"],
+	},
 	// async rewrites() {
 	// 	source: '',
 	// 	destination: '',
--- pages/outlooks.jsx
+++ pages/outlooks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import esriConfig from "@arcgis/core/config";
 import { PageWrapper } from "components/common";
 import { SPCOutlookMap } from "components/feature";
@@ -15,37 +16,37 @@ export async function getStaticProps() {
 }
 
 const OutlooksPage = ({ spcOutlooks }) => {
-	const [outlooks, setOutlooks] = React.useState([]);
-	const [filteredLayers, setFilteredLayers] = React.useState([]);
+	// const [outlooks, setOutlooks] = React.useState([]);
+	// const [filteredLayers, setFilteredLayers] = React.useState([]);
 
-	React.useEffect(() => {
-		const convective = spcOutlooks.filter((layer) => layer.subLayerIds);
+	// React.useEffect(() => {
+	// 	const convective = spcOutlooks.filter((layer) => layer.subLayerIds);
 
-		const categorical = spcOutlooks.filter((layer) =>
-			layer.name.toLowerCase().includes("categorical")
-		);
-		const prob4Day = spcOutlooks.filter((layer) => layer.parentLayerId == 20);
+	// 	const categorical = spcOutlooks.filter((layer) =>
+	// 		layer.name.toLowerCase().includes("categorical")
+	// 	);
+	// 	const prob4Day = spcOutlooks.filter((layer) => layer.parentLayerId == 20);
 
-		const tornado = spcOutlooks.filter((layer) =>
-			layer.name.toLowerCase().includes("tornado")
-		);
-		const hail = spcOutlooks.filter((layer) =>
-			layer.name.toLowerCase().includes("hail")
-		);
-		const wind = spcOutlooks.filter((layer) =>
-			layer.name.toLowerCase().includes("wind")
-		);
+	// 	const tornado = spcOutlooks.filter((layer) =>
+	// 		layer.name.toLowerCase().includes("tornado")
+	// 	);
+	// 	const hail = spcOutlooks.filter((layer) =>
+	// 		layer.name.toLowerCase().includes("hail")
+	// 	);
+	// 	const wind = spcOutlooks.filter((layer) =>
+	// 		layer.name.toLowerCase().includes("wind")
+	// 	);
 
-		setOutlooks(categorical);
-		setFilteredLayers(prob4Day);
-		// setFilteredLayers(tornado);
-		// setFilteredLayers(hail);
-		// setFilteredLayers(wind);
-	}, [spcOutlooks]);
+	// 	setOutlooks(categorical);
+	// 	setFilteredLayers(prob4Day);
+	// 	// setFilteredLayers(tornado);
+	// 	// setFilteredLayers(hail);
+	// 	// setFilteredLayers(wind);
+	// }, [spcOutlooks]);
 
-	if (outlooks.length) console.log("CONVECTIVE OUTLOOKS >>\n", outlooks);
-	if (filteredLayers.length)
-		console.log("4-DAY PROBABILISTIC >>\n", filteredLayers);
+	// if (outlooks.length) console.log("CONVECTIVE OUTLOOKS >>\n", outlooks);
+	// if (filteredLayers.length)
+	// 	console.log("4-DAY PROBABILISTIC >>\n", filteredLayers);
 
 	return (
 		<PageWrapper>
@@ -53,14 +54,20 @@ const OutlooksPage = ({ spcOutlooks }) => {
 				<h1 className='uppercase font-bold text-4xl'>
 					Convective Outlooks Page
 				</h1>
-				{outlooks &&
+				<Image
+					alt='Day 1 Outlook Image'
+					src='http://www.spc.noaa.gov/products/outlook/day1otlk.gif'
+					height={555}
+					width={815}
+				/>
+				{/* {outlooks &&
 					outlooks.map((layer) => (
 						<SPCOutlookMap layer={layer} key={layer.name} />
 					))}
 				{filteredLayers &&
 					filteredLayers.map((layer) => (
 						<SPCOutlookMap layer={layer} key={layer.name} />
-					))}
+					))} */}
 			</div>
 		</PageWrapper>
 	);
