151e673fb97468f3569c3a854477a1803b94cb1c
Ephraim <admin@ephraimsmith.dev>
2022-05-30 18:21:57 -0500

    troubleshooting unfound nws modules: change folder names

--- react_hooks/usePublicInfoStatements.js
+++ react_hooks/usePublicInfoStatements.js
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { fetchPublicInfoStatements } from "services/nws";
+import { fetchPublicInfoStatements } from "services/national-ws";
 
 export const usePublicInfoStatementQuery = () => {
 	return useQuery(["nws-pub-info-stmt"], () => fetchPublicInfoStatements());
