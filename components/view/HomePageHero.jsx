e97ba5cfa690dd83486e4965158b7a5f6a257858
Ephraim <admin@ephraimsmith.dev>
2022-05-30 14:16:33 -0500

    relo alerts to homepage for now

--- components/view/HomePageHero.jsx
+++ components/view/HomePageHero.jsx
@@ -3,13 +3,12 @@ import { FaChevronDown } from "react-icons/fa";
 
 export const HomePageHero = () => {
 	return (
-		<div className='h-[50vh] flex flex-col items-center sm:text-center lg:text-left  border-b-2 border-red-500'>
+		<div className='h-[50vh] flex flex-col items-center sm:text-center lg:text-left border-b-2 border-red-500 px-4'>
 			<div className='grow flex flex-col justify-center'>
-				<h1 className='bg-gradient-to-br from-stone-500 to-red-500 text-transparent bg-clip-text uppercase inline-block text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
-					Tornado Action
+				<h1 className='uppercase inline-block text-3xl tracking-tight font-extrabold sm:text-5xl md:text-6xl'>
+					Tornado <span className='text-red-500'>Action</span>
 				</h1>
-
-				<p className='text-neutral-200 text-4xl'>stay ahead of the storm.</p>
+				<p className='text-neutral-200'>stay ahead of the storm.</p>
 			</div>
 			<div className='mb-5'></div>
 		</div>
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
-import dynamic from "next/dynamic";
-import { useQuery } from "react-query";
-import { app_features } from "site_data";
 import { PageWrapper } from "components/common";
-import { SPCOutlookMap } from "components/feature";
-import {
-	HomePageFeatureSection,
-	HomePageHero,
-	AlertsPageTornadoAlertSection,
-} from "components/view";
+import { AlertsPageTornadoAlertSection } from "components/view";
 
 import { Clock } from "components/common";
 
 const HomeScreen = () => {
 	return (
 		<PageWrapper>
-			<HomePageHero />
-			{app_features &&
-				app_features.map((feature) => {
-					return (
-						<HomePageFeatureSection featureOBJ={feature} key={feature.name} />
-					);
-				})}
+			<AlertsPageTornadoAlertSection />
 		</PageWrapper>
 	);
 };
