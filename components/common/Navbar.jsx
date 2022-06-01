1646c5b7e103e739e259e90e00e41375f6df9de7
Ephraim <admin@ephraimsmith.dev>
2022-05-31 19:36:35 -0500

    add responsive navbar text

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -2,31 +2,29 @@ import Link from "next/link";
 import { RiTornadoLine } from "react-icons/ri";
 
 const NAV_ITEMS = [
-	{ name: "alerts", href: "/" },
-	{ name: "outlooks", href: "/outlooks" },
+  { name: "alerts", href: "/" },
+  { name: "outlooks", href: "/outlooks" },
 ];
 
 export const Navbar = () => {
-	return (
-		<nav className='flex items-center justify-between container mx-auto'>
-			<Link href='/' passHref>
-				<a className='flex items-center'>
-					<RiTornadoLine color='red' className='h-9 w-auto mr-2' />
-					<div className='flex flex-col leading-4 text-right italic'>
-						<span className='inline-block'>TORNADO</span>
-						<span className='inline-block text-red-500 text-xs'>
-							&nbsp;ACTION
-						</span>
-					</div>
-				</a>
-			</Link>
-			<div className='space-x-4'>
-				{NAV_ITEMS.map((navItem) => (
-					<Link key={navItem.name} href={navItem.href}>
-						<a className='text-stone-200 hover:text-red-500'>{navItem.name}</a>
-					</Link>
-				))}
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="flex items-center justify-between container mx-auto">
+      <Link href="/" passHref>
+        <a className="flex items-center">
+          <RiTornadoLine color="red" className="h-9 w-auto mr-2 md:h-16" />
+          <div className="flex flex-col leading-4 text-right italic md:text-2xl md:flex-row">
+            <span className="inline-block">TORNADO</span>
+            <span className="inline-block text-red-500">&nbsp;ACTION</span>
+          </div>
+        </a>
+      </Link>
+      <div className="space-x-4 md:text-2xl">
+        {NAV_ITEMS.map((navItem) => (
+          <Link key={navItem.name} href={navItem.href}>
+            <a className="text-stone-200 hover:text-red-500">{navItem.name}</a>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
 };
