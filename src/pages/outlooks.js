a37e17190c929eb561cab36736ffbdcb728c644c
Eph <ephraim@modevx.com>
2022-08-24 03:35:47 -0500

    FIX: temp hardcode convective outlook img src urls

--- src/pages/outlooks.js
+++ src/pages/outlooks.js
@@ -1,16 +1,18 @@
 import Image from "next/image";
 import { Page } from "../layout";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const OutlooksPage = () => {
-	const OUTLOOKS = [
-		{ day: "1", src: "http://www.spc.noaa.gov/products/outlook/day1otlk.gif" },
-		{ day: "2", src: "http://www.spc.noaa.gov/products/outlook/day2otlk.gif" },
-		{ day: "3", src: "http://www.spc.noaa.gov/products/outlook/day3otlk.gif" },
-		{
-			day: "4-8",
-			src: "http://www.spc.noaa.gov/products/outlook/day48prob.gif",
-		},
-	];
+	// const OUTLOOKS = [
+	// 	{ day: "1", src: "http://www.spc.noaa.gov/products/outlook/day1otlk.gif" },
+	// 	{ day: "2", src: "http://www.spc.noaa.gov/products/outlook/day2otlk.gif" },
+	// 	{ day: "3", src: "http://www.spc.noaa.gov/products/outlook/day3otlk.gif" },
+	// 	{
+	// 		day: "4-8",
+	// 		src: "http://www.spc.noaa.gov/products/outlook/day48prob.gif",
+	// 	},
+	// ];
 
 	return (
 		<Page>
@@ -19,7 +21,7 @@ const OutlooksPage = () => {
 					Convective Outlooks
 				</h1>
 
-				{OUTLOOKS.map((outlook) => (
+				{/* {OUTLOOKS.map((outlook) => (
 					<div key={outlook.day}>
 						<h2 className='mb-2'>Day {outlook.day} Outlook</h2>
 						<Image
@@ -30,7 +32,44 @@ const OutlooksPage = () => {
 							width={815}
 						/>
 					</div>
-				))}
+				))} */}
+
+				<div>
+					<h2 className='mb-2'>Day 1 Convective Outlook</h2>
+					<Image
+						alt='Day 1 Outlook Image'
+						src='http://www.spc.noaa.gov/products/outlook/day1otlk.gif'
+						height={555}
+						width={815}
+					/>
+				</div>
+				<div>
+					<h2 className='mb-2'>Day 2 Convective Outlook</h2>
+					<Image
+						alt='Day 2 Outlook Image'
+						src='http://www.spc.noaa.gov/products/outlook/day2otlk.gif'
+						height={555}
+						width={815}
+					/>
+				</div>
+				<div>
+					<h2 className='mb-2'>Day 3 Convective Outlook</h2>
+					<Image
+						alt='Day 3 Outlook Image'
+						src='http://www.spc.noaa.gov/products/outlook/day3otlk.gif'
+						height={555}
+						width={815}
+					/>
+				</div>
+				<div>
+					<h2 className='mb-2'>Day 4 Convective Outlook</h2>
+					<Image
+						alt='Day 4 Outlook Image'
+						src='http://www.spc.noaa.gov/products/outlook/day48prob.gif'
+						height={555}
+						width={815}
+					/>
+				</div>
 			</div>
 		</Page>
 	);
