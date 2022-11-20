6cf8536bc0a4d91e7d924bd6d13095e47b5eb6c0
Ephraim <ephraim@modevx.com>
2022-11-20 01:48:52 -0600

    update local storage error message

--- src/constants/site-data.js
+++ src/constants/site-data.js
@@ -1,8 +1,8 @@
 import {
-	GiAwareness,
-	GiDamagedHouse,
-	GiDeathSkull,
-	GiModernCity,
+  GiAwareness,
+  GiDamagedHouse,
+  GiDeathSkull,
+  GiModernCity,
 } from "react-icons/gi";
 import { BiMapAlt, BiTargetLock } from "react-icons/bi";
 import { RiNumbersLine, RiTornadoLine } from "react-icons/ri";
@@ -10,113 +10,112 @@ import { TiWeatherStormy } from "react-icons/ti";
 import { ImWarning } from "react-icons/im";
 
 export const FEATURES = [
-	{
-		name: "Tornado Alerts",
-		tagline:
-			"Tornado warnings and watches straight from the National Weather Service.",
-		benefits: [
-			{
-				name: "Particulary Dangerous Situation",
-				description:
-					"Visually confirmed, life-threatening tornado on the ground.  Get to a basement or storm shelter NOW.  Your life depends on it.",
-				icon: GiDeathSkull,
-			},
-			{
-				name: "Tornado Warning",
-				description:
-					"Visually or radar-confirmed storm rotation.  Get to the center-most room on the lowest floor A.S.A.P.",
-				icon: RiTornadoLine,
-			},
-			{
-				name: "Tornado Watch",
-				description:
-					'Conditions are right to produce a tornado.  Double check your safety list and "keep your head on a swivel."',
-				icon: GiAwareness,
-			},
-		],
-	},
-	{
-		name: "Severe Weather Outlooks",
-		tagline:
-			"Never get caught off-guard.  Be proactive and stay aware of building storm systems.",
-		benefits: [
-			{
-				name: "Severe Thunderstorm Outlooks",
-				description:
-					"Maps displaying areas at increased risk of severe weather over the next 8 days.",
-				icon: TiWeatherStormy,
-			},
-			{
-				name: "Elevated Risk Levels",
-				description: "Specific risk probablities for high-risk locations.",
-				icon: ImWarning,
-			},
-			{
-				name: "Major Cities",
-				description:
-					"Largely populated areas likely to be impacted by severe weather.",
-				icon: GiModernCity,
-			},
-		],
-	},
+  {
+    name: "Tornado Alerts",
+    tagline:
+      "Tornado warnings and watches straight from the National Weather Service.",
+    benefits: [
+      {
+        name: "Particulary Dangerous Situation",
+        description:
+          "Visually confirmed, life-threatening tornado on the ground.  Get to a basement or storm shelter NOW.  Your life depends on it.",
+        icon: GiDeathSkull,
+      },
+      {
+        name: "Tornado Warning",
+        description:
+          "Visually or radar-confirmed storm rotation.  Get to the center-most room on the lowest floor A.S.A.P.",
+        icon: RiTornadoLine,
+      },
+      {
+        name: "Tornado Watch",
+        description:
+          'Conditions are right to produce a tornado.  Double check your safety list and "keep your head on a swivel."',
+        icon: GiAwareness,
+      },
+    ],
+  },
+  {
+    name: "Severe Weather Outlooks",
+    tagline:
+      "Never get caught off-guard.  Be proactive and stay aware of building storm systems.",
+    benefits: [
+      {
+        name: "Severe Thunderstorm Outlooks",
+        description:
+          "Maps displaying areas at increased risk of severe weather over the next 8 days.",
+        icon: TiWeatherStormy,
+      },
+      {
+        name: "Elevated Risk Levels",
+        description: "Specific risk probablities for high-risk locations.",
+        icon: ImWarning,
+      },
+      {
+        name: "Major Cities",
+        description:
+          "Largely populated areas likely to be impacted by severe weather.",
+        icon: GiModernCity,
+      },
+    ],
+  },
 ];
 
 export const NAV_ITEMS = [
-	{ name: "alerts", href: "/" },
-	{ name: "outlooks", href: "/convective-outlooks" },
-	// { name: "live", href: "/dashboard-live" },
-	// { name: "stats", href: "/dashboard-stats" },
+  { name: "alerts", href: "/" },
+  { name: "outlooks", href: "/convective-outlooks" },
+  { name: "stats", href: "/dashboard-stats" },
 ];
 
 export const STATE_NAMES_ABBR = {
-	AL: "Alabama",
-	AK: "Alaska",
-	AZ: "Arizona",
-	AR: "Arkansas",
-	CA: "California",
-	CO: "Colorado",
-	CT: "Connecticut",
-	DE: "Delaware",
-	FL: "Florida",
-	GA: "Georgia",
-	HI: "Hawaii",
-	ID: "Idaho",
-	IL: "Illinois",
-	IN: "Indiana",
-	IA: "Iowa",
-	KS: "Kansas",
-	KY: "Kentucky",
-	LA: "Louisiana",
-	ME: "Maine",
-	MD: "Maryland",
-	MA: "Massachusetts",
-	MI: "Michigan",
-	MN: "Minnesota",
-	MS: "Mississippi",
-	MO: "Missouri",
-	MT: "Montana",
-	NE: "Nebraska",
-	NV: "Nevada",
-	NH: "New Hampshire",
-	NJ: "New Jersey",
-	NM: "New Mexico",
-	NY: "New York",
-	NC: "North Carolina",
-	ND: "North Dakota",
-	OH: "Ohio",
-	OK: "Oklahoma",
-	OR: "Oregon",
-	PA: "Pennsylvania",
-	RI: "Rhode Island",
-	SC: "South Carolina",
-	SD: "South Dakota",
-	TN: "Tennessee",
-	TX: "Texas",
-	UT: "Utah",
-	VT: "Vermont",
-	VA: "Virginia",
-	WA: "Washington",
-	WV: "West Virginia",
-	WI: "Wisconsin",
-	WY: "Wyoming",
+  AL: "Alabama",
+  AK: "Alaska",
+  AZ: "Arizona",
+  AR: "Arkansas",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DE: "Delaware",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  IA: "Iowa",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  ME: "Maine",
+  MD: "Maryland",
+  MA: "Massachusetts",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MS: "Mississippi",
+  MO: "Missouri",
+  MT: "Montana",
+  NE: "Nebraska",
+  NV: "Nevada",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NY: "New York",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VT: "Vermont",
+  VA: "Virginia",
+  WA: "Washington",
+  WV: "West Virginia",
+  WI: "Wisconsin",
+  WY: "Wyoming",
 };
--- src/services/local-session-storage-service.js
+++ src/services/local-session-storage-service.js
@@ -2,12 +2,12 @@ import React from "react";
 
 // -- CONSTANTS
 const TYPES = {
-	GET_LOCAL_STORAGE: "get_ls",
-	GET_SESSION_STORAGE: "get_ss",
-	SET_LOCAL_STORAGE: "set_ls",
-	SET_SESSION_STORAGE: "set_ss",
-	REMOVE_FROM_LOCAL_STORAGE: "remove_ls",
-	REMOVE_FROM_SESSION_STORAGE: "remove_ss",
+  GET_LOCAL_STORAGE: "get_ls",
+  GET_SESSION_STORAGE: "get_ss",
+  SET_LOCAL_STORAGE: "set_ls",
+  SET_SESSION_STORAGE: "set_ss",
+  REMOVE_FROM_LOCAL_STORAGE: "remove_ls",
+  REMOVE_FROM_SESSION_STORAGE: "remove_ss",
 };
 
 // -- CONTEXTS
@@ -16,56 +16,58 @@ const DispatchContext = React.createContext();
 
 // -- ACTIONS MAP (reducer)
 const reducer = (state, action) => {
-	switch (action.type) {
-		case TYPES.GET_LOCAL_STORAGE: {
-			return localStorage.getItem(action.payload);
-		}
-		case TYPES.GET_SESSION_STORAGE: {
-			return sessionStorage.getItem(action.payload);
-		}
-		case TYPES.SET_LOCAL_STORAGE: {
-			return localStorage.setItem(action.payload);
-		}
-		case TYPES.SET_SESSION_STORAGE: {
-			return sessionStorage.setItem(action.payload);
-		}
-		case TYPES.REMOVE_LOCAL_STORAGE: {
-			return localStorage.removeItem(action.payload);
-		}
-		case TYPES.REMOVE_SESSION_STORAGE: {
-			return sessionStorage.removeItem(action.payload);
-		}
-		default: {
-			throw new Error("Invalid action type.");
-		}
-	}
+  switch (action.type) {
+    case TYPES.GET_LOCAL_STORAGE: {
+      return localStorage.getItem(action.payload);
+    }
+    case TYPES.GET_SESSION_STORAGE: {
+      return sessionStorage.getItem(action.payload);
+    }
+    case TYPES.SET_LOCAL_STORAGE: {
+      return localStorage.setItem(action.payload);
+    }
+    case TYPES.SET_SESSION_STORAGE: {
+      return sessionStorage.setItem(action.payload);
+    }
+    case TYPES.REMOVE_LOCAL_STORAGE: {
+      return localStorage.removeItem(action.payload);
+    }
+    case TYPES.REMOVE_SESSION_STORAGE: {
+      return sessionStorage.removeItem(action.payload);
+    }
+    default: {
+      throw new Error("Invalid action type.");
+    }
+  }
 };
 
 // -- SERVICE (Context) PROVIDER
 export const LocalSessionStorageProvider = ({ children }) => {
-	const [state, dispatch] = React.useReducer(reducer, {});
+  const [state, dispatch] = React.useReducer(reducer, {});
 
-	return (
-		<StateContext.Provider value={state}>
-			<DispatchContext.Provider value={dispatch}>
-				{children}
-			</DispatchContext.Provider>
-		</StateContext.Provider>
-	);
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  );
 };
 
 // -- USE HOOKS
 export const useLocalSessionStorageState = () => {
-	const stateContext = React.useContext(StateContext);
-	if (stateContext === undefined) {
-		throw new Error("context use attempted outside  state provider");
-	}
-	return stateContext;
+  const stateContext = React.useContext(StateContext);
+  if (stateContext === undefined) {
+    throw new Error("CONTEXT ERROR >> context must be used inside provider...");
+  }
+  return stateContext;
 };
 export const useLocalSessionStorageDispatch = () => {
-	const dispatchContext = React.useContext(DispatchContext);
-	if (dispatchContext === undefined) {
-		throw new Error("context use attempted outside  dispatch provider");
-	}
-	return dispatchContext;
+  const dispatchContext = React.useContext(DispatchContext);
+  if (dispatchContext === undefined) {
+    throw new Error(
+      "CONTEXT ERROR >> context must be used inside dispatch provider..."
+    );
+  }
+  return dispatchContext;
 };
