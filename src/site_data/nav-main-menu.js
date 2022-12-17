d9bdf813ecfe34623b102a0074315355b5731fdd
Eph <ephraim@modevx.com>
2022-12-16 21:23:52 -0600

    site data reorg

---
+++ src/site_data/nav-main-menu.js
@@ -0,0 +1,6 @@
+export const MAIN_NAV = [
+	// { name: "alerts", href: "/" },
+	// { name: "stats", href: "/dashboard-stats" },
+	// { name: "local storm reports", href: "/local-storm-reports" },
+	// { name: "radar", href: "/radar" },
+];
