114698668b21c10d60566ac1da810a76be34eb78
Ephraim <ephraim@modevx.com>
2023-01-22 23:07:41 -0600

    COMPONENTS/add React DaisyUI footer component

--- src/components/_shared/Footer.jsx
+++ src/components/_shared/Footer.jsx
@@ -1,9 +1,33 @@
-export const Footer = () => {
-	const year = new Date().getFullYear();
+import { Footer as DUIFooter } from "react-daisyui";
 
-	return (
-		<footer className='text-center text-xs mb-3'>
-			<p>&copy; {year} MODEVX, LLC. &nbsp; All Rights Reserved.</p>
-		</footer>
-	);
+export const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <DUIFooter className="p-10 bg-neutral text-neutral-content">
+      <div>
+        <DUIFooter.Title>National Weather Service</DUIFooter.Title>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </div>
+      <div>
+        <DUIFooter.Title>Storm Prediction Center</DUIFooter.Title>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </div>
+      <div>
+        <DUIFooter.Title>NOAA</DUIFooter.Title>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </div>
+      <p className="text-center text-xs mb-3">
+        &copy; {year} MODEVX, LLC. &nbsp; All Rights Reserved.
+      </p>
+    </DUIFooter>
+  );
 };
