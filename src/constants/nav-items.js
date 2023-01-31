da769baa02fc50b633aa6b347cb1e5f5160acc0b
Eph <ephraim@modevx.com>
2023-01-30 21:34:25 -0600

    add nav-items & urls constants files

---
+++ src/constants/nav-items.js
@@ -0,0 +1,37 @@
+export const FOOTER_NAV_ITEMS = {
+	national_weather_service: [
+		{
+			label: "AWIPS Basemaps",
+			href: "https://www.weather.gov/gis/AWIPSShapefiles",
+		},
+	],
+	storm_prediction_center: [{ label: "", href: "" }],
+	noaa: [{ label: "", href: "" }],
+};
+
+export const MAIN_NAV_ITEMS = {
+	// live_dashboard: {
+	// 	label: "dashboard",
+	// 	href: "/live-dashboard",
+	// },
+	convective_outlooks: {
+		label: "outlooks",
+		href: "/",
+	},
+	// alerts: {
+	// 	label: "alerts",
+	// 	href: "/active-alerts",
+	// },
+	// local_storm_reports: {
+	// 	label: "storm reports",
+	// 	href: "/local-storm-reports",
+	// },
+	// radar_signatures: {
+	//   label: "signatures",
+	//   href: "/nexrad-signatures",
+	// },
+	// latest_info: {
+	//   label: "latest info",
+	//   href: "/latest-info",
+	// },
+};
---
+++ src/constants/urls.js
@@ -0,0 +1,5 @@
+export const IMAGE_SERVER = Object.freeze({});
+
+export const MAP_SERVER = Object.freeze({});
+
+export const RSS_FEED = Object.freeze({});
