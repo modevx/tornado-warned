b01cb9a7bba1680d64e8ac3d9f9837e3d123baaf
Eph <ephraim@modevx.com>
2023-01-30 21:35:57 -0600

    update prop name

--- src/features/spc-convective-outlooks/components/outlook-svg-path-group.jsx
+++ src/features/spc-convective-outlooks/components/outlook-svg-path-group.jsx
@@ -5,27 +5,27 @@ import rewind from "@turf/rewind";
 const projection = geoAlbers();
 const pathGen = geoPath(projection);
 
-export const OutlookSVGPathGroup = ({ geometry }) => {
-  return geometry ? (
-    <g>
-      {geometry.features.map((feature, index) => {
-        const {
-          properties: { dn, valid, expire, idp_source },
-        } = feature;
+export const OutlookSVGPathGroup = ({ featuresARR }) => {
+	return featuresARR ? (
+		<g>
+			{featuresARR.map((feature, index) => {
+				const {
+					properties: { dn, valid, expire, idp_source },
+				} = feature;
 
-        const fLabel = OUTLOOK_CATEGORIES[dn]?.label;
+				const fLabel = OUTLOOK_CATEGORIES[dn]?.label;
 
-        return (
-          <path
-            d={pathGen(rewind(feature, { reverse: true }))}
-            key={`${idp_source}-${dn}`}
-            fill={OUTLOOK_CATEGORIES[dn]?.fill}
-            stroke={OUTLOOK_CATEGORIES[dn]?.stroke}
-            fillOpacity={0.5}
-            strokeWidth={2} // `${CONVECTIVE_OUTLOOK_CATEGORIES[dn]?.label}`
-          />
-        );
-      })}
-    </g>
-  ) : null;
+				return (
+					<path
+						d={pathGen(rewind(feature, { reverse: true }))}
+						key={`${idp_source}-${dn}`}
+						fill={OUTLOOK_CATEGORIES[dn]?.fill}
+						stroke={OUTLOOK_CATEGORIES[dn]?.stroke}
+						fillOpacity={0.5}
+						strokeWidth={2} // `${CONVECTIVE_OUTLOOK_CATEGORIES[dn]?.label}`
+					/>
+				);
+			})}
+		</g>
+	) : null;
 };
