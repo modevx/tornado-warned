66c8958e6a8d8f59093faea068f1eb020f556cbe
Eph <ephraim@modevx.com>
2023-01-30 21:35:43 -0600

    add properties to convective outlook preview card

--- src/features/spc-convective-outlooks/components/outlook-preview-card.jsx
+++ src/features/spc-convective-outlooks/components/outlook-preview-card.jsx
@@ -1,14 +1,35 @@
 import { Basemap } from "components/maps";
 import { OutlookSVGPathGroup } from "./outlook-svg-path-group";
 import { Card } from "react-daisyui";
+import dayjs from "dayjs";
+import LocalizedFormat from "dayjs/plugin/localizedFormat";
+dayjs.extend(LocalizedFormat);
 
-export const OutlookPreviewCard = ({ outlookDayGeoJson, day }) => (
-  <Card className="rounded-md my-4 bg-stone-800 cursor-pointer md:mx-4 hover:scale-105">
-    <Basemap>
-      <OutlookSVGPathGroup geometry={outlookDayGeoJson} />
-    </Basemap>
-    <Card.Body>
-      <Card.Title>{`Day ${day} Outlook`}</Card.Title>
-    </Card.Body>
-  </Card>
-);
+export const OutlookPreviewCard = ({ day, featuresARR }) => {
+	const {
+		properties: { valid, expire },
+	} = featuresARR[0];
+
+	console.log("OutlookPreviewCard\n", featuresARR[0]?.properties);
+
+	return (
+		<Card className='rounded-md my-4 bg-stone-800 cursor-pointer md:mx-4 hover:scale-105'>
+			<Basemap>
+				<OutlookSVGPathGroup featuresARR={featuresARR} />
+			</Basemap>
+			<Card.Body>
+				<Card.Title className='uppercase'>{`Day ${day} Outlook`}</Card.Title>
+				<div className='flex justify-between'>
+					<div className='flex flex-col'>
+						<span>FROM:</span>
+						<span>{dayjs(valid).format(`ddd MMM D, YYYY HHmm`)}</span>
+					</div>
+					<div className='flex flex-col'>
+						<span>TO:</span>
+						<span>{dayjs(expire).format(`ddd MMM D, YYYY HHmm`)}</span>
+					</div>
+				</div>
+			</Card.Body>
+		</Card>
+	);
+};
