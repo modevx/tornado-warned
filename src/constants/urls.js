9aaf8f2446c804244de1f88c70698c3217a7f708
Ephraim <ephraim@modevx.com>
2023-01-30 23:06:52 -0600

    start adding global constants

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,19 @@
+const BASE = Object.freeze({
+  NOAA: "https://www.ncdc.noaa.gov",
+  NWS: "https://www.weather.gov",
+  SPC: "http://www.spc.noaa.gov",
+});
+
 export const IMAGE_SERVER = Object.freeze({});
 
-export const MAP_SERVER = Object.freeze({});
+export const MAP_SERVER = Object.freeze({
+  SPC_OUTLOOKS: {
+    base: "https://mapservices.weather.noaa.gov/vector/rest/services/outlooks/SPC_wx_outlks/MapServer",
+  },
+});
 
-export const RSS_FEED = Object.freeze({});
+export const ENDPOINT = Object.freeze({
+  NOAA: {},
+  NWS: {},
+  SPC: {},
+});
