1b9ef9bfa6c95824c16f2f9e518556d09aeaf965
Eph <ephraim@modevx.com>
2023-02-02 00:21:18 -0600

    CONSTANTS/refactor urls into base urls & service urls only

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,49 +1,48 @@
 const BASE = Object.freeze({
-	APP_API: "/api",
 	NOAA: "https://www.ncdc.noaa.gov",
+	NOAA_MAP_SERV: "https://idpgis.ncep.noaa.gov/arcgis/rest/services",
 	NWS: "https://www.weather.gov",
+	NWS_MAP_SERV: "https://mapservices.weather.noaa.gov/vector/rest/services",
 	SPC: "http://www.spc.noaa.gov",
-	SPC_MAP_SERVICES: "https://mapservices.weather.noaa.gov/vector",
 });
 
+// https://idpgis.ncep.noaa.gov/arcgis/rest/services/radar/radar_base_reflectivity_time/ImageServer
+
 export const ENDPOINT = Object.freeze({
-	APP: { spc_rss_feeds: "spc-rss-feeds" },
-	NOAA: {},
-	NWS: {},
 	SPC: {
 		outlook_geometry: "query?&outFields=*&geometry=true&f=geojson",
-		outlooks_legend: "legend?f=pjson",
-		outlook_map_server: "rest/services/outlooks/SPC_wx_outlks/MapServer",
-		outlook_wms_server: "services/outlooks/SPC_wx_outlks/MapServer/WMSServer",
-		RSS: {
-			all: "products/spcrss.xml",
-			conv_otlks: "products/spcacrss.xml",
-			fire_forecasts: "products/spcfwrss.xml",
-			meso_disc: "products/spcmdrss.xml",
-			pds: "products/spcpdswwrss.xml",
-			sev_wx_multi_med_brfs: "products/spcmbrss.xml",
-			tor_sev_wx: "products/spcwwrss.xml",
-		},
 	},
 });
 
 export const APP = Object.freeze({
-	spc_rss_feeds: `${BASE.APP_API}/${ENDPOINT.APP.spc_rss_feeds}`,
+	spc_rss_feeds: "api/spc-rss-feeds",
 });
 
-export const NOAA = Object.freeze({});
+export const NOAA = Object.freeze({
+	DATA_ACCESS: `${BASE.NOAA}/access`,
+});
 
-export const NWS = Object.freeze({});
+export const NWS = Object.freeze({
+	API_WEB_SERVICE: `${BASE.NWS}/documentation/services-web-api`,
+	AWIPS_BASEMAPS: `${BASE.NWS}/gis/AWIPSShapefiles`,
+	GIS_CLOUD_WEB_SERVICES: `${BASE.NWS}/gis/cloudgiswebservices`,
+	IDPGIS_REST_META: `${BASE.NWS}/gis/IDP-GISRestMetadata`,
+	LOCAL_STORM_REPORT_MAP_SERV: `${BASE.NWS_MAP_SERV}/obs/nws_local_storm_reports/MapServer`,
+	RADAR_BASE_REFLECTIVITY_IMG_SERV: `${BASE.NOAA_MAP_SERV}/radar/radar_base_reflectivity_time/ImageServer`,
+	RADAR_BASE_REFLECTIVITY_MAP_SERV: `${BASE.NOAA_MAP_SERV}/NWS_Observations/radar_base_reflectivity/MapServer`,
+	REFERENCE_MAP_MAP_SERV: `${BASE.NOAA_MAP_SERV}/NWS/nws_reference_map/MapServer`,
+});
 
 export const SPC = Object.freeze({
-	outlooks_legend: `${BASE.SPC_MAP_SERVICES}/${ENDPOINT.SPC.outlook_map_server}/${ENDPOINT.SPC.outlooks_legend}`,
-	outlooks_map_server: `${BASE.SPC_MAP_SERVICES}/${ENDPOINT.SPC.outlook_map_server}`,
-	outlooks_wms_server: `${BASE.SPC_MAP_SERVICES}/${ENDPOINT.SPC.outlook_wms_server}`,
-	rss_all_products: `${BASE.SPC}/${ENDPOINT.SPC.RSS.all}`,
-	rss_convective_outlooks: `${BASE.SPC}/${ENDPOINT.SPC.RSS.conv_otlks}`,
-	rss_fire_forecasts: `${BASE.SPC}/${ENDPOINT.SPC.RSS.fire_forecasts}`,
-	rss_mesoscale_discussions: `${BASE.SPC}/${ENDPOINT.SPC.RSS.meso_disc}`,
-	rss_particularly_dangerous_situations: `${BASE.SPC}/${ENDPOINT.SPC.RSS.pds}`,
-	rss_severe_wx_outlook_multimedia_briefings: `${BASE.SPC}/${ENDPOINT.SPC.RSS.sev_wx_multi_med_brfs}`,
-	rss_tornado_severe_wx_watches: `${BASE.SPC}/${ENDPOINT.SPC.RSS.tor_sev_wx}`,
+	FORECAST_PRODUCTS: `${BASE.SPC}/products`,
+	OTLK_MAP_SERV: `${BASE.NWS_MAP_SERV}/outlooks/SPC_wx_outlks/MapServer`,
+	OTLK_MAP_SERV_LGND: `${BASE.NWS_MAP_SERV}/outlooks/SPC_wx_outlks/MapServer/legend?f=pjson`,
+	RSS_ALL: `${BASE.SPC}/products/spcrss.xml`,
+	RSS_CONV_OTLKS: `${BASE.SPC}/products/spcacrss.xml`,
+	RSS_FEED_DOCS: `${BASE.SPC}/aboutrss.html`,
+	RSS_FIRE_FC: `${BASE.SPC}/products/spcfwrss.xml`,
+	RSS_MESO_DISC: `${BASE.SPC}/products/spcmdrss.xml`,
+	RSS_PDS: `${BASE.SPC}/products/spcpdswwrss.xml`,
+	RSS_SEV_WX_OTLK_MM_BRF: `${BASE.SPC}/products/spcmbrss.xml`,
+	RSS_TOR_SEV_WX: `${BASE.SPC}/products/spcwwrss.xml`,
 });
