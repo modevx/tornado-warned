b417003dedac86b7730e8af0d394d40500279742
Ephraim <ephraim@modevx.com>
2023-02-03 16:29:11 -0600

    PROJECT/make file names more semantic

--- src/custom_hooks/useRssFeedQuery.js
+++ src/custom_hooks/useRssFeedQuery.js
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { getSpcRssFeed } from "services/spc-map-server";
+import { getSpcRssFeed } from "services/convective-outlook-map-server";
 
 export const useRssFeedQuery = (feedType) => {
-	return useQuery(["spc-rss-feeds", feedType], () => getSpcRssFeed(feedType));
+  return useQuery(["spc-rss-feeds", feedType], () => getSpcRssFeed(feedType));
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,51 +1,51 @@
 import React from "react";
 import { PageLayout } from "components";
 import {
-	ConvectiveOutlookMap,
-	OutlookPreviewCard,
+  ConvectiveOutlookMap,
+  OutlookPreviewCard,
 } from "features/spc-convective-outlooks/components";
 import {
-	useConvectiveOutlooksQuery,
-	useConvectiveOutlookLegendQuery,
-} from "services/spc-map-server";
+  useConvectiveOutlooksQuery,
+  useConvectiveOutlookLegendQuery,
+} from "services/convective-outlook-map-server";
 import { OUTLOOK_LAYERS } from "features/spc-convective-outlooks/constants";
 
 const OutlooksPage = () => {
-	const { data: outlooks, error: outlookError } = useConvectiveOutlooksQuery();
-	const { data: outlookLegendData, error: legendDataError } =
-		useConvectiveOutlookLegendQuery();
+  const { data: outlooks, error: outlookError } = useConvectiveOutlooksQuery();
+  const { data: outlookLegendData, error: legendDataError } =
+    useConvectiveOutlookLegendQuery();
 
-	React.useEffect(() => {
-		outlooks &&
-			console.log("useConvectiveOutlooksQuery {data: outlooks} >>\n", outlooks);
-	}, [outlooks]);
+  React.useEffect(() => {
+    outlooks &&
+      console.log("useConvectiveOutlooksQuery {data: outlooks} >>\n", outlooks);
+  }, [outlooks]);
 
-	React.useEffect(() => {
-		outlookLegendData &&
-			console.log(
-				"useConvectiveOutlookLegendQuery\n",
-				outlookLegendData?.data.layers
-			);
-	}, [outlookLegendData]);
+  React.useEffect(() => {
+    outlookLegendData &&
+      console.log(
+        "useConvectiveOutlookLegendQuery\n",
+        outlookLegendData?.data.layers
+      );
+  }, [outlookLegendData]);
 
-	return (
-		<PageLayout>
-			{/* <ConvectiveOutlookMap /> */}
-			<div className='md:grid md:grid-cols-2 lg:grid-cols-3'>
-				{outlooks
-					? outlooks.map((outlook, index) => {
-							return (
-								<OutlookPreviewCard
-									key={`outlook-${index}`}
-									day={index + 1}
-									featuresARR={outlook?.data.features}
-								/>
-							);
-					  })
-					: null}
-			</div>
-		</PageLayout>
-	);
+  return (
+    <PageLayout>
+      {/* <ConvectiveOutlookMap /> */}
+      <div className="md:grid md:grid-cols-2 lg:grid-cols-3">
+        {outlooks
+          ? outlooks.map((outlook, index) => {
+              return (
+                <OutlookPreviewCard
+                  key={`outlook-${index}`}
+                  day={index + 1}
+                  featuresARR={outlook?.data.features}
+                />
+              );
+            })
+          : null}
+      </div>
+    </PageLayout>
+  );
 };
 
 export default OutlooksPage;
