1e704f6f0048ac29d50a0d186c9db4b1f80de106
Ephraim <ephraim@modevx.com>
2023-02-09 22:20:28 -0600

    add outlook service query to fetch group layer json

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,60 +1,66 @@
 import React from "react";
 import { PageLayout } from "components";
 import {
-	ConvectiveOutlookMap,
-	OutlookPreviewCard,
+  ConvectiveOutlookMap,
+  OutlookPreviewCard,
 } from "features/convective-outlooks/components";
 import {
-	useAllConvectiveOutlookLayersQuery,
-	useConvectiveOutlooksQuery,
-	useConvectiveOutlookLegendQuery,
+  useAllConvectiveOutlookGroupLayersQuery,
+  useAllConvectiveOutlookFeatureLayersQuery,
+  useConvectiveOutlooksQuery,
+  useConvectiveOutlookLegendQuery,
 } from "services/convective-outlook-map-server";
 import { OUTLOOK_LAYERS } from "features/convective-outlooks/constants";
 
 const OutlooksPage = () => {
-	const { data: outlooks, error: outlookError } = useConvectiveOutlooksQuery();
-	const { data: allOutlookLayers, error: allOutlookLayersError } =
-		useAllConvectiveOutlookLayersQuery();
-	const { data: outlookLegendData, error: legendDataError } =
-		useConvectiveOutlookLegendQuery();
+  const { data: outlooks, error: outlookError } = useConvectiveOutlooksQuery();
+  const { data: featureLayers, error: featureLayersError } =
+    useAllConvectiveOutlookFeatureLayersQuery();
+  const { data: groupLayers, error: groupLayersError } =
+    useAllConvectiveOutlookGroupLayersQuery();
+  const { data: outlookLegendData, error: legendDataError } =
+    useConvectiveOutlookLegendQuery();
 
-	React.useEffect(() => {
-		outlooks && console.log("{data: outlooks} >>\n", outlooks);
-	}, [outlooks]);
+  // React.useEffect(() => {
+  //   outlooks && console.log("{data: outlooks} >>\n", outlooks);
+  // }, [outlooks]);
 
-	React.useEffect(() => {
-		allOutlookLayers &&
-			console.log("{data: allOutlookLayers} >>\n", allOutlookLayers);
-	}, [allOutlookLayers]);
+  React.useEffect(() => {
+    groupLayers && console.log("{data: groupLayers} >>\n", groupLayers);
+  }, [groupLayers]);
 
-	React.useEffect(() => {
-		outlookLegendData &&
-			console.log(
-				"{data: outlookLegendData} >>\n",
-				outlookLegendData?.data.layers
-			);
-	}, [outlookLegendData]);
+  // React.useEffect(() => {
+  //   featureLayers && console.log("{data: featureLayers} >>\n", featureLayers);
+  // }, [featureLayers]);
 
-	return (
-		<PageLayout>
-			{/* <ConvectiveOutlookMap /> */}
-			<div className='md:grid md:grid-cols-2 lg:grid-cols-3'>
-				{outlooks
-					? outlooks.map((outlook, index) => {
-							const featureCollection = outlook?.data.features;
+  // React.useEffect(() => {
+  //   outlookLegendData &&
+  //     console.log(
+  //       "{data: outlookLegendData} >>\n",
+  //       outlookLegendData?.data.layers
+  //     );
+  // }, [outlookLegendData]);
 
-							return (
-								<OutlookPreviewCard
-									key={`outlook-${index}`}
-									day={index + 1}
-									featuresCollection={featureCollection}
-								/>
-							);
-					  })
-					: null}
-			</div>
-		</PageLayout>
-	);
+  return (
+    <PageLayout>
+      {/* <ConvectiveOutlookMap /> */}
+      <div className="md:grid md:grid-cols-2 lg:grid-cols-3">
+        {outlooks
+          ? outlooks.map((outlook, index) => {
+              const featureCollection = outlook?.data.features;
+
+              return (
+                <OutlookPreviewCard
+                  key={`outlook-${index}`}
+                  day={index + 1}
+                  featuresCollection={featureCollection}
+                />
+              );
+            })
+          : null}
+      </div>
+    </PageLayout>
+  );
 };
 
 export default OutlooksPage;
--- src/services/convective-outlook-map-server.js
+++ src/services/convective-outlook-map-server.js
@@ -4,8 +4,8 @@ import { DEFAULT_TIMEOUT } from "constants/services";
 import { useQuery } from "@tanstack/react-query";
 
 const MAP_SERVER_CLIENT = axios.create({
-	baseURL: URLS.SPC.OTLK_MAP_SERV,
-	timeout: DEFAULT_TIMEOUT,
+  baseURL: URLS.SPC.OTLK_MAP_SERV,
+  timeout: DEFAULT_TIMEOUT,
 });
 
 // **********************************************
@@ -13,89 +13,98 @@ const MAP_SERVER_CLIENT = axios.create({
 // **********************************************
 
 const OUTLOOK_MAP_SERVER_LAYERS = Object.freeze({
-	GroupLayers: {
-		day_1_convective: "0",
-		day_2_convective: "8",
-		day_3_convective: "16",
-		day_4_8_convective: "20",
-	},
-	FeatureLayers: {
-		day_1_categorical: "1",
-		day_1_significant_tornado: "2",
-		day_1_probabilistic_tornado: "3",
-		day_1_significant_hail: "4",
-		day_1_probabilistic_hail: "5",
-		day_1_significant_wind: "6",
-		day_1_probabilistic_wind: "7",
-		day_2_categorical: "9",
-		day_2_significant_tornado: "10",
-		day_2_probabilistic_tornado: "11",
-		day_2_significant_hail: "12",
-		day_2_probabilistic_hail: "13",
-		day_2_significant_wind: "14",
-		day_2_probabilistic_wind: "15",
-		day_3_categorical: "17",
-		day_3_probabilistic: "18",
-		day_3_significant_severe: "19",
-		day_4_probabilistic: "21",
-		day_5_probabilistic: "22",
-		day_6_probabilistic: "23",
-		day_7_probabilistic: "24",
-		day_8_probabilistic: "25",
-	},
+  GroupLayers: {
+    day_1_convective: "0",
+    day_2_convective: "8",
+    day_3_convective: "16",
+    day_4_8_convective: "20",
+  },
+  FeatureLayers: {
+    day_1_categorical: "1",
+    day_1_significant_tornado: "2",
+    day_1_probabilistic_tornado: "3",
+    day_1_significant_hail: "4",
+    day_1_probabilistic_hail: "5",
+    day_1_significant_wind: "6",
+    day_1_probabilistic_wind: "7",
+    day_2_categorical: "9",
+    day_2_significant_tornado: "10",
+    day_2_probabilistic_tornado: "11",
+    day_2_significant_hail: "12",
+    day_2_probabilistic_hail: "13",
+    day_2_significant_wind: "14",
+    day_2_probabilistic_wind: "15",
+    day_3_categorical: "17",
+    day_3_probabilistic: "18",
+    day_3_significant_severe: "19",
+    day_4_probabilistic: "21",
+    day_5_probabilistic: "22",
+    day_6_probabilistic: "23",
+    day_7_probabilistic: "24",
+    day_8_probabilistic: "25",
+  },
 });
 
 // -- Group Layers (Convective Outlook )
 
-const fetchConvectiveOutlookGroupLayerById = async (layerId) => {
-	return await MAP_SERVER_CLIENT.get("/${layerId}");
+const fetchConvectiveOutlookGroupLayerById = async (groupLayerId) => {
+  return await MAP_SERVER_CLIENT.get(`/${groupLayerId}?f=json`);
 };
 
 const fetchAllConvectiveOutlookGroupLayers = async () => {
-	const groupLayerIds = Object.values(OUTLOOK_MAP_SERVER_LAYERS.GroupLayers);
+  const groupLayerIds = Object.values(OUTLOOK_MAP_SERVER_LAYERS.GroupLayers);
 
-	return await Promise.all(
-		groupLayerIds.map((groupLayerId) =>
-			fetchConvectiveOutlookGroupLayerById(groupLayerId)
-		)
-	);
+  return await Promise.all(
+    groupLayerIds.map((groupLayerId) =>
+      fetchConvectiveOutlookGroupLayerById(groupLayerId)
+    )
+  );
+};
+
+export const useAllConvectiveOutlookGroupLayersQuery = () => {
+  return useQuery(
+    ["convective-outlooks", "group-layers"],
+    async () => await fetchAllConvectiveOutlookGroupLayers()
+  );
 };
 
 // -- Feature Layers
 
-const fetchConvectiveOutlookFeatureLayerById = async (layerId) => {
-	return await MAP_SERVER_CLIENT.get(
-		`/${layerId}/${URLS.ENDPOINT.SPC.OTLK_GEOMETRY}`
-	);
+const fetchConvectiveOutlookFeatureLayerById = async (featureLayerId) => {
+  return await MAP_SERVER_CLIENT.get(
+    `/${featureLayerId}/query?&outFields=*&geometry=true&f=geojson`
+  );
 };
 
 const fetchAllConvectiveOutlookFeatureLayers = async () => {
-	const featureLayerIds = Object.values(
-		OUTLOOK_MAP_SERVER_LAYERS.FeatureLayers
-	);
+  const featureLayerIds = Object.values(
+    OUTLOOK_MAP_SERVER_LAYERS.FeatureLayers
+  );
 
-	return await Promise.all(
-		featureLayerIds.map((featureLayerId) =>
-			fetchConvectiveOutlookFeatureLayerById(featureLayerId)
-		)
-	);
+  return await Promise.all(
+    featureLayerIds.map((featureLayerId) =>
+      fetchConvectiveOutlookFeatureLayerById(featureLayerId)
+    )
+  );
 };
 
-export const useAllConvectiveOutlookLayersQuery = () => {
-	return useQuery(
-		["convective-outlooks", "all"],
-		async () => await fetchAllConvectiveOutlookFeatureLayers()
-	);
+export const useAllConvectiveOutlookFeatureLayersQuery = () => {
+  return useQuery(
+    ["convective-outlooks", "feature-layers"],
+    async () => await fetchAllConvectiveOutlookFeatureLayers()
+  );
 };
+
+// -- Original Outlook Query
 
 export const useConvectiveOutlooksQuery = () => {
-	return useQuery(["convective-outlooks", "categorical"], async () => {
-		return await Promise.all([
-			fetchConvectiveOutlookFeatureLayerById(1),
-			fetchConvectiveOutlookFeatureLayerById(9),
-			fetchConvectiveOutlookFeatureLayerById(17),
-		]);
-	});
+  return useQuery(["convective-outlooks", "categorical"], async () => {
+    return await Promise.all([
+      fetchConvectiveOutlookFeatureLayerById(1),
+      fetchConvectiveOutlookFeatureLayerById(9),
+      fetchConvectiveOutlookFeatureLayerById(17),
+    ]);
+  });
 };
 
 // **********************************************
@@ -103,27 +112,11 @@ export const useConvectiveOutlooksQuery = () => {
 // **********************************************
 
 const fetchConvectiveOutlookLegend = async () => {
-	return await MAP_SERVER_CLIENT.get(URLS.SPC.OTLK_MAP_SERV_LGND);
+  return await MAP_SERVER_CLIENT.get(URLS.SPC.OTLK_MAP_SERV_LGND);
 };
 
 export const useConvectiveOutlookLegendQuery = () => {
-	return useQuery(["convective-outlooks", "legend"], async () => {
-		return await fetchConvectiveOutlookLegend();
-	});
-};
-
-// consider this...
-const fetchConvectiveOutlookDataForDay = async (dayNumber) => {
-	const outlookDayLayerIdMap = {
-		1: [0, 1, 2, 3, 4, 5, 6, 7],
-		2: [8, 9, 10, 11, 12, 13, 14, 15],
-		3: [16, 17, 18, 19],
-		4: [20, 21, 22, 23, 24, 25],
-	};
-
-	const outlookLayers = await Promise.all(() => {
-		return outlookDayLayerIdMap[dayNumber].map((layerId) =>
-			fetchConvectiveOutlookFeatureLayerById(layerId)
-		);
-	});
+  return useQuery(["convective-outlooks", "legend"], async () => {
+    return await fetchConvectiveOutlookLegend();
+  });
 };
