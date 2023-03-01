a01ab1def8a35424e4f394172ee21abec42404b0
Eph <ephraim@modevx.com>
2023-03-01 07:37:24 -0600

    rename feature folders

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { PageLayout } from "_shared/components";
-import { TornadoAlertMap } from "features/tornado-alerts";
+import { TornadoAlertMap } from "features/alerts";
 import {
-  ConvectiveOutlookMap,
-  OutlookPreviewCard,
-  OutlookPreviewCardList,
-} from "features/convective-outlooks/components";
+	ConvectiveOutlookMap,
+	OutlookPreviewCard,
+	OutlookPreviewCardList,
+} from "features/outlooks/components";
 import {
-  useConvectiveOutlookLegendQuery,
-  useSPCConvectiveOutlooks,
+	useConvectiveOutlookLegendQuery,
+	useSPCConvectiveOutlooks,
 } from "services/convective-outlooks";
-import { OUTLOOK_LAYERS } from "features/convective-outlooks/constants";
+import { OUTLOOK_LAYERS } from "features/outlooks/constants";
 import { feature } from "topojson-client";
 
 const OutlooksPage = () => {
-  return (
-    <PageLayout>
-      <TornadoAlertMap />
-      {/* <ConvectiveOutlookMap /> */}
-      {/* TornadoNews/> */}
-      {/* <OutlookPreviewCardList outlooks={featureLayers} /> */}
-    </PageLayout>
-  );
+	return (
+		<PageLayout>
+			<TornadoAlertMap />
+			{/* <ConvectiveOutlookMap /> */}
+			{/* TornadoNews/> */}
+			{/* <OutlookPreviewCardList outlooks={featureLayers} /> */}
+		</PageLayout>
+	);
 };
 
 export default OutlooksPage;
