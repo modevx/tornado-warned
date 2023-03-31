6710502d89fc3231706095f5681fe2e17656e0ab
Eph <ephraim@modevx.com>
2023-03-31 18:53:51 -0500

    import icons

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { Stats } from "react-daisyui";
+
 import { PageLayout } from "_shared/components";
-import { AlertPolygonMap } from "features/alerts";
+import { AlertPolygonMap, AlertTextProduct } from "features/alerts";
+import { ICONS } from "_shared/constants";
+
 import {
 	useActiveTornadoWarningTextProducts,
 	useActiveTornadoWarningPolygons,
@@ -9,13 +13,26 @@ import {
 const HomePage = () => {
 	const { data: warningTextProducts } = useActiveTornadoWarningTextProducts();
 	const { data: warningPolygons } = useActiveTornadoWarningPolygons();
+	let warningCount = 0;
 
-	if (warningTextProducts)
+	// [?] DAISYUI: why do I need to desctructure for Stats.Stat to work?
+	const { Stat } = Stats;
+
+	if (warningTextProducts) {
+		warningCount = warningTextProducts.length;
 		console.log("Warning Text Products:\n", warningTextProducts);
+	}
 	if (warningPolygons) console.log("Warning Polygons:\n", warningPolygons);
 
 	return (
 		<PageLayout>
+			<Stats>
+				<Stats.Stat>
+					<Stat.Item variant='title'>Active Warnings</Stat.Item>
+					<Stat.Item variant='value'>{warningCount}</Stat.Item>
+				</Stats.Stat>
+			</Stats>
+
 			<div className='bg-red-500 p-4'>
 				<H1>Tornado Warnings</H1>
 				<div className='h-[200px]'></div>
