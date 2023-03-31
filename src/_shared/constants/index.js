55f4f6d6bacb4631ba08be0312fccf1edbf6ac26
Eph <ephraim@modevx.com>
2023-03-31 18:52:50 -0500

    remove unused import

--- src/_shared/constants/index.js
+++ src/_shared/constants/index.js
@@ -2,5 +2,4 @@ export { DEFAULT_TIMEOUT } from "./services";
 export { FOOTER_NAV_ITEMS } from "./footer-nav-items";
 export { ICONS } from "./icons";
 export { MAIN_NAV_ITEMS } from "./main-nav-items";
-export { ALERT_TYPES as NWS_ALERT_TYPES } from "../../features/alerts/alert-types";
 export { STATE_ABBREVIATIONS } from "./state-abbreviations";
