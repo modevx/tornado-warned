a48baf133bdbeb69ef01c569dea6f532c19dc24f
Eph <ephraim@modevx.com>
2023-05-05 17:01:00 -0500

    remove log

--- src/features/nws-alerts/AlertPolygonGroup.jsx
+++ src/features/nws-alerts/AlertPolygonGroup.jsx
@@ -1,17 +1,15 @@
 import { TornadoAlertPolygon } from "./TornadoAlertPolygon";
 
 export const AlertPolygonGroup = ({ alertsObj, fillColor }) => {
-  console.log("alertsObj >>\n", alertsObj);
-
-  return (
-    <g>
-      {alertsObj.features.map((feature, index) => (
-        <TornadoAlertPolygon
-          key={index}
-          feature={feature}
-          fillColor={fillColor}
-        />
-      ))}
-    </g>
-  );
+	return (
+		<g>
+			{alertsObj.features.map((feature, index) => (
+				<TornadoAlertPolygon
+					key={index}
+					feature={feature}
+					fillColor={fillColor}
+				/>
+			))}
+		</g>
+	);
 };
