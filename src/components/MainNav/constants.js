8cc1f4e47009ae947e21e34b8492e7f0c4883ba3
Eph <ephraim@modevx.com>
2023-05-05 16:58:28 -0500

    create component constants.js

---
+++ src/components/MainNav/constants.js
@@ -0,0 +1,30 @@
+export const MAIN_NAV_ITEMS = [
+	{
+		label: "active alerts",
+		href: "/active-alerts",
+	},
+	{
+		label: "convective outlooks",
+		href: "/",
+	},
+	{
+		label: "radar",
+		href: "/radar",
+	},
+	// live_dashboard: {
+	// 	label: "dashboard",
+	// 	href: "/live-dashboard",
+	// },
+	// local_storm_reports: {
+	// 	label: "storm reports",
+	// 	href: "/local-storm-reports",
+	// },
+	// radar_signatures: {
+	//   label: "signatures",
+	//   href: "/nexrad-signatures",
+	// },
+	// latest_info: {
+	//   label: "latest info",
+	//   href: "/latest-info",
+	// },
+];
--- src/features/convective-outlook-map/constants.js
+++ src/features/convective-outlook-map/constants.js
@@ -1,3 +1,4 @@
+import ICONS from "constants/icons";
 export const OUTLOOK_DAYS = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export const OUTLOOK_DAY_FEATURE_LAYER_IDS = Object.freeze({
@@ -10,3 +11,101 @@ export const OUTLOOK_DAY_FEATURE_LAYER_IDS = Object.freeze({
 	7: [24],
 	8: [25],
 });
+
+export const LAYER_IDS = Object.freeze({
+	day_1_convective: "0",
+	day_1_categorical: "1",
+	day_1_significant_tornado: "2",
+	day_1_probabilistic_tornado: "3",
+	day_1_significant_hail: "4",
+	day_1_probabilistic_hail: "5",
+	day_1_significant_wind: "6",
+	day_1_probabilistic_wind: "7",
+	day_2_convective: "8",
+	day_2_categorical: "9",
+	day_2_significant_tornado: "10",
+	day_2_probabilistic_tornado: "11",
+	day_2_significant_hail: "12",
+	day_2_probabilistic_hail: "13",
+	day_2_significant_wind: "14",
+	day_2_probabilistic_wind: "15",
+	day_3_convective: "16",
+	day_3_categorical: "17",
+	day_3_probabilistic: "18",
+	day_3_significant_severe: "19",
+	days_4_thru_8_convective: "20",
+	day_4_probabilistic: "21",
+	day_5_probabilistic: "22",
+	day_6_probabilistic: "23",
+	day_7_probabilistic: "24",
+	day_8_probabilistic: "25",
+});
+
+export const CATEGORY_STYLES = {
+	2: {
+		label: "storm",
+		description: "> 10% chance of thunderstorms",
+		abbr: "tstm",
+		activeColor: "bg-[hsl(120,60%,30%)]",
+		fill: "hsl(120,60%,30%)",
+		stroke: "hsl(120,70%,30%)",
+		icon: ICONS.IoRainy,
+	},
+	3: {
+		label: "marginal",
+		description: "scattered severe storms",
+		abbr: "mrgl",
+		activeColor: "bg-[hsl(120,100%,50%)]",
+		fill: "hsl(120,100%,50%)",
+		stroke: "hsl(120,100%,50%)",
+		icon: ICONS.IoThunderstorm,
+	},
+	4: {
+		label: "slight",
+		description: "organized severe storms expected",
+		abbr: "slgt",
+		activeColor: "bg-[hsl(60,100%,50%)]",
+		fill: "hsl(60,100%,30%)",
+		stroke: "hsl(60,100%,50%)",
+		icon: ICONS.IoWarningOutline,
+	},
+	5: {
+		label: "enhanced",
+		description: "concentrated, severe storms.",
+		abbr: "enh",
+		activeColor: "bg-[hsl(30,100%,50%)]",
+		fill: "hsl(30,100%,30%)",
+		stroke: "hsl(30,100%,50%)",
+		icon: ICONS.GiDamagedHouse,
+	},
+	6: {
+		label: "moderate",
+		description:
+			"widespread severe weather with multiple tornadoes and/or intense storms.",
+		abbr: "mdt",
+		activeColor: "bg-[hsl(0,100%,50%)]",
+		fill: "hsl(0,100%,30%)",
+		stroke: "hsl(0,100%,50%)",
+		icon: ICONS.BsTornado,
+	},
+	8: {
+		label: "high",
+		description:
+			"severe weather outbreak with intense tornadoes or a long-lived derecho system.",
+		abbr: "high",
+		activeColor: "bg-[hsl(300,100%,30%)]",
+		fill: "hsl(300,100%,30%)",
+		stroke: "hsl(300,100%,50%)",
+		icon: ICONS.IoSkull,
+	},
+	10: {
+		label: "significant",
+		description:
+			"severe weather outbreak with intense tornadoes or a long-lived derecho system.",
+		abbr: "high",
+		activeColor: "bg-[hsl(300,100%,30%)]",
+		fill: "hsl(300,100%,30%)",
+		stroke: "hsl(300,100%,50%)",
+		icon: ICONS.IoSkull,
+	},
+};
