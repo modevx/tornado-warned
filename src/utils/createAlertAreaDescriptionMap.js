4cdfb2a817808b5e41e191f6c3785afb690fe269
Eph <ephraim@modevx.com>
2023-05-05 16:56:37 -0500

    fix file name typo

---
+++ src/utils/createAlertAreaDescriptionMap.js
@@ -0,0 +1,23 @@
+// TODO: move to /features/nws-alerts/constants
+
+export const createAlertAreaDescriptionMap = (strAreaDescription) => {
+	if (strAreaDescription === null || strAreaDescription === undefined) return;
+
+	const arrAreaDescription = strAreaDescription.split(/(?:;\s)/gm);
+	const mapAreaDescription = new Map();
+
+	arrAreaDescription.forEach((affectedArea) => {
+		const [alertArea, state] = affectedArea.split(/\,\s/);
+
+		if (mapAreaDescription.get(state) === undefined) {
+			mapAreaDescription.set(state, new Array(alertArea));
+		} else {
+			mapAreaDescription.set(state, [
+				...mapAreaDescription.get(state),
+				alertArea,
+			]);
+		}
+	});
+
+	return mapAreaDescription;
+};
