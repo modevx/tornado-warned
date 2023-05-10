fa3b3137885433c59d193ca0f767c409d6831cbf
Eph <ephraim@modevx.com>
2023-05-09 23:29:08 -0500

    CONVECTIVE OUTLOOKS: display subLayer selector buttons based on selected outlook day

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,91 +1,111 @@
 import React from "react";
-import { Form, Radio } from "react-daisyui";
+import { Button, Form, Radio } from "react-daisyui";
 
 import { PageLayout } from "components";
 import { AlertPolygonMap, AlertStats } from "features/nws-alerts";
 import { ConvectiveOutlookMap } from "features/spc-convective-outlooks/components";
 import {
-  useAllLayersAndTables,
-  useLayerGeoJSONQuery,
-  useLegendLayersQuery,
+	useAllLayersAndTables,
+	useLayerGeoJSONQuery,
+	useLegendLayersQuery,
 } from "services/arcgis-server-convective-outlooks/service";
 
 const HomePage = () => {
-  const alertStats = { warningCount: 0, watchCount: 0 };
-  const { data: allLayersAndTables, error: allLayersAndTablesError } =
-    useAllLayersAndTables();
-  const { data: legendLayers, error: legendLayersError } =
-    useLegendLayersQuery();
+	const alertStats = { warningCount: 0, watchCount: 0 };
+	const { data: allLayersAndTables, error: allLayersAndTablesError } =
+		useAllLayersAndTables();
+	const { data: legendLayers, error: legendLayersError } =
+		useLegendLayersQuery();
 
-  const [selectedDay, setSelectedDay] = React.useState("1");
-  const [selectedLayer, setSelectedLayer] = React.useState("1");
+	const [selectedDay, setSelectedDay] = React.useState(0);
+	const [selectedLayer, setSelectedLayer] = React.useState(0);
 
-  const handleOutlookDaySelect = (e) => {
-    setSelectedDay(e.target.value);
-    setSelectedLayer(e.target.value);
-  };
+	const handleSelectedDay = (e) => {
+		setSelectedDay(e.target.value);
+		setSelectedLayer(e.target.value);
+	};
 
-  const handleLayerOnSelect = (e) => {
-    setSelectedLayer(e.target.value);
-  };
+	const handleLayerOnSelect = (e) => {
+		setSelectedLayer(e.target.value);
+	};
 
-  if (allLayersAndTables)
-    console.log(">> allLayersAndTables\n", allLayersAndTables);
-  if (legendLayers) console.log(">> legendLayers\n", legendLayers);
+	if (allLayersAndTables)
+		console.log(">> allLayersAndTables\n", allLayersAndTables);
+	if (legendLayers) console.log(">> legendLayers\n", legendLayers);
 
-  return (
-    <PageLayout>
-      {/* <AlertStats stats={alertStats} /> */}
-      <AlertPolygonMap />
-      <div>
-        <OutlookDaySelector onChangeHandler={handleOutlookDaySelect} />
-        <OutlookDaySubLayerSelector
-          selectedDay={selectedDay}
-          onSelectHandler={handleLayerOnSelect}
-        />
-        <h1>{selectedLayer}</h1>
-        <ConvectiveOutlookMap />
-      </div>
-    </PageLayout>
-  );
+	return (
+		<PageLayout>
+			{/* <AlertStats stats={alertStats} /> */}
+			<AlertPolygonMap />
+			<div>
+				<OutlookDaySelector onChangeHandler={handleSelectedDay} />
+				<h1 className='text-center'>Selected Layer: {selectedLayer}</h1>
+
+				<div className='flex'>
+					<ConvectiveOutlookMap />
+					{allLayersAndTables && (
+						<SubLayerSelector
+							allLayers={allLayersAndTables}
+							selectedDay={selectedDay}
+							onSelectHandler={handleLayerOnSelect}
+						/>
+					)}
+				</div>
+			</div>
+		</PageLayout>
+	);
 };
 
 export default HomePage;
 
 const OutlookDaySelector = ({ onChangeHandler }) => {
-  return (
-    <div className="flex items-center justify-center">
-      <span className="mr-10">Convective Outlook Day:</span>
-      <Form className="flex flex-row" onChange={onChangeHandler}>
-        <Form.Label title="1">
-          <Radio
-            name="outlookDays"
-            value="0"
-            className="ml-3"
-            size="xs"
-            defaultChecked
-          />
-        </Form.Label>
-        <Form.Label title="2">
-          <Radio name="outlookDays" value="8" className="ml-3" size="xs" />
-        </Form.Label>
-        <Form.Label title="3">
-          <Radio name="outlookDays" value="16" className="ml-3" size="xs" />
-        </Form.Label>
-      </Form>
-    </div>
-  );
+	return (
+		<div className='flex items-center justify-center'>
+			<span className='mr-10'>Convective Outlook Day:</span>
+			<Form className='flex flex-row' onChange={onChangeHandler}>
+				<OptionRadio title='1' value={0} defaultChecked />
+				<OptionRadio title='2' value={8} />
+				<OptionRadio title='3' value={16} />
+			</Form>
+		</div>
+	);
 };
 
-const OutlookDaySubLayerSelector = ({ selectedDay, onSelectHandler }) => {
-  // const subLayersIds = allLayersAndTables.filter(
-  //   ({ id }) => id === selectedDay
-  // );
-  // const {id, name} = allLayersAndTables[selectedDay].subLayers
-  return null;
+const OptionRadio = ({ title, value, ...rest }) => {
+	return (
+		<Form.Label title={title}>
+			<Radio
+				name='outlookDays'
+				value={value}
+				className='ml-3'
+				size='xs'
+				{...rest}
+			/>
+		</Form.Label>
+	);
 };
 
-const getSubLayerIds = ({ allLayers, selectedDay }) => {
-  const selectedLayerData = allLayers.filter(({ id }) => id === selectedDay);
-  const { subLayers } = selectedDay[0];
+const SubLayerSelector = ({ allLayers, selectedDay, onSelectHandler }) => {
+	const subLayers = getSubLayerArr({ allLayers, selectedDay });
+
+	return (
+		<div className='flex flex-col justify-center'>
+			{subLayers.map(({ id, name }) => {
+				const dayRemoved = name.replace(/^.{6}/, "");
+				const label = dayRemoved.toLowerCase().replace(" outlook", "");
+
+				return (
+					<Button key={name} className='mb-3 text-2xs'>
+						{/* {name.replace(/^.{6}/, "")} */}
+						{label}
+					</Button>
+				);
+			})}
+		</div>
+	);
+};
+
+const getSubLayerArr = ({ allLayers, selectedDay }) => {
+	const selectedLayer = allLayers.find(({ id }) => id == selectedDay);
+	return selectedLayer.subLayers;
 };
