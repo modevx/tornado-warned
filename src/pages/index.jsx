d0124cd4853d9bba475240f3787b347e5776942a
Ephraim <ephraim@modevx.com>
2023-05-10 16:36:33 -0500

    DASHBOARD: setup new dashboard grid layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,109 +5,85 @@ import { PageLayout } from "components";
 import { AlertPolygonMap, AlertStats } from "features/nws-alerts";
 import { ConvectiveOutlookMap } from "features/spc-convective-outlooks/components";
 import {
-	fetchOutlookLayerFeatures,
-	useAllLayersAndTables,
-	useLegendLayersQuery,
+  fetchConvectiveOutlookFeatures,
+  useAllLayersAndTables,
+  useLegendLayersQuery,
 } from "services/arcgis-server-convective-outlooks/service";
 
 const HomePage = () => {
-	const alertStats = { warningCount: 0, watchCount: 0 };
-	// ----------------------------
-	const { data: allLayers, error: allLayersError } = useAllLayersAndTables();
-	// if (allLayers) console.log(">> allLayers\n", allLayers);
-
-	const { data: legendLayers, error: legendLayersError } =
-		useLegendLayersQuery();
-	// if (legendLayers) console.log(">> legendLayers\n", legendLayers);
-	// ----------------------------
-	// const [selectedLayerId, setSelectedLayerId] = React.useState(1);
-	const [outlookFeatures, setOutlookFeatures] = React.useState([]);
-	// ----------------------------
-
-	const handleOutlookSelection = async (e) => {
-		// console.log(">> handleOutlookSelection: ", e.target.value);
-		const layerId = e.target.value;
-		const features = await fetchOutlookLayerFeatures(layerId);
-		setOutlookFeatures(features);
-	};
-
-	return (
-		<PageLayout>
-			{/* <AlertStats stats={alertStats} /> */}
-			{/* <AlertPolygonMap /> */}
-			<div>
-				{/* <OutlookDaySelector onChangeHandler={handleSelectedDay} /> */}
-				<Form className='grid grid-cols-3' onChange={handleOutlookSelection}>
-					{allLayers &&
-						allLayers.map(({ id, name }) => (
-							<OptionRadio title={`(${id}) ${name}`} value={id} key={name} />
-						))}
-				</Form>
-
-				<ConvectiveOutlookMap arrFeatures={outlookFeatures} />
-			</div>
-		</PageLayout>
-	);
+  return (
+    <PageLayout>
+      <div className="grid gap-4 grid-cols-4 grid-rows-3 bg-neutral-400 min-h-screen">
+        <div className="row-span-2 bg-purple-300">Tornado Warnings</div>
+        <div className="col-span-2 row-span-2 bg-red-400">
+          NEXRAD/Alert Polygons
+        </div>
+        <div className="row-span-3 bg-green-300">Convective Outlooks</div>
+        <div className="bg-yellow-200">Tornado Watches</div>
+        <div className="bg-blue-300">Twitter Feed</div>
+      </div>
+    </PageLayout>
+  );
 };
 
 export default HomePage;
 
 const OutlookDaySelector = ({ onChangeHandler }) => {
-	return (
-		<div className='flex items-center justify-center'>
-			<span className='mr-10'>Convective Outlook Day:</span>
-			<Form className='flex flex-row' onChange={onChangeHandler}>
-				<OptionRadio title='1' value={0} defaultChecked />
-				<OptionRadio title='2' value={8} />
-				<OptionRadio title='3' value={16} />
-			</Form>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center">
+      <span className="mr-10">Convective Outlook Day:</span>
+      <Form className="flex flex-row" onChange={onChangeHandler}>
+        <OptionRadio title="1" value={0} defaultChecked />
+        <OptionRadio title="2" value={8} />
+        <OptionRadio title="3" value={16} />
+      </Form>
+    </div>
+  );
 };
 
 const OptionRadio = ({ title, value, ...rest }) => {
-	return (
-		<Form.Label title={title}>
-			<Radio
-				name='outlookDays'
-				value={value}
-				// className='ml-3'
-				size='xs'
-				{...rest}
-			/>
-		</Form.Label>
-	);
+  return (
+    <Form.Label title={title}>
+      <Radio
+        name="outlookDays"
+        value={value}
+        // className='ml-3'
+        size="xs"
+        {...rest}
+      />
+    </Form.Label>
+  );
 };
 
 const SubLayerSelector = ({ allLayers, selectedDay, onClickHandler }) => {
-	const subLayers = getSubLayerArr({ allLayers, selectedDay });
+  const subLayers = getSubLayerArr({ allLayers, selectedDay });
 
-	return (
-		<div className='flex flex-col justify-center'>
-			{subLayers.map(({ id, name }) => {
-				const label = createSubLayerSelectBtnLabelText(name);
+  return (
+    <div className="flex flex-col justify-center">
+      {subLayers.map(({ id, name }) => {
+        const label = createSubLayerSelectBtnLabelText(name);
 
-				return (
-					<Button
-						key={name}
-						className='mb-3 text-2xs'
-						onClick={() => onClickHandler(id)}
-					>
-						{label}
-					</Button>
-				);
-			})}
-		</div>
-	);
+        return (
+          <Button
+            key={name}
+            className="mb-3 text-2xs"
+            onClick={() => onClickHandler(id)}
+          >
+            {label}
+          </Button>
+        );
+      })}
+    </div>
+  );
 };
 
 const getSubLayerArr = ({ allLayers, selectedDay }) => {
-	const selectedLayer = allLayers.find(({ id }) => id == selectedDay);
-	return selectedLayer.subLayers;
+  const selectedLayer = allLayers.find(({ id }) => id == selectedDay);
+  return selectedLayer.subLayers;
 };
 
 const createSubLayerSelectBtnLabelText = (name) => {
-	const dayRemoved = name.replace(/^.{6}/, "");
-	const label = dayRemoved.toLowerCase().replace(" outlook", "");
-	return label;
+  const dayRemoved = name.replace(/^.{6}/, "");
+  const label = dayRemoved.toLowerCase().replace(" outlook", "");
+  return label;
 };
--- src/services/arcgis-server-convective-outlooks/service.js
+++ src/services/arcgis-server-convective-outlooks/service.js
@@ -3,68 +3,68 @@ import { useQuery } from "@tanstack/react-query";
 import { convectiveOutlookHTTPClient } from "./http-client";
 
 const fetchAllLayersAndTables = async () => {
-	try {
-		const response = await convectiveOutlookHTTPClient.get("/layers?f=json");
-		// TODO: only return what I use
-		return response.data.layers;
-	} catch (error) {
-		console.log(">> fetchAllLayersAndTables:\n", error);
-	}
+  try {
+    const response = await convectiveOutlookHTTPClient.get("/layers?f=json");
+    // TODO: only return what I use
+    return response.data.layers;
+  } catch (error) {
+    console.log(">> fetchAllLayersAndTables:\n", error);
+  }
 };
 
 const fetchLayerJSON = async (layerId) => {
-	try {
-		const response = await convectiveOutlookHTTPClient.get(
-			`/${layerId}?f=json`
-		);
-		return response.data;
-	} catch (error) {
-		console.log(">> fetchLayerJSON:\n", error);
-	}
+  try {
+    const response = await convectiveOutlookHTTPClient.get(
+      `/${layerId}?f=json`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(">> fetchLayerJSON:\n", error);
+  }
 };
 
-export const fetchOutlookLayerFeatures = async (layerId) => {
-	try {
-		const response = await convectiveOutlookHTTPClient.get(
-			`/${layerId}/query?f=geojson&geometry=true&outfields=*`
-		);
-		return response.data.features;
-	} catch (error) {
-		console.log(">> fetchLayerGeoJSON:\n", error);
-	}
+export const fetchConvectiveOutlookFeatures = async (layerId) => {
+  try {
+    const response = await convectiveOutlookHTTPClient.get(
+      `/${layerId}/query?f=geojson&geometry=true&outfields=*`
+    );
+    return response.data.features;
+  } catch (error) {
+    console.log(">> fetchLayerGeoJSON:\n", error);
+  }
 };
 
 const fetchLegendLayers = async () => {
-	try {
-		const response = await convectiveOutlookHTTPClient.get("/legend?f=json");
-		return response.data.layers;
-	} catch (error) {
-		console.log(">> fetchLegendLayers:\n", error);
-	}
+  try {
+    const response = await convectiveOutlookHTTPClient.get("/legend?f=json");
+    return response?.data.layers;
+  } catch (error) {
+    console.log(">> fetchLegendLayers:\n", error);
+  }
 };
 
 export const useAllLayersAndTables = () => {
-	return useQuery(["convective outlooks", "all layers & tables", "json"], () =>
-		fetchAllLayersAndTables()
-	);
+  return useQuery(["convective outlooks", "all layers & tables", "json"], () =>
+    fetchAllLayersAndTables()
+  );
 };
 
 export const useLayerJSONQuery = (layerId) => {
-	return useQuery(["convective outlooks", "json"], (layerId) =>
-		fetchLayerJSON(layerId)
-	);
+  return useQuery(["convective outlooks", "json"], (layerId) =>
+    fetchLayerJSON(layerId)
+  );
 };
 
 export const useLayerGeoJSONQuery = (layerId) => {
-	// console.log(">> layerId: ", layerId);
+  // console.log(">> layerId: ", layerId);
 
-	return useQuery(["convective outlooks", "geojson"], (layerId) =>
-		fetchOutlookLayerFeatures(layerId)
-	);
+  return useQuery(["convective outlooks", "geojson"], (layerId) =>
+    fetchConvectiveOutlookFeatures(layerId)
+  );
 };
 
 export const useLegendLayersQuery = () => {
-	return useQuery(["convective outlooks", "legend"], () => {
-		return fetchLegendLayers();
-	});
+  return useQuery(["convective outlooks", "legend"], () => {
+    return fetchLegendLayers();
+  });
 };
