fa21bfd0718ec6160ef5162ca18cf1bd6728657d
Ephraim <ephraim@modevx.com>
2023-05-27 09:11:03 -0500

    ALERTS: create enhanced alert graphics

--- src/features/nws-alerts/TornadoAlertTextProduct.jsx
+++ src/features/nws-alerts/TornadoAlertTextProduct.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Card, Collapse, Modal } from "react-daisyui";
 
 import { LocaleTime } from "components";
-import { STATE_ABBREVIATIONS } from "constants";
 import ICONS from "constants/icons";
-import { createAlertAreaDescriptionMap } from "utils";
-
-// TODO: delete TornadoWatchTextProduct & turn TornadoWarningTextProduct into single TornadoAlertTextProduct
 
 export const TornadoAlertTextProduct = ({ alert }) => {
   const { id, properties } = alert;
@@ -20,124 +16,92 @@ export const TornadoAlertTextProduct = ({ alert }) => {
     senderName,
     parameters: { maxHailSize, tornadoDetection },
   } = properties;
-  const DownArrow = ICONS.FaChevronDown;
 
-  const [visible, setVisible] = React.useState(false);
+  const doesStringContainPhrase = (string, phrase) => {
+    const loweredString = string.toLowerCase();
+    const loweredPhrase = phrase.toLowerCase();
 
-  const areaDescMAP = createAlertAreaDescriptionMap(areaDesc);
-  const areaDescMapARR = Array.from(areaDescMAP.entries());
+    console.log("typeof loweredString\n", typeof loweredString);
+    console.log("loweredPhrase\n", loweredPhrase);
 
-  const toggleVisible = () => setVisible((prev) => !prev);
+    return loweredString.includes(loweredPhrase);
+  };
+
+  const isAlertParticularlyDangerousSituation = (alertDescription) => {
+    return doesStringContainPhrase(
+      alertDescription,
+      "particularly dangerous situation"
+    );
+  };
+
+  const isAlertornadoEmergency = (alertDescription) => {
+    return doesStringContainPhrase(alertDescription, "tornado emergency");
+  };
+
+  const isPDS = isAlertParticularlyDangerousSituation(description);
+
+  const isTornadoEmergency = isAlertornadoEmergency(description);
+
+  const tornadoEmergencyBannerStyles = "bg-red-500 animate-pulse";
+  const pdsBannerStyles = "bg-yellow-400 animate-pulse text-black";
+
+  const bgStyle = isTornadoEmergency
+    ? tornadoEmergencyBannerStyles
+    : isPDS
+    ? pdsBannerStyles
+    : "bg-black";
+
+  const enhancedAlertLabel = isTornadoEmergency
+    ? "TORNADO EMERGENCY"
+    : isPDS
+    ? "PARTICULARLY DANGEROUS SITUATION"
+    : null;
 
   return (
     <Collapse className="bg-black m-2 rounded-md" checkbox>
-      <Collapse.Title className="flex justify-between items-center px-2">
-        <span className="text-xs">{senderName.replace("NWS ", "")}</span>
-        <span className="text-xs flex items-center">
-          <LocaleTime date={expires} />
-          <DownArrow className="ml-2" />
-        </span>
+      <Collapse.Title className="p-0 bg-black">
+        <p
+          className={`${bgStyle} text-xs rounded-t-md p-1 mb-2 text-center font-bold`}
+        >
+          {enhancedAlertLabel}
+        </p>
+        <div className="flex justify-between items-center px-2">
+          <span className="text-xs">{senderName.replace("NWS ", "")}</span>
+          <span className="text-xs flex items-center">
+            <LocaleTime date={expires} />
+            <DownChevronIcon className="ml-2" />
+          </span>
+        </div>
       </Collapse.Title>
-      <Collapse.Content className="text-xs">{description}</Collapse.Content>
+
+      <Collapse.Content className="text-xs bg-black">
+        {description}
+      </Collapse.Content>
     </Collapse>
-    // <Card className="bg-red-600 m-3 p-2">
-    //   <Card.Body className="p-2">
-    //     <CardSection>
-    //       <p className="text-right text-2xl font-bold">
-    //         {senderName.replace("NWS ", "")}
-    //       </p>
-    //       <p className="text-right text-sm">
-    //         Expires <LocaleDate date={expires} format="LLL" />
-    //       </p>
-    //     </CardSection>
+  );
+};
 
-    //     <div className="flex justify-between">
-    //       <CardSection>
-    //         <p>{tornadoDetection}</p>
-    //       </CardSection>
-    //       <CardSection>
-    //         <p>{`Max Hail: ${maxHailSize[0].replace("Up to ", "")}`}</p>
-    //       </CardSection>
-    //     </div>
+const DownChevronIcon = ICONS.FaChevronDown;
 
-    //     <CardSection>
-    //       <AffectedAreas
-    //         areaDescription={areaDescMapARR}
-    //         stateMap={STATE_ABBREVIATIONS}
-    //       />
-    //     </CardSection>
+const AlertIcon = ICONS.IoWarningOutline;
 
-    //     <CardSection>{description}</CardSection>
+const TornadoIcon = ICONS.BsTornado;
 
-    //     <CardSection>{instruction}</CardSection>
-    //   </Card.Body>
-    //   {/* <Card.Actions></Card.Actions> */}
-    //   {/* <Card.Image /> */}
-    // </Card>
-    // <div className="font-sans">
-    //   <button
-    //     onClick={toggleVisible}
-    //     className={`w-full flex justify-between align-center bg-gradient-to-r ${fromColor} to-steel-900 my-2 hover:opacity-80`}
-    //   >
-    //     <span key={1} className="text-xs">
-    //       {senderName.replace("NWS", "")}
-    //     </span>
-    //     <div>
-    //       <span key={2} className="text-xs">
-    //         Expires: &nbsp;
-    //       </span>
-    //       <span key={3} className="text-xs">
-    //         <LocaleDate date={expires} format="LT" />
-    //       </span>
-    //     </div>
-    //   </button>
-    //   <Modal
-    //     open={visible}
-    //     className="bg-gradient-to-br from-black to-gray-800 text-white mx-auto"
-    //   >
-    //     <button
-    //       size="sm"
-    //       shape="circle"
-    //       className="absolute right-2 top-2"
-    //       onClick={toggleVisible}
-    //     >
-    //       ✕
-    //     </button>
-    //     <Modal.Header className="font-bold text-3xl">{event}</Modal.Header>
+const PDSGraphic = () => {
+  return (
+    <div className="flex flex-col items-center animate-pulse">
+      <AlertIcon size={24} color="yellow" />
+      <span className="text-xs mt-1 text-yellow-300">PDS</span>
+    </div>
+  );
+};
 
-    //     <Modal.Body>
-    //       <div className="flex flex-col items-center mb-4 w-full">
-    //         <div className="bg-neutral-700 px-4 py-2 mb-4 rounded w-full">
-    //           <div className="text-xs text-center">
-    //             <span>Expires: </span>
-    //             <LocaleDate date={expires} format="LT" />
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <div className="bg-neutral-700 px-4 py-2 mb-4 rounded">
-    //         {areaDescMapARR.map(([state, areas]) => {
-    //           const joinedAreaDescSTR = areas.join(", ");
-
-    //           return (
-    //             <div key={state}>
-    //               <h4 className="text-lg font-medium mb-2 uppercase">
-    //                 {STATE_ABBREVIATIONS[state]}
-    //               </h4>
-    //               <p className="text-sm mb-2">{joinedAreaDescSTR}</p>
-    //             </div>
-    //           );
-    //         })}
-    //       </div>
-    //       {instruction !== null ? (
-    //         <div className="bg-neutral-700 px-4 py-2 mb-4 rounded">
-    //           <p className="text-sm">{instruction}</p>
-    //         </div>
-    //       ) : (
-    //         ""
-    //       )}
-    //     </Modal.Body>
-    //   </Modal>
-    // </div>
+const TornadoEmergencyGraphic = () => {
+  return (
+    <div className="flex flex-col items-center animate-pulse">
+      <TornadoIcon size={24} color="red" />
+      <span className="text-xs mt-1 text-red-400">EMERGENCY</span>
+    </div>
   );
 };
 
