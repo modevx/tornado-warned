14cf2240304c34cfad051545b0b4e236e83e21cc
Ephraim <ephraim@modevx.com>
2023-06-09 21:37:33 -0500

    delete unused base components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,2 @@
-export * from "./Clock/Clock";
 export * from "./Footer/Footer";
-export * from "./Header/Header";
 export * from "./LocaleDate/LocaleDate";
-export * from "./Navbar/Navbar";
-export * from "./SectionHeading/SectionHeading";
