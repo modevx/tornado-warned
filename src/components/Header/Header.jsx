4915b7c869046408494224843a2adfe8f9af51ad
Ephraim <ephraim@modevx.com>
2023-06-09 23:08:08 -0500

    create Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,66 @@
+import NextLink from "next/link";
+import { GoAlert } from "react-icons/go";
+import { Menu, Navbar } from "react-daisyui";
+
+const NAV_ITEMS = [
+  {
+    label: "alerts",
+    href: "/alerts",
+  },
+  {
+    label: "outlooks",
+    href: "/convective-outlooks",
+  },
+];
+
+export const Header = () => {
+  return (
+    <header
+      data-testid="header"
+      className="text-center p-4 sm:flex sm:justify-between sm:items-center"
+    >
+      <Navbar>
+        <Navbar.Start>
+          <Branding />
+        </Navbar.Start>
+        <Navbar.End>
+          <Menu horizontal>
+            <Menu.Item key="alerts">
+              <GoAlert />
+            </Menu.Item>
+            {/* <Menu.Item key="convective-outlooks">
+              <BiSolidBinoculars color="white" size={20} />
+            </Menu.Item> */}
+          </Menu>
+        </Navbar.End>
+      </Navbar>
+    </header>
+  );
+};
+
+const Branding = () => {
+  return (
+    <NextLink href="/">
+      <a className="text-3xl text-neutral-200 font-display uppercase">
+        Tornado{" "}
+        <span className="italic bg-clip-text text-transparent bg-gradient-to-br from-red-500 to-neutral-400 pr-2">
+          Warned
+        </span>
+      </a>
+    </NextLink>
+  );
+};
+
+const MainNav = () => {
+  return (
+    <Menu horizontal>
+      {NAV_ITEMS.map(({ icon, label, href }) => {
+        return (
+          <Menu.Item key={label}>
+            <NextLink href={href}>{label}</NextLink>
+          </Menu.Item>
+        );
+      })}
+    </Menu>
+  );
+};
