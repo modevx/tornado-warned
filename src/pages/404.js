adab63c666a073964176e4e4df364fd2bf8acc4d
Ephraim <ephraim@modevx.com>
2023-06-09 23:09:24 -0500

    update PageLayout import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
-import { Page } from "../layout";
+import { PageLayout } from "components/PageLayout";
 
 export default function Custom404() {
-	return (
-		<Page>
-			<h1 className='sr-only'>404 - Page Not Found</h1>
-			<p className='font-bold text-3xl'>
-				sorry. couldn&apos;t find THAT page. smash the BACK button to get back
-				on track.
-			</p>
-		</Page>
-	);
+  return (
+    <PageLayout>
+      <h1 className="sr-only">404 - Page Not Found</h1>
+      <p className="font-bold text-3xl">
+        sorry. couldn&apos;t find THAT page. smash the BACK button to get back
+        on track.
+      </p>
+    </PageLayout>
+  );
 }
