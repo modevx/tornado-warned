bb9b47ecee324b1c559d0d829e3faf83495c33dc
Ephraim <ephraim@modevx.com>
2023-06-09 23:08:55 -0500

    update PageLayout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,7 @@
-import { Page } from "../layout";
-import { NationalWeatherServiceAlerts } from "features";
-import { ALERT_TYPES } from "services/national_weather_service/config";
+import { PageLayout } from "components/PageLayout";
 
 const HomeScreen = () => {
-	return (
-		<Page>
-			<NationalWeatherServiceAlerts alertType={ALERT_TYPES.tornadoWarning} />
-			<NationalWeatherServiceAlerts alertType={ALERT_TYPES.tornadoWatch} />
-		</Page>
-	);
+  return <PageLayout></PageLayout>;
 };
 
 export default HomeScreen;
