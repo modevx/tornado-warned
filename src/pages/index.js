100d2968bd8212dd63ddca57c41c25e53005e6a1
Eph <ephraim@modevx.com>
2023-06-15 16:30:22 -0500

    import new service queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import { PageLayout } from "components/PageLayout";
-import { useTornadoWatchAlertQuery } from "services/nws-api-web-service";
+import {
+	useTornadoWarningAlertQuery,
+	useTornadoWatchAlertQuery,
+	useSevereStormWarningAlertQuery,
+	useSevereStormWatchAlertQuery,
+} from "services/nws-api-web-service";
 
 const HomeScreen = () => {
-	const tornadoWatches = useTornadoWatchAlertQuery();
+	const { data: tornadoWarnings } = useTornadoWarningAlertQuery();
+	const { data: tornadoWatches } = useTornadoWatchAlertQuery();
+	const { data: stormWarnings } = useSevereStormWarningAlertQuery();
+	const { data: stormWatches } = useSevereStormWatchAlertQuery();
 
-	if (tornadoWatches) {
-		console.log(tornadoWatches);
-	}
+	if (tornadoWarnings) console.log(tornadoWarnings);
+	if (tornadoWatches) console.log(tornadoWatches);
+	if (stormWarnings) console.log(stormWarnings);
+	if (stormWatches) console.log(stormWatches);
 
 	return <PageLayout></PageLayout>;
 };
