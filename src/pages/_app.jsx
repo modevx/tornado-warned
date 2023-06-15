3ac3f6814e20284f0e35c4ee4a4a0606ba5d7eb3
Eph <ephraim@modevx.com>
2023-06-15 16:29:42 -0500

    remove redundant 'alerts' link

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import "styles/tailwind.css";
 
 export default function MyApp({ Component, pageProps }) {
