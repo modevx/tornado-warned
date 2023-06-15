89e45d53c97087e451a89f96bd6117577df36d42
Eph <ephraim@modevx.com>
2023-06-15 17:20:10 -0500

    return from components

---
+++ src/features/active-alerts/TornadoWarningAlert.jsx
@@ -0,0 +1,17 @@
+/**
+ *
+ * DISPLAY PROPS:
+ * - areaDesc
+ * - effective
+ * - expires
+ * - senderName
+ * - description
+ * - instruction
+ * PARAMETERS
+ * - NWSheadline
+ * - maxHailSize
+ * - tornadoDetection
+ */
+export const TornadoWarningAlert = (alert) => {
+	return JSON.stringify(alert);
+};
