da3ebc64703553cb6d83728557fb0585db6f5be2
Ephraim <ephraim@modevx.com>
2023-06-15 21:12:49 -0500

    update imports

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import { PageLayout } from "components/PageLayout";
+import { PageLayout } from "components";
 
 export default function Custom404() {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,66 @@
-import { PageLayout } from "components/PageLayout";
+import { Stats } from "react-daisyui";
+
+import { PageLayout } from "components";
 import {
-	TornadoWarningAlert,
-	TornadoWatchAlert,
-	SevereStormWarningAlert,
-	SevereStormWatchAlert,
+  TornadoWarningAlert,
+  TornadoWatchAlert,
+  SevereStormWarningAlert,
+  SevereStormWatchAlert,
 } from "features/active-alerts";
 import {
-	useTornadoWarningAlertQuery,
-	useTornadoWatchAlertQuery,
-	useSevereStormWarningAlertQuery,
-	useSevereStormWatchAlertQuery,
+  useTornadoWarningAlertQuery,
+  useTornadoWatchAlertQuery,
+  useSevereStormWarningAlertQuery,
+  useSevereStormWatchAlertQuery,
 } from "services/nws-api-web-service";
 
 const HomeScreen = () => {
-	const { data: tornadoWarnings } = useTornadoWarningAlertQuery();
-	const { data: tornadoWatches } = useTornadoWatchAlertQuery();
-	const { data: stormWarnings } = useSevereStormWarningAlertQuery();
-	const { data: stormWatches } = useSevereStormWatchAlertQuery();
+  const { data: tornadoWarnings } = useTornadoWarningAlertQuery();
+  const { data: tornadoWatches } = useTornadoWatchAlertQuery();
+  const { data: stormWarnings } = useSevereStormWarningAlertQuery();
+  const { data: stormWatches } = useSevereStormWatchAlertQuery();
 
-	if (tornadoWarnings) console.log(tornadoWarnings);
-	if (tornadoWatches) console.log(tornadoWatches);
-	if (stormWarnings) console.log(stormWarnings);
-	if (stormWatches) console.log(stormWatches);
+  if (tornadoWarnings) console.log(tornadoWarnings);
+  if (tornadoWatches) console.log(tornadoWatches);
+  if (stormWarnings) console.log(stormWarnings);
+  if (stormWatches) console.log(stormWatches);
 
-	return (
-		<PageLayout>
-			{tornadoWarnings
-				? tornadoWarnings.map((alert) => (
-						<TornadoWarningAlert key={alert.id} alert={alert} />
-				  ))
-				: null}
-		</PageLayout>
-	);
+  const { Stat } = Stats;
+
+  return (
+    <PageLayout>
+      <Stats className="mb-6">
+        <Stats.Stat>
+          <Stat.Item variant="title"> ACTIVE WARNINGS</Stat.Item>
+          <Stat.Item variant="value">
+            {tornadoWarnings ? tornadoWarnings.length : 0}
+          </Stat.Item>
+        </Stats.Stat>
+        <Stats.Stat>
+          <Stat.Item variant="title">ACTIVE WATCHES</Stat.Item>
+          <Stat.Item variant="value">
+            {tornadoWatches ? tornadoWatches.length : 0}
+          </Stat.Item>
+        </Stats.Stat>
+      </Stats>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {tornadoWarnings
+          ? tornadoWarnings.map((featureObj) => (
+              <TornadoWarningAlert key={featureObj.id} alert={featureObj} />
+            ))
+          : null}
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {tornadoWatches
+          ? tornadoWatches.map((featureObj) => (
+              <TornadoWatchAlert key={featureObj.id} alert={featureObj} />
+            ))
+          : null}
+      </div>
+    </PageLayout>
+  );
 };
 
 export default HomeScreen;
