a804f744e87b4f9d29c27d662bbf11e1a145c4e8
Eph <ephraim@modevx.com>
2023-06-16 12:27:51 -0500

    create convective outlooks page with CategoricalOutlookMaps

--- src/pages/convective-outlooks.jsx
+++ src/pages/convective-outlooks.jsx
@@ -1,15 +1,37 @@
 import { PageLayout } from "components";
+import { CategoricalOutlookMap } from "features/convective-outlooks";
 import { useCategoricalOutlooks } from "services/spc-convective-outlook-mapserver";
 
 const ConvectiveOutlookScreen = () => {
-  const { data } = useCategoricalOutlooks();
-  let day1outlook = [],
-    day2outlook = [],
-    day3outlook = [];
+	const { data } = useCategoricalOutlooks();
+	let day1outlook = [],
+		day2outlook = [],
+		day3outlook = [];
 
-  if (data) {
-    [day1outlook, day2outlook, day3outlook] = data;
-  }
+	if (data) {
+		[day1outlook, day2outlook, day3outlook] = data;
+	}
 
-  return <PageLayout></PageLayout>;
+	return (
+		<PageLayout>
+			{data ? (
+				<>
+					<CategoricalOutlookMap
+						key='day-1-categorical-outlook-map'
+						features={day1outlook}
+					/>
+					<CategoricalOutlookMap
+						key='day-2-categorical-outlook-map'
+						features={day2outlook}
+					/>
+					<CategoricalOutlookMap
+						key='day-3-categorical-outlook-map'
+						features={day3outlook}
+					/>
+				</>
+			) : null}
+		</PageLayout>
+	);
 };
+
+export default ConvectiveOutlookScreen;
