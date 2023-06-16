7154f540877b54569459a2a6502f58ba9bb7477b
Ephraim <ephraim@modevx.com>
2023-06-15 21:13:06 -0500

    add DaisyUI Card component

--- src/features/active-alerts/TornadoWatchAlert.jsx
+++ src/features/active-alerts/TornadoWatchAlert.jsx
@@ -1,3 +1,21 @@
-export const TornadoWatchAlert = () => {
-	return null;
+import { Card } from "react-daisyui";
+
+export const TornadoWatchAlert = ({ alert }) => {
+  const { properties } = alert;
+  const {
+    areaDesc,
+    effective,
+    expires,
+    senderName,
+    description,
+    instruction,
+    parameters: { NWSheadline, maxHailSize, tornadoDetection },
+  } = properties;
+
+  return (
+    <Card className="bg-yellow-400 p-2">
+      <Card.Title className="bg-black rounded-md p-2">{senderName}</Card.Title>
+      <Card.Body></Card.Body>
+    </Card>
+  );
 };
