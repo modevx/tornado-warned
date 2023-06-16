f3674d68482f73bd34424feb7b37834f9a689c3d
Eph <ephraim@modevx.com>
2023-06-16 12:28:03 -0500

    finish building component

--- src/features/convective-outlooks/CategoricalOutlookMap.jsx
+++ src/features/convective-outlooks/CategoricalOutlookMap.jsx
@@ -1,64 +1,33 @@
 import rewind from "@turf/rewind";
 import { geoAlbers, geoPath } from "d3-geo";
 
-import { USMap } from "components/AlbersStateMap";
-import { OutlookFeaturePath } from "./OutlookFeaturePath";
-
-const projection = geoAlbers();
-const albersGeoPath = geoPath(projection);
+import { USMap } from "components/USMap";
+import { CATEGORICAL_OUTLOOK_FEATURE_STYLES as styles } from "./constants";
 
 export const CategoricalOutlookMap = ({ features }) => {
-  return (
-    <USMap>
-      {features &&
-        features.map((feature) => {
-          const {
-            id,
-            properties: { idp_source },
-          } = feature;
+	const projection = geoAlbers();
+	const albersGeoPath = geoPath(projection);
 
-          return (
-            <OutlookFeaturePath key={`${idp_source}-${id}`} feature={feature} />
-          );
-        })}
-    </USMap>
-  );
-};
+	return (
+		<USMap>
+			{features &&
+				features.map((feature) => {
+					const {
+						id,
+						properties: { dn, idp_source },
+					} = feature;
 
-// AlbersSvgPathReverse
-import rewind from "@turf/rewind";
-
-import { albersGeoPath } from "utils";
-
-export const AlbersSvgPathReverse = ({ feature, ...styles }) => {
-  return (
-    <path d={albersGeoPath(rewind(feature, { reverse: true }))} {...styles} />
-  );
-};
-
-// albersGeoPath
-import { geoAlbers, geoPath } from "d3-geo";
-
-const projection = geoAlbers();
-
-export const albersGeoPath = geoPath(projection);
-
-// CategoricalOutlookFeaturePath
-import { AlbersSvgPathReverse } from "components/AlbersSvgPathReverse";
-import { CATEGORICAL_OUTLOOK_FEATURE_STYLES as styles } from "features/spc-convective-outlooks/constants";
-
-export const CategoricalOutlookFeaturePath = ({ feature }) => {
-  const {
-    properties: { dn },
-  } = feature;
-
-  return (
-    <AlbersSvgPathReverse
-      feature={feature}
-      fill={styles[dn].color}
-      opacity={0.7}
-      stroke={styles[dn].stroke}
-      strokeWidth={4}
-    />
-  );
+					return (
+						<path
+							key={`${idp_source}-${id}`}
+							d={albersGeoPath(rewind(feature, { reverse: true }))}
+							fill={styles[dn].color}
+							opacity={0.7}
+							stroke={styles[dn].stroke}
+							strokeWidth={4}
+						/>
+					);
+				})}
+		</USMap>
+	);
 };
