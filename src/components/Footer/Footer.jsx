6cd3d46cb54b5905ce1c6eb1d7694961cbd834a6
Ephraim <ephraim@modevx.com>
2023-06-17 21:35:09 -0500

    FOOTER/fix skywarn link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,62 +2,62 @@ import { Footer as DaisyFooter } from "react-daisyui";
 import NextLink from "next/link";
 
 const FOOTER_SECTION_LINKS = [
-	{
-		title: "BE PREPARED",
-		links: [
-			{
-				label: "Tornado Safety",
-				href: "https://www.weather.gov/safety/tornado",
-			},
-		],
-	},
-	{
-		title: "STORM CHASING",
-		links: [
-			{
-				label: "SKYWARN Storm Spotter Program",
-				href: "https://www.weather/skywarn",
-			},
-		],
-	},
+  {
+    title: "BE PREPARED",
+    links: [
+      {
+        label: "Tornado Safety",
+        href: "https://www.weather.gov/safety/tornado",
+      },
+    ],
+  },
+  {
+    title: "STORM CHASING",
+    links: [
+      {
+        label: "SKYWARN Storm Spotter Program",
+        href: "https://www.weather.gov/skywarn",
+      },
+    ],
+  },
 ];
 
 export const Footer = () => {
-	const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-	return (
-		<FooterComponent>
-			{FOOTER_SECTION_LINKS.map(({ title, links }) => (
-				<div key={title}>
-					<FooterSectionTitle title={title} />
-					<FooterSectionLinks links={links} />
-				</div>
-			))}
-			<FooterCopyright year={currentYear} />
-		</FooterComponent>
-	);
+  return (
+    <FooterComponent>
+      {FOOTER_SECTION_LINKS.map(({ title, links }) => (
+        <div key={title}>
+          <FooterSectionTitle title={title} />
+          <FooterSectionLinks links={links} />
+        </div>
+      ))}
+      <FooterCopyright year={currentYear} />
+    </FooterComponent>
+  );
 };
 
 const FooterComponent = ({ children }) => (
-	<DaisyFooter className='p-10 bg-neutral text-neutral-content'>
-		{children}
-	</DaisyFooter>
+  <DaisyFooter className="p-10 bg-neutral text-neutral-content">
+    {children}
+  </DaisyFooter>
 );
 
 const FooterCopyright = ({ year }) => (
-	<p className='text-center text-xs mb-3'>
-		&copy; {year} MODEVX, LTD. &nbsp; All Rights Reserved.
-	</p>
+  <p className="text-center text-xs mb-3">
+    &copy; {year} MODEVX, LTD. &nbsp; All Rights Reserved.
+  </p>
 );
 
 const FooterSectionLinks = ({ links }) => {
-	return links.map(({ label, href }) => (
-		<NextLink key={label} href={`${href}`} className='link link-hover'>
-			{label}
-		</NextLink>
-	));
+  return links.map(({ label, href }) => (
+    <NextLink key={label} href={`${href}`} className="link link-hover">
+      {label}
+    </NextLink>
+  ));
 };
 
 const FooterSectionTitle = ({ title }) => (
-	<DaisyFooter.Title>{title}</DaisyFooter.Title>
+  <DaisyFooter.Title>{title}</DaisyFooter.Title>
 );
