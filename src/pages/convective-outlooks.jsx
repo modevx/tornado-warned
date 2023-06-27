37857a6aa6c2a78f2e684d4b02cb49094d6f4f48
Eph <ephraim@modevx.com>
2023-06-27 14:14:35 -0500

    import new legend component

--- src/pages/convective-outlooks.jsx
+++ src/pages/convective-outlooks.jsx
@@ -1,9 +1,15 @@
 import { PageLayout } from "components";
-import { OutlookSection } from "features/convective-outlooks";
+import {
+	CategoricalLegend,
+	OutlookSection,
+} from "features/convective-outlooks";
+import { CATEGORICAL_OUTLOOK_FEATURE_STYLES } from "features/convective-outlooks/constants";
 
 const ConvectiveOutlookScreen = () => {
 	return (
 		<PageLayout>
+			<CategoricalLegend stylesObj={CATEGORICAL_OUTLOOK_FEATURE_STYLES} />
+
 			<div className='lg:flex lg:justify-evenly lg:items-center'>
 				<OutlookSection day={1} layerId={1} />
 				<OutlookSection day={2} layerId={9} />
