41f116c815b320cd3f028645d20332ab5bf622e9
Eph <ephraim@modevx.com>
2023-06-27 15:29:59 -0500

    reduce rounded corners on legend color badges

--- src/features/convective-outlooks/CategoricalLegendKey.jsx
+++ src/features/convective-outlooks/CategoricalLegendKey.jsx
@@ -1,5 +1,5 @@
 export const CategoricalLegendKey = ({ colorStr, labelStr }) => {
-	const badgeStyles = `bg-[${colorStr}] h-2 w-2 rounded-sm sm:h-4 sm:w-4 sm:rounded-md`;
+	const badgeStyles = `bg-[${colorStr}] h-2 w-2 rounded-sm sm:h-4 sm:w-4 sm:rounded-sm`;
 
 	return (
 		<div className='flex items-center'>
