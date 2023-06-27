e119efd94e892b59c78325d50dfffed0f05e719d
Eph <ephraim@modevx.com>
2023-06-27 11:41:08 -0500

    create outlook sub-component & consume individual mapserver layer responses

--- src/pages/convective-outlooks.jsx
+++ src/pages/convective-outlooks.jsx
@@ -1,107 +1,102 @@
-import { DayJSDateTime, PageLayout } from "components";
+import { DayJSDateTime, PageLayout, USMap } from "components";
 import { CategoricalOutlookMap } from "features/convective-outlooks";
-import { useCategoricalOutlooks } from "services/spc-convective-outlook-mapserver";
+import {
+	useCategoricalOutlooks,
+	useCategoricalOutlookGeoJsonQuery,
+} from "services/spc-convective-outlook-mapserver";
 
 const ConvectiveOutlookScreen = () => {
-  const { data } = useCategoricalOutlooks();
-  let day1outlook = [],
-    day2outlook = [],
-    day3outlook = [];
+	const { data: day1outlook } = useCategoricalOutlookGeoJsonQuery(1);
+	const { data: day2outlook } = useCategoricalOutlookGeoJsonQuery(9);
+	const { data: day3outlook } = useCategoricalOutlookGeoJsonQuery(17);
 
-  if (data) {
-    [day1outlook, day2outlook, day3outlook] = data;
-  }
+	let day1valid, day1expire, day2valid, day2expire, day3valid, day3expire;
 
-  if (data) console.log(day1outlook[day1outlook.length - 1].properties.dn);
+	if (day1outlook) {
+		day1valid = day1outlook[0].properties.valid;
+		day1expire = day1outlook[0].properties.expire;
+	}
+	if (day2outlook) {
+		day2valid = day2outlook[0].properties.valid;
+		day2expire = day2outlook[0].properties.expire;
+	}
+	if (day3outlook) {
+		day3valid = day3outlook[0].properties.valid;
+		day3expire = day3outlook[0].properties.expire;
+	}
 
-  return (
-    <PageLayout>
-      {data ? (
-        <>
-          <h2 className="text-center uppercase text-2xl">
-            Day 1 Convective Outlook
-          </h2>
+	console.log("Day 1 Valid: ", day1valid);
+	console.log("Day 1 Expire: ", day1expire);
+	console.log("Day 2 Valid: ", day2valid);
+	console.log("Day 2 Expire: ", day2expire);
+	console.log("Day 3 Valid: ", day3valid);
+	console.log("Day 4 Expire: ", day3expire);
 
-          <p className="text-center text-sm">
-            {day1outlook.length > 1 ? (
-              <>
-                <DayJSDateTime
-                  utcDate={day1outlook[0]?.properties.valid}
-                  format="ddd MMM D LT"
-                />
-                &nbsp;&#45;&nbsp;
-                <DayJSDateTime
-                  utcDate={day1outlook[0]?.properties.expire}
-                  format="ddd MMM D LT"
-                />
-              </>
-            ) : (
-              ""
-            )}
-          </p>
+	return (
+		<PageLayout>
+			<div className='md:flex md:justify-evenly'>
+				<section id='day-1-categorical-outlook-map'>
+					<OutlookTitle>Day 1</OutlookTitle>
+					<OutlookValidDates valid={day1valid} expire={day1expire} />
+					{day1outlook ? (
+						<CategoricalOutlookMap features={day1outlook} />
+					) : (
+						<OutlookLoadingMap />
+					)}
+				</section>
 
-          <CategoricalOutlookMap
-            key="day-1-categorical-outlook-map"
-            features={day1outlook}
-          />
+				<section id='day-2-categorical-outlook-map'>
+					<OutlookTitle>Day 2</OutlookTitle>
+					<OutlookValidDates valid={day2valid} expire={day2expire} />
+					{day2outlook ? (
+						<CategoricalOutlookMap
+							key='day-2-categorical-outlook-map'
+							features={day2outlook}
+						/>
+					) : (
+						<OutlookLoadingMap />
+					)}
+				</section>
 
-          <h2 className="text-center uppercase text-2xl">
-            Day 2 Convective Outlook
-          </h2>
-
-          <p className="text-center text-sm">
-            {day2outlook.length > 1 ? (
-              <>
-                <DayJSDateTime
-                  utcDate={day2outlook[0]?.properties.valid}
-                  format="ddd MMM D LT"
-                />
-                &nbsp;&#45;&nbsp;
-                <DayJSDateTime
-                  utcDate={day2outlook[0]?.properties.expire}
-                  format="ddd MMM D LT"
-                />
-              </>
-            ) : (
-              ""
-            )}
-          </p>
-
-          <CategoricalOutlookMap
-            key="day-2-categorical-outlook-map"
-            features={day2outlook}
-          />
-
-          <h2 className="text-center uppercase text-2xl">
-            Day 3 Convective Outlook
-          </h2>
-
-          <p className="text-center text-sm">
-            {day3outlook.length > 1 ? (
-              <>
-                <DayJSDateTime
-                  utcDate={day3outlook[0]?.properties.valid}
-                  format="ddd MMM D LT"
-                />
-                &nbsp;&#45;&nbsp;
-                <DayJSDateTime
-                  utcDate={day3outlook[0]?.properties.expire}
-                  format="ddd MMM D LT"
-                />
-              </>
-            ) : (
-              ""
-            )}
-          </p>
-
-          <CategoricalOutlookMap
-            key="day-3-categorical-outlook-map"
-            features={day3outlook}
-          />
-        </>
-      ) : null}
-    </PageLayout>
-  );
+				<section id='day-3-categorical-outlook-map'>
+					<OutlookTitle>Day 3</OutlookTitle>
+					<OutlookValidDates valid={day3valid} expire={day3expire} />
+					{day3outlook ? (
+						<CategoricalOutlookMap
+							key='day-3-categorical-outlook-map'
+							features={day3outlook}
+						/>
+					) : (
+						<OutlookLoadingMap />
+					)}
+				</section>
+			</div>
+		</PageLayout>
+	);
 };
 
 export default ConvectiveOutlookScreen;
+
+const OutlookTitle = ({ children }) => {
+	return <h2 className='text-center'>{children}</h2>;
+};
+
+const OutlookValidDates = ({ valid, expire }) => {
+	return (
+		<p className='text-center text-xs'>
+			<DayJSDateTime utcDate={valid ? valid : "N/A"} format='ddd LT' />
+			&nbsp;&#45;&nbsp;
+			<DayJSDateTime utcDate={expire ? expire : "N/A"} format='ddd LT' />
+		</p>
+	);
+};
+
+const OutlookLoadingMap = () => {
+	return (
+		<USMap>
+			<text x={350} y={250} fill='red' className='text-3xl font-bold'>
+				Loading Outlook...
+			</text>
+		</USMap>
+	);
+};
