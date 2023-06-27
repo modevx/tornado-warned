ab08cfd21c9df32faa4fe6c4200130cc2384fa3a
Eph <ephraim@modevx.com>
2023-06-27 15:17:55 -0500

    add responsive styling to label color squares

--- src/features/convective-outlooks/CategoricalLegendKey.jsx
+++ src/features/convective-outlooks/CategoricalLegendKey.jsx
@@ -1,8 +1,8 @@
 export const CategoricalLegendKey = ({ colorStr, labelStr }) => {
-	const badgeStyles = `bg-[${colorStr}] h-2 w-2 rounded-lg`;
+	const badgeStyles = `bg-[${colorStr}] h-2 w-2 rounded-sm sm:h-4 sm:w-4 sm:rounded-md`;
 
 	return (
-		<div className='flex items-center mx-3 my-1'>
+		<div className='flex items-center'>
 			<div className={badgeStyles} />
 			<span className='ml-2'>{labelStr}</span>
 		</div>
