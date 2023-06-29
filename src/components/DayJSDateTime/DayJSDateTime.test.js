aa6fd166c56e789c92b05309e0fa1b197aad6971
Eph <ephraim@modevx.com>
2023-06-29 11:50:57 -0500

    create DayJSDateTime test suite

--- src/components/DayJSDateTime/DayJSDateTime.test.js
+++ src/components/DayJSDateTime/DayJSDateTime.test.js
@@ -1,22 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 import { DayJSDateTime } from "./DayJSDateTime";
 
-/**
- * SCENARIOS
- * 1. displays component
- * 2. IF utcDate = null, displays "Unknown"
- * 3. IF format = null, auto-formats using "LT" (h:mm A)
- * 4. IF both args are passed, displays properly formatted date
- */
+describe("DayJSDateTime Component", () => {
+	test("renders component to screen", () => {
+		const renderedComponent = render(
+			<DayJSDateTime utcDate='2023-06-26T21:45:00-06:00' format='ddd LT' />
+		);
 
-test("renders component to screen", () => {
-	const component = render(
-		<DayJSDateTime utcDate='2023-06-26T21:45:00-06:00' format='' />
-	);
+		const spanElement = renderedComponent.getByTestId("DayJSDateTime");
+		expect(spanElement).toBeVisible();
+	});
+
+	test("renders properly formatted date", () => {
+		const renderedComponent = render(
+			<DayJSDateTime utcDate='2023-06-26T21:45:00-06:00' format='ddd LT' />
+		);
+
+		// const formattedDate = screen.getByText(/Mon 10:45 PM/i);
+		const formattedDate = renderedComponent.getByText(/Mon 10:45 PM/i);
+		expect(formattedDate).toBeInTheDocument();
+	});
+
+	test("renders 'Unknown' if utcDate is null", () => {
+		const renderedComponent = render(
+			<DayJSDateTime utcDate={null} format='ddd LT' />
+		);
+
+		const unknownText = renderedComponent.getByText(/unknown/i);
+		expect(unknownText).toBeInTheDocument();
+	});
+
+	test("renders in default LT format if format is undefined", () => {
+		const renderedComponent = render(
+			<DayJSDateTime utcDate='2023-06-26T21:45:00-06:00' />
+		);
+
+		const defaultFormattedDate = renderedComponent.getByText("10:45 PM");
+		expect(defaultFormattedDate).toBeInTheDocument();
+	});
 });
-
-test("renders properly formatted date", () => {});
-
-test("renders 'Unknown' if utcDate is null", () => {});
-
-test("renders in default LT format if format is null", () => {});
