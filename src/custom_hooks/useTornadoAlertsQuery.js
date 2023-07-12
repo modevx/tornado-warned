cffc8e857802ce9d40629d8fcd2730c73b5f594a
Eph <ephraim@modevx.com>
2023-07-12 15:28:05 -0500

    delete unused custom hooks

--- src/custom_hooks/useTornadoAlertsQuery.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "react-query";
-import { getTornadoAlerts } from "services/nws-api-web-service";
-
-export const useTornadoAlertsQuery = (alertType) => {
-	return useQuery(["tornado-alerts", alertType], () =>
-		getTornadoAlerts(alertType)
-	);
-};
