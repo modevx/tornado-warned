e7c62132bd7a0d1621738790d3e915c9842d5662
Eph <ephraim@modevx.com>
2023-07-12 16:59:11 -0500

    start max hail size icons

--- src/features/active-alerts/AlertCardMaxHailSize/AlertCardMaxHailSize.jsx
+++ src/features/active-alerts/AlertCardMaxHailSize/AlertCardMaxHailSize.jsx
@@ -1,11 +1,24 @@
+import { GiGolfTee, GiMarbles } from "react-icons/gi";
+import { BiCreditCard } from "react-icons/bi";
+import { IoBaseballOutline } from "react-icons/io5";
+import { TbToiletPaper } from "react-icons/tb";
+
 export const AlertCardMaxHailSize = ({ maxHailSize }) => {
-	const maxHail = maxHailSize?.length
-		? maxHailSize[0].replace("Up to ", "")
+	const maxSizeFloat = maxHailSize?.length
+		? parseFloat(maxHailSize[0].split(" ")[2])
 		: "N/A";
+
+	const sizeIcons = [
+		[0.5, GiMarbles],
+		[1.75, GiGolfTee],
+		[2.75, IoBaseballOutline],
+		[3, BiCreditCard],
+		[4, TbToiletPaper],
+	];
 
 	return (
 		<span className='text-sm bg-black rounded-md p-2'>
-			Max Hail Size: {maxHail}
+			Max Hail Size: {maxSizeFloat}
 		</span>
 	);
 };
