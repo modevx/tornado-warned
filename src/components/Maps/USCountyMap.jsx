9554972c8f294c1310f68ba7cf1a96654a9b0602
Eph <ephraim@modevx.com>
2023-08-07 17:11:26 -0500

    memoize Alert sections to mitigate lag from polygon alerts

--- src/components/Maps/USCountyMap.jsx
+++ src/components/Maps/USCountyMap.jsx
@@ -1,37 +1,56 @@
+import React from "react";
 import * as topojson from "topojson-client";
+import rewind from "@turf/rewind";
 
 import AlbersTopoJSONMap from "./_constants/albers-topojson-map.json";
 
-const { features: countyFeatures } = topojson.feature(
-  AlbersTopoJSONMap,
-  "counties"
-);
+// TODO: topojson.mesh() polygons dont render right
 
-const meshedCountyFeatures = topojson.mesh(
-  AlbersTopoJSONMap,
-  AlbersTopoJSONMap.objects.counties
-  // (a, b) => a !== b
-);
-
-// console.log(">> MESHED COUNTY FEATURES >>\n", meshedCountyFeatures);
+const counties = topojson.feature(AlbersTopoJSONMap, "counties");
+console.log("COUNTIES >>\n", counties);
 
 export const USCountyMap = ({ children, pathGen }) => {
-  return (
-    <svg viewBox="0 0 975 610" xmlns="http://www.w3.org/2000/svg">
-      <CountyFeatures pathGen={pathGen} features={meshedCountyFeatures} />
-
-      {children}
-    </svg>
-  );
+	return (
+		<svg viewBox='0 0 975 610' xmlns='http://www.w3.org/2000/svg'>
+			<CountyFeatures features={counties} pathGen={pathGen} />
+			{children}
+			{/* <CountyLabels collection={counties} pathGen={pathGen} /> */}
+		</svg>
+	);
 };
 
-const CountyFeatures = ({ pathGen, features }) => {
-  return (
-    <path
-      d={pathGen({ type: "FeatureCollection", features: features })}
-      // d={pathGen(meshedCountyFeatures)}
-      stroke="white"
-      fill="grey"
-    />
-  );
+const CountyFeatures = ({ features, pathGen }) => {
+	return (
+		<path d={pathGen(counties)} stroke='white' fill='grey' strokeWidth={0.5} />
+	);
+};
+
+const CountyLabels = ({ collection, pathGen }) => {
+	const { features } = collection;
+
+	return (
+		<g>
+			{features.map((feature) => {
+				const centroid = pathGen.centroid(feature);
+				const {
+					id,
+					properties: { name },
+				} = feature;
+
+				return (
+					<g key={`${id}`}>
+						<text
+							x={centroid[0]}
+							y={centroid[1]}
+							fontSize='35'
+							fill='white'
+							textAnchor='middle'
+						>
+							{name}
+						</text>
+					</g>
+				);
+			})}
+		</g>
+	);
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,66 +4,62 @@ import { PageLayout } from "components";
 import { AlertFilters } from "features/active-alerts/AlertFilters";
 import { AlertSection } from "features/active-alerts/AlertSection";
 import {
-  TornadoWarningAlert,
-  TornadoWatchAlert,
-  SevereStormWarningAlert,
-  SevereStormWatchAlert,
+	TornadoWarningAlert,
+	TornadoWatchAlert,
+	SevereStormWarningAlert,
+	SevereStormWatchAlert,
 } from "features/active-alerts/AlertCards";
 
 const HomeScreen = () => {
-  const [alertFilters, setAlertFilters] = useState({
-    showTornadoWarnings: true,
-    showTornadoWatches: true,
-    showStormWarnings: true,
-    showStormWatches: true,
-  });
+	const [alertFilters, setAlertFilters] = useState({
+		showTornadoWarnings: true,
+		showTornadoWatches: true,
+		showStormWarnings: true,
+		showStormWatches: true,
+	});
 
-  const handleToggleChange = (e) => {
-    const { name: toggleName } = e.target;
+	const handleToggleChange = (e) => {
+		const { name: toggleName } = e.target;
 
-    setAlertFilters((prev) =>
-      Object.assign({ ...prev }, { [toggleName]: !prev[toggleName] })
-    );
-  };
+		setAlertFilters((prev) =>
+			Object.assign({ ...prev }, { [toggleName]: !prev[toggleName] })
+		);
+	};
 
-  return (
-    <PageLayout>
-      <>
-        <AlertFilters
-          handler={handleToggleChange}
-          filterState={alertFilters ?? {}}
-        />
+	return (
+		<PageLayout>
+			<>
+				<AlertFilters
+					handler={handleToggleChange}
+					filterState={alertFilters ?? {}}
+				/>
 
-        {alertFilters.showTornadoWarnings && (
-          <AlertSection
-            alertComponent={TornadoWarningAlert}
-            event="Tornado Warning"
-          />
-        )}
+				<AlertSection
+					alertComponent={TornadoWarningAlert}
+					event='Tornado Warning'
+					isDisplayed={alertFilters.showTornadoWarnings}
+				/>
 
-        {alertFilters.showTornadoWatches && (
-          <AlertSection
-            alertComponent={TornadoWatchAlert}
-            event="Tornado Watch"
-          />
-        )}
+				<AlertSection
+					alertComponent={TornadoWatchAlert}
+					event='Tornado Watch'
+					isDisplayed={alertFilters.showTornadoWatches}
+				/>
 
-        {alertFilters.showStormWarnings && (
-          <AlertSection
-            alertComponent={SevereStormWarningAlert}
-            event="Severe Thunderstorm Warning"
-          />
-        )}
+				<AlertSection
+					alertComponent={SevereStormWarningAlert}
+					event='Severe Thunderstorm Warning'
+					isDisplayed={alertFilters.showStormWarnings}
+				/>
 
-        {alertFilters.showStormWatches && (
-          <AlertSection
-            alertComponent={SevereStormWatchAlert}
-            event="Severe Thunderstorm Watch"
-          />
-        )}
-      </>
-    </PageLayout>
-  );
+				<AlertSection
+					alertComponent={SevereStormWatchAlert}
+					event='Severe Thunderstorm Watch'
+					isDisplayed={alertFilters.showStormWatches}
+				/>
+			</>
+		</PageLayout>
+	);
 };
 
 export default HomeScreen;
