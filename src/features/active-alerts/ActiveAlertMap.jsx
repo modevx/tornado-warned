9c5160c92f0efb8adcb0cba2bc7d37d45985f540
Eph <ephraim@modevx.com>
2023-08-16 14:26:54 -0500

    relo all active alert map code to ActiveAlertMap component file

--- src/features/active-alerts/ActiveAlertMap.jsx
+++ src/features/active-alerts/ActiveAlertMap.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Rewind from "@turf/rewind";
+import { Button, Modal } from "react-daisyui";
 import { geoAlbers, geoPath } from "d3";
 
-import { USStateMap } from "components";
+import { Basemap, BasemapFeatureSelector } from "components";
 import {
 	useWarningAlertsByEvent,
 	useFakeWarningAlerts,
@@ -11,19 +13,72 @@ const projection = geoAlbers();
 const d3GeoPath = geoPath(projection);
 
 export const ActiveAlertMap = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [alertInfo, setAlertInfo] = useState(null);
+
+	const [features, setFeatures] = useState({
+		states: true,
+		counties: false,
+		cwas: false,
+		pfzs: false,
+	});
+
+	const handleShowAlertModal = (alert) => {
+		setAlertInfo(alert);
+		setIsOpen((isOpen) => !isOpen);
+	};
+
+	const handleCloseModal = () => {
+		setIsOpen(false);
+	};
+
+	const handleFeatureSelectorOnChange = (e) => {
+		const { name } = e.target;
+		setFeatures((prev) => Object.assign({ ...prev }, { [name]: !prev[name] }));
+	};
+
 	return (
-		<USStateMap>
-			{/* Feature Filter */}
-			<WarningFeatures event='Tornado Warning' color='stroke-red-400' />
+		<>
+			<Basemap
+				showStates={features.states}
+				showCounties={features.counties}
+				showCWAs={features.cwas}
+				showPFZs={features.pfzs}
+			>
+				{/* Feature Filter */}
+				{/* <WarningFeatures event='Tornado Warning' color='stroke-red-400' />
 			<WarningFeatures
 				event='Severe Thunderstorm Warning'
 				color='stroke-orange-400'
+			/> */}
+
+				<WarningFeatures
+					event='Tornado Warning'
+					color='stroke-red-400'
+					onClickHandler={handleShowAlertModal}
+				/>
+				<WarningFeatures
+					event='Severe Thunderstorm Warning'
+					color='stroke-orange-400'
+					onClickHandler={handleShowAlertModal}
+				/>
+			</Basemap>
+
+			<BasemapFeatureSelector
+				showValues={features}
+				onChangeHandler={handleFeatureSelectorOnChange}
 			/>
-		</USStateMap>
+
+			<AlertModal
+				isOpen={isOpen}
+				alertInfo={alertInfo}
+				closeModalHandler={handleCloseModal}
+			/>
+		</>
 	);
 };
 
-const WarningFeatures = ({ event, color }) => {
+const WarningFeatures = ({ event, color, onClickHandler }) => {
 	const { data } = useWarningAlertsByEvent(event);
 	const fakeAlerts = useFakeWarningAlerts(event);
 
@@ -34,14 +89,19 @@ const WarningFeatures = ({ event, color }) => {
 			{/* {data.length > 0 ? (
         <g>
           {data.map(({ feature }) => (
-            <AlertPolygon key={feature.id} color={color} />
+            <AlertPolygon key={feature.id} feature={feature.geometry} color={color} />
           ))}
         </g>
       ) : null} */}
 			{fakeAlerts.length > 0 ? (
 				<g>
-					{fakeAlerts.map(({ id, geometry }) => (
-						<AlertPolygon key={id} feature={geometry} color={color} />
+					{fakeAlerts.map((alert) => (
+						<AlertPolygon
+							key={alert.id}
+							feature={alert}
+							color={color}
+							onClickHandler={onClickHandler}
+						/>
 					))}
 				</g>
 			) : null}
@@ -49,15 +109,46 @@ const WarningFeatures = ({ event, color }) => {
 	);
 };
 
-const AlertPolygon = ({ feature, color }) => {
+const AlertPolygon = ({ feature, color, onClickHandler }) => {
 	return (
 		<path
-			d={d3GeoPath(Rewind(feature, { reverse: true }))}
-			// fill={color}
-			// stroke={color}
+			d={albersPathGenerator(rewind(feature.geometry, { reverse: true }))}
 			strokeWidth={10}
 			opacity={0.7}
 			className={color}
+			onClick={() => onClickHandler(feature)}
 		/>
+	);
+};
+
+const AlertModal = ({ isOpen, closeModalHandler, alertInfo }) => {
+	const ALERT_TYPE = {
+		"Tornado Warning": TornadoWarningAlert,
+		"Severe Thunderstorm Warning": SevereStormWarningAlert,
+	};
+
+	const AlertModal = ALERT_TYPE[alertInfo?.properties?.event];
+
+	return (
+		<>
+			{alertInfo !== null ? (
+				<Modal open={isOpen} className='overflow-auto'>
+					<Button
+						size='sm'
+						color='ghost'
+						shape='circle'
+						className='absolute right-2 top-2'
+						onClick={closeModalHandler}
+					>
+						x
+					</Button>
+					<AlertModal alert={alertInfo} />
+					{/* <Modal.Header className='font-bold'>Hello!</Modal.Header>
+			<Modal.Body className='uppercase font-bold text-red-500'>
+				{properties.event}
+			</Modal.Body> */}
+				</Modal>
+			) : null}
+		</>
 	);
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,7 @@
 import { useState } from "react";
-import { Button, Checkbox, Form, Modal } from "react-daisyui";
 
-import { Basemap, BasemapFeatureSelector, PageLayout } from "components";
-import { AlertFilters } from "features/active-alerts/AlertFilters";
-import { AlertSection } from "features/active-alerts/AlertSection";
-import {
-	TornadoWarningAlert,
-	TornadoWatchAlert,
-	SevereStormWarningAlert,
-	SevereStormWatchAlert,
-} from "features/active-alerts/AlertCards";
+import { PageLayout } from "components";
 import { ActiveAlertMap } from "features/active-alerts";
-
-import {
-	useWarningAlertsByEvent,
-	useFakeWatchAlerts,
-	useFakeWarningAlerts,
-} from "services/nws-api-web-service";
-import rewind from "@turf/rewind";
-import { albersPathGenerator } from "components/_constants/path-generators";
 
 const HomeScreen = () => {
 	const [alertFilters, setAlertFilters] = useState({
@@ -36,64 +19,11 @@ const HomeScreen = () => {
 		);
 	};
 
-	const [isOpen, setIsOpen] = useState(false);
-	const [alertInfo, setAlertInfo] = useState(null);
-
-	const handleShowAlertModal = (alert) => {
-		setAlertInfo(alert);
-		setIsOpen((isOpen) => !isOpen);
-	};
-
-	const handleCloseModal = () => {
-		setIsOpen(false);
-	};
-
-	const [features, setFeatures] = useState({
-		states: true,
-		counties: false,
-		cwas: false,
-		pfzs: false,
-	});
-
-	const handleFeatureSelectorOnChange = (e) => {
-		const { name } = e.target;
-		setFeatures((prev) => Object.assign({ ...prev }, { [name]: !prev[name] }));
-	};
-
-	// useFakeWatchAlerts("Severe Thunderstorm Watch");
-
 	return (
 		<PageLayout>
 			<>
 				<div className='flex items-center p-10'>
-					<Basemap
-						showStates={features.states}
-						showCounties={features.counties}
-						showCWAs={features.cwas}
-						showPFZs={features.pfzs}
-					>
-						<WarningFeatures
-							event='Tornado Warning'
-							color='stroke-red-400'
-							onClickHandler={handleShowAlertModal}
-						/>
-						<WarningFeatures
-							event='Severe Thunderstorm Warning'
-							color='stroke-orange-400'
-							onClickHandler={handleShowAlertModal}
-						/>
-					</Basemap>
-
-					<BasemapFeatureSelector
-						showValues={features}
-						onChangeHandler={handleFeatureSelectorOnChange}
-					/>
-
-					<ClickedPolygonModal
-						isOpen={isOpen}
-						alertInfo={alertInfo}
-						closeModalHandler={handleCloseModal}
-					/>
+					<ActiveAlertMap />
 				</div>
 			</>
 		</PageLayout>
@@ -101,78 +31,3 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
-
-const WarningFeatures = ({ event, color, onClickHandler }) => {
-	const { data } = useWarningAlertsByEvent(event);
-	const fakeAlerts = useFakeWarningAlerts(event);
-
-	if (fakeAlerts) console.log(`${event} >>\n`, fakeAlerts);
-
-	return (
-		<>
-			{/* {data.length > 0 ? (
-        <g>
-          {data.map(({ feature }) => (
-            <AlertPolygon key={feature.id} feature={feature.geometry} color={color} />
-          ))}
-        </g>
-      ) : null} */}
-			{fakeAlerts.length > 0 ? (
-				<g>
-					{fakeAlerts.map((alert) => (
-						<AlertPolygon
-							key={alert.id}
-							feature={alert}
-							color={color}
-							onClickHandler={onClickHandler}
-						/>
-					))}
-				</g>
-			) : null}
-		</>
-	);
-};
-
-const AlertPolygon = ({ feature, color, onClickHandler }) => {
-	return (
-		<path
-			d={albersPathGenerator(rewind(feature.geometry, { reverse: true }))}
-			strokeWidth={10}
-			opacity={0.7}
-			className={color}
-			onClick={() => onClickHandler(feature)}
-		/>
-	);
-};
-
-const ClickedPolygonModal = ({ isOpen, closeModalHandler, alertInfo }) => {
-	const ALERT_TYPE = {
-		"Tornado Warning": TornadoWarningAlert,
-		"Severe Thunderstorm Warning": SevereStormWarningAlert,
-	};
-
-	const AlertModal = ALERT_TYPE[alertInfo?.properties?.event];
-
-	return (
-		<>
-			{alertInfo !== null ? (
-				<Modal open={isOpen} className='overflow-auto'>
-					<Button
-						size='sm'
-						color='ghost'
-						shape='circle'
-						className='absolute right-2 top-2'
-						onClick={closeModalHandler}
-					>
-						x
-					</Button>
-					<AlertModal alert={alertInfo} />
-					{/* <Modal.Header className='font-bold'>Hello!</Modal.Header>
-			<Modal.Body className='uppercase font-bold text-red-500'>
-				{properties.event}
-			</Modal.Body> */}
-				</Modal>
-			) : null}
-		</>
-	);
-};
