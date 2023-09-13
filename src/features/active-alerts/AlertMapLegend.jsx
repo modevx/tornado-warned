4aca1a9304c78f611b8b8001b5a2c0d358b350cb
Eph <ephraim@modevx.com>
2023-09-13 15:17:16 -0500

    UI/add footer links, responsive mobile nav, & Daisy themeing

---
+++ src/features/active-alerts/AlertMapLegend.jsx
@@ -0,0 +1,61 @@
+import { Card } from "react-daisyui";
+import { FaTornado } from "react-icons/fa";
+import { GoAlertFill } from "react-icons/go";
+import { TbPolygon } from "react-icons/tb";
+
+const events = [
+	{
+		title: "Tornado Emergency",
+		description:
+			"Confirmed, life-threatening tornado causing catastrophic damage.",
+		from_color: "from-fuchsia-400",
+	},
+	{
+		title: "Particularly Dangerous Situation",
+		description:
+			"Probable long-track tornadoes or wide-spread severe events such as intense derechos.",
+		from_color: "from-purple-700",
+	},
+	{
+		title: "Tornado Warning",
+		description:
+			"Radar-indicated or confirmed tornado on the ground.  Imminent danger to life and property.",
+		from_color: "from-red-700",
+	},
+	{
+		title: "Severe Thunderstorm Warning",
+		description:
+			"Confirmed severe weather in the form of damaging winds and/or hail. Like a tornado warning, there is imminent danger to life and property.",
+		from_color: "from-orange-500",
+	},
+	{
+		title: "Tornado Watch",
+		description:
+			"Tornadoes possible in and close to the watch area. Stay weather-aware if a warning is issued.",
+		from_color: "from-yellow-300",
+	},
+	{
+		title: "Severe Thunderstorm Watch",
+		description:
+			"Severe weather is possible in and close to the watch area. Be ready in case a severe thunderstorm warning is issued.",
+		from_color: "from-green-300",
+	},
+];
+
+export const AlertMapLegend = () => {
+	return (
+		<div className='px-6 grid gap-4 sm:grid-cols-2 md:grid-cols-3'>
+			{events.map(({ description, from_color, title }) => (
+				<Card
+					key={title}
+					className={`p-3 bg-gradient-to-br ${from_color} to-black`}
+				>
+					<Card.Body className='bg-black rounded-lg'>
+						<Card.Title className='uppercase text-sm'>{title}</Card.Title>
+						<p className='text-xs'>{description}</p>
+					</Card.Body>
+				</Card>
+			))}
+		</div>
+	);
+};
--- src/features/active-alerts/AlertSection.jsx
+++ src/features/active-alerts/AlertSection.jsx
@@ -1,54 +1,54 @@
 import { twMerge } from "tailwind-merge";
 import {
-  useAlertFeaturesByEvent,
-  useFakeAlerts,
+	useWarningAlertsByEvent,
+	useFakeWarningAlertsByEvent,
 } from "services/nws-api-web-service";
 
 export const AlertSection = ({
-  alertComponent,
-  event,
-  className,
-  ...props
+	alertComponent,
+	event,
+	className,
+	...props
 }) => {
-  const classes = twMerge("p-2", className);
-  const AlertComponent = alertComponent;
-  const { data: alerts } = useAlertFeaturesByEvent(event);
-  const fakeAlerts = useFakeAlerts(event);
+	const classes = twMerge("p-2", className);
+	const AlertComponent = alertComponent;
+	const { data: alerts } = useWarningAlertsByEvent(event);
+	const fakeAlerts = useFakeWarningAlertsByEvent(event);
 
-  return (
-    <section {...props} className={classes}>
-      <SectionTitle title={event} />
-      <GridLayout>
-        {alerts
-          ? alerts.map((alert) => (
-              <AlertComponent key={alert.id} alert={alert} />
-            ))
-          : null}
-        {/* TODO: create "no active alerts" component */}
-        {/* {fakeAlerts
+	return (
+		<section {...props} className={classes}>
+			<SectionTitle title={event} />
+			<GridLayout>
+				{alerts
+					? alerts.map((alert) => (
+							<AlertComponent key={alert.id} alert={alert} />
+					  ))
+					: null}
+				{/* TODO: create "no active alerts" component */}
+				{/* {fakeAlerts
           ? fakeAlerts.map((alert) => (
               <AlertComponent key={alert.id} alert={alert} />
             ))
           : null} */}
-      </GridLayout>
-    </section>
-  );
+			</GridLayout>
+		</section>
+	);
 };
 
 const SectionTitle = ({ title }) => {
-  return (
-    <>
-      <h2 className="font-display uppercase text-3xl mb-4 bg-clip-text text-transparent bg-gradient-to-br from-white to-neutral-600">
-        {title}
-      </h2>
-    </>
-  );
+	return (
+		<>
+			<h2 className='font-display uppercase text-3xl mb-4 bg-clip-text text-transparent bg-gradient-to-br from-white to-neutral-600'>
+				{title}
+			</h2>
+		</>
+	);
 };
 
 const GridLayout = ({ children }) => {
-  return (
-    <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {children}
-    </div>
-  );
+	return (
+		<div className='grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+			{children}
+		</div>
+	);
 };
