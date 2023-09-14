ac967681c5c06c6b1e754843f34929f669d8e3fb
Eph <ephraim@modevx.com>
2023-09-14 14:10:12 -0500

    refactor Outlooks into responsive grid with text product modal

--- src/features/convective-outlooks/CategoricalMap.jsx
+++ src/features/convective-outlooks/CategoricalMap.jsx
@@ -1,67 +1,43 @@
-import { Button } from "react-daisyui";
-
 import { USStateMap, USMapLoading } from "components";
 import { useCategoricalOutlookByLayerId } from "services/outlook-mapserver";
 import { rewindPathGenerator } from "components/_constants/path-generators";
+import { CATEGORICAL as styles } from "features/convective-outlooks/_constants/outlook-feature-styles";
 
-export const CategoricalMap = ({ features, styles }) => {
+export const CategoricalMap = ({ outlookDay }) => {
+	const DAY_TO_LAYERS = {
+		1: 1,
+		2: 9,
+		3: 17,
+	};
+	const { data: features } = useCategoricalOutlookByLayerId(
+		DAY_TO_LAYERS[outlookDay]
+	);
+
 	return (
 		<>
-			{features ? (
-				<div className='w-full h-full'>
-					<USStateMap>
-						{features.map((feature) => {
-							const {
-								id,
-								properties: { dn: categoryKey, idp_source },
-							} = feature;
+			<div className='w-full h-full'>
+				<USStateMap>
+					{features
+						? features?.map((feature) => {
+								const {
+									id,
+									properties: { dn: category, idp_source },
+								} = feature;
 
-							return (
-								<path
-									key={`${idp_source}-${id}`}
-									d={rewindPathGenerator(feature)}
-									fill={styles[categoryKey].bgColor}
-									opacity={0.7}
-									stroke={styles[categoryKey].stroke}
-									strokeWidth={4}
-								/>
-							);
-						})}
-					</USStateMap>
-					<Button variant='outline' color='accent' className='w-25' size='xs'>
-						Read Outlook
-					</Button>
-				</div>
-			) : (
-				<USMapLoading loadingMessage='Outlook Loading...' />
-			)}
-		</>
-	);
-};
-
-const CategoricalLegend = ({ styles }) => {
-	const stylesArr = Object.values(styles);
-
-	return (
-		<div className='text-xs flex justify-center mt-10 mb-4 '>
-			<div className='grid gap-4 grid-cols-2 sm:grid-cols-3 lg:flex lg:justify-center'>
-				{stylesArr.map(({ bgColor, textColor, label }) => {
-					return (
-						<div
-							key={label}
-							style={{ backgroundColor: `${bgColor}` }}
-							className='p-2 rounded'
-						>
-							<span
-								style={{ color: `${textColor}` }}
-								className='block text-black text-xs lg:text-base text-center font-bold uppercase'
-							>
-								{label}
-							</span>
-						</div>
-					);
-				})}
+								return (
+									<path
+										key={`${idp_source}-${id}`}
+										d={rewindPathGenerator(feature)}
+										fill={styles[category].bgColor}
+										opacity={0.7}
+										stroke={styles[category].stroke}
+										strokeWidth={4}
+									/>
+								);
+						  })
+						: null}
+				</USStateMap>
 			</div>
-		</div>
+		</>
 	);
 };
--- src/pages/convective-outlooks.jsx
+++ src/pages/convective-outlooks.jsx
@@ -1,40 +1,66 @@
-import React from "react";
+import { useState } from "react";
+import { Button, Modal } from "react-daisyui";
 
-import { DayJSDateTime, PageLayout } from "components";
-import { CategoricalMap, DayInfo } from "features/convective-outlooks";
-import { useCategoricalOutlookByLayerId } from "services/outlook-mapserver";
-
-import { CATEGORICAL as styles } from "features/convective-outlooks/_constants/outlook-feature-styles";
+import { PageLayout } from "components";
+import {
+	CategoricalMap,
+	DayInfo,
+	TextProductModal,
+} from "features/convective-outlooks";
 
 const ConvectiveOutlookScreen = () => {
-	const { data: day1 } = useCategoricalOutlookByLayerId(1);
-	const { data: day2 } = useCategoricalOutlookByLayerId(9);
-	const { data: day3 } = useCategoricalOutlookByLayerId(17);
+	const [isOpen, setIsOpen] = useState(false);
+	const [outlookDay, setOutlookDay] = useState(1);
+
+	const showModalHandler = (outlookDay) => {
+		setOutlookDay(outlookDay);
+		setIsOpen(true);
+	};
+
+	const closeModalHandler = () => {
+		setIsOpen(false);
+	};
 
 	return (
 		<PageLayout>
-			<div className='md:grid md:grid-cols-2 xl:grid-cols-3'>
-				{day1 && (
-					<div className='flex flex-col items-center md:flex-1'>
-						<DayInfo day={1} />
-						<CategoricalMap features={day1} styles={styles} />
-					</div>
-				)}
-				{day2 && (
-					<div className='flex flex-col items-center md:flex-1'>
-						<DayInfo day={2} />
-						<CategoricalMap features={day2} styles={styles} />
-					</div>
-				)}
-				{day3 && (
-					<div className='flex flex-col items-center md:flex-1'>
-						<DayInfo day={3} />
-						<CategoricalMap features={day3} styles={styles} />
-					</div>
-				)}
-			</div>
+			<OutlooksGrid>
+				<OutlookGridItem dayNumber={1} showOutlookText={showModalHandler} />
+
+				<OutlookGridItem dayNumber={2} showOutlookText={showModalHandler} />
+
+				<OutlookGridItem dayNumber={3} showOutlookText={showModalHandler} />
+			</OutlooksGrid>
+			<TextProductModal
+				isOpen={isOpen}
+				outlookDay={outlookDay}
+				closeHandler={closeModalHandler}
+			/>
 		</PageLayout>
 	);
 };
 
 export default ConvectiveOutlookScreen;
+
+const OutlooksGrid = ({ children }) => (
+	<div className='md:grid md:grid-cols-2 xl:grid-cols-3'>{children}</div>
+);
+
+const OutlookGridItem = ({ dayNumber, showOutlookText }) => (
+	<div className='flex flex-col items-center md:flex-1'>
+		<DayInfo day={dayNumber} />
+		<CategoricalMap outlookDay={dayNumber} />
+		<OutlookTextModalBtn openHandler={showOutlookText} outlookDay={dayNumber} />
+	</div>
+);
+
+const OutlookTextModalBtn = ({ openHandler, outlookDay }) => (
+	<Button
+		variant='outline'
+		color='accent'
+		className='w-25'
+		size='xs'
+		onClick={() => openHandler(outlookDay)}
+	>
+		Read Details
+	</Button>
+);
