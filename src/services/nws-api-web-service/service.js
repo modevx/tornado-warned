e42b2120ab21ead36ec7508769c60292055e0292
Ephraim <ephraim@modevx.com>
2023-09-19 16:46:01 -0500

    add funs to get just PDSs & Tornado Emergency alerts

--- src/services/nws-api-web-service/service.js
+++ src/services/nws-api-web-service/service.js
@@ -1,75 +1,93 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { checkAlertIsPDS, checkAlertIsTornadoEmergency } from "utils";
 import { FAKE_ALERTS } from "./fake-alerts";
 import { createHTTPClient } from "services/create-http-client";
 
 const nwsApiClient = createHTTPClient({
-	baseURL: "https://api.weather.gov",
+  baseURL: "https://api.weather.gov",
 });
 
 const ERROR_TITLE = "/// ERROR: National Weather Service API Web Service ///";
 
 const getNwsAlertsByEvent = async (event) => {
-	const encodedEvent = encodeURIComponent(event);
-	const endpoint = `/alerts/active?status=actual&message_type=alert&event=${encodedEvent}`;
+  const encodedEvent = encodeURIComponent(event);
+  const endpoint = `/alerts/active?status=actual&message_type=alert&event=${encodedEvent}`;
 
-	try {
-		const response = await nwsApiClient.get(endpoint);
-		const { features } = response?.data;
+  try {
+    const response = await nwsApiClient.get(endpoint);
+    const { features } = response?.data;
 
-		return features;
-	} catch (error) {
-		console.log(`${ERROR_TITLE}\n`, error);
-		throw new Error(`${ERROR_TITLE}\n`, error);
-	}
+    return features;
+  } catch (error) {
+    console.log(`${ERROR_TITLE}\n`, error);
+    throw new Error(`${ERROR_TITLE}\n`, error);
+  }
 };
 
-const getPDSAlerts = async () => null;
+const getPDSAlerts = async () => {
+  const events = encodeURIComponent(
+    "Tornado Warning, Tornado Watch, Severe Thunderstorm Warning, Severe Thunderstorm Watch"
+  );
+
+  const endpoint = `/alerts/active?status=actual&message_type=alert&event=${events}`;
+
+  try {
+    const response = await nwsApiClient.get(endpoint);
+    const { features: alerts } = response?.data;
+    const pdsAlerts = alerts.length
+      ? alerts.filter((alert) => checkAlertIsPDS(alert))
+      : [];
+
+    return pdsAlerts;
+  } catch (error) {
+    console.log(`${ERROR_TITLE}\n`, error);
+    throw new Error(`${ERROR_TITLE}\n`, error);
+  }
+};
+
+const getTornadoEmergencyAlerts = async () => {
+  const endpoint = `/alerts/active?status=actual&message_type=alert&event=${encodeURIComponent(
+    "Tornado Warning"
+  )}`;
+
+  try {
+    const response = await nwsApiClient.get(endpoint);
+    const { features: alerts } = response?.data;
+    const tornadoEmergencyAlerts = alerts.length
+      ? alerts.filter((alert) => checkAlertIsPDS(alert))
+      : [];
+
+    return tornadoEmergencyAlerts;
+  } catch (error) {
+    console.log(`${ERROR_TITLE}\n`, error);
+    throw new Error(`${ERROR_TITLE}\n`, error);
+  }
+};
+
+export const usePDSAlerts = () => {
+  return useQuery(["NWS", "Alerts", "Particularly Dangerous Situation"], () =>
+    getPDSAlerts()
+  );
+};
+
+export const useTornadoEmergencyAlerts = () => {
+  return useQuery(["NWS", "Alerts", "Tornado Emergency"], () =>
+    getTornadoEmergencyAlerts()
+  );
+};
 
 export const useNwsAlertsByEvent = (event) => {
-	return useQuery(["NWS", "Alerts", event], () => getNwsAlertsByEvent(event));
-};
-
-// ------------------------ SIMPLIFY SERVICE -------------------------
-
-const getWatchAlertsByEvent = async (watchEvent) => {
-	const uriEncodedEvent = encodeURIComponent(watchEvent);
-	const queryParams = `/alerts/active?status=actual&message_type=alert&event=${uriEncodedEvent}`;
-
-	let watchFeatures = [];
-
-	try {
-		const response = await nwsApiClient.get(queryParams);
-		const { features } = response?.data;
-
-		if (features.length > 0) {
-			watchFeatures = features.map((feature) => {
-				const affectedZones = parseAffectedFIPSCodes(feature);
-
-				return Object.assign({ ...feature }, { zoneIds: affectedZones });
-			});
-		}
-
-		return watchFeatures;
-	} catch (error) {
-		console.log(`${ERROR_TITLE}\n`, error);
-		throw new Error(`${ERROR_TITLE}\n`, error);
-	}
-};
-
-export const useWatchAlertsByEvent = (watchEvent) => {
-	return useQuery(["NWS", "Alerts", "Watches", watchEvent], () =>
-		getWatchAlertsByEvent(watchEvent)
-	);
+  return useQuery(["NWS", "Alerts", event], () => getNwsAlertsByEvent(event));
 };
 
 export const useFakeWatchAlertsByEvent = (watchEvent) => {
-	const features = FAKE_ALERTS[watchEvent];
-	let affectedZones = [];
+  const features = FAKE_ALERTS[watchEvent];
+  let affectedZones = [];
 
-	features.forEach((feature) => {
-		affectedZones = [...affectedZones, ...feature.properties.geocode.SAME];
-	});
+  features.forEach((feature) => {
+    affectedZones = [...affectedZones, ...feature.properties.geocode.SAME];
+  });
 
-	return { features, affectedZones };
+  return { features, affectedZones };
 };
