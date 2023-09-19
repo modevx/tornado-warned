1394be52bf84fbedb694ac93277820d62dd9f703
Ephraim <ephraim@modevx.com>
2023-09-19 16:46:19 -0500

    import PDS & Tornado Emergency service funcs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
-
+import {
+  usePDSAlerts,
+  useTornadoEmergencyAlerts,
+} from "services/nws-api-web-service";
 import { H1, PageLayout } from "components";
 import { ActiveAlertMap } from "features/active-alerts";
+import { FAKE_ALERTS } from "services/nws-api-web-service";
 
 const HomeScreen = () => {
-	return (
-		<PageLayout>
-			{/* Extreme Alert CTAs */}
-			<H1>Active Alerts</H1>
-			<ActiveAlertMap />
-			{/* Alert Description Modal Btns */}
-		</PageLayout>
-	);
+  const { data: pdsAlerts } = usePDSAlerts();
+  const { data: tornadoEmergencyAlerts } = useTornadoEmergencyAlerts();
+
+  return (
+    <PageLayout>
+      {/* Extreme Alert CTAs */}
+      <H1>Active Alerts</H1>
+      <ActiveAlertMap />
+      {/* Alert Description Modal Btns */}
+    </PageLayout>
+  );
 };
 
 export default HomeScreen;
+
+const PdsCtas = () => null;
+
+const TornadoEmergencyCtas = () => null;
