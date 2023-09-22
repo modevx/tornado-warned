9e50e7d6ea0ea4c5a509e2180093f90918f256c0
Ephraim <ephraim@modevx.com>
2023-09-22 10:36:37 -0500

    use real time alert polygons

--- src/features/active-alerts/ActiveAlertMap.jsx
+++ src/features/active-alerts/ActiveAlertMap.jsx
@@ -3,65 +3,65 @@ import { useState } from "react";
 import { USStateMap } from "components";
 import { AlertModal } from "./AlertModal";
 import {
-	useNwsAlertsByEvent,
-	EVENTS,
-	FAKE_ALERTS,
+  useNwsAlertsByEvent,
+  EVENTS,
+  FAKE_ALERTS,
 } from "services/nws-api-web-service";
 import { WarningPolygons, WatchPolygons } from "features/active-alerts";
 
 export const ActiveAlertMap = () => {
-	const { data: tor_warn } = useNwsAlertsByEvent(EVENTS.tor_warn);
-	const { data: tor_watch } = useNwsAlertsByEvent(EVENTS.tor_watch);
-	const { data: st_warn } = useNwsAlertsByEvent(EVENTS.st_warn);
-	const { data: st_watch } = useNwsAlertsByEvent(EVENTS.st_watch);
+  const { data: tor_warn } = useNwsAlertsByEvent(EVENTS.tor_warn);
+  const { data: tor_watch } = useNwsAlertsByEvent(EVENTS.tor_watch);
+  const { data: st_warn } = useNwsAlertsByEvent(EVENTS.st_warn);
+  const { data: st_watch } = useNwsAlertsByEvent(EVENTS.st_watch);
 
-	const fake_tor_warn = FAKE_ALERTS.tor_warn;
-	const fake_tor_watch = FAKE_ALERTS.tor_watch;
-	const fake_st_warn = FAKE_ALERTS.st_warn;
-	const fake_st_watch = FAKE_ALERTS.st_watch;
+  const fake_tor_warn = FAKE_ALERTS.tor_warn;
+  const fake_tor_watch = FAKE_ALERTS.tor_watch;
+  const fake_st_warn = FAKE_ALERTS.st_warn;
+  const fake_st_watch = FAKE_ALERTS.st_watch;
 
-	const [isOpen, setIsOpen] = useState(false);
-	const [alertInfo, setAlertInfo] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [alertInfo, setAlertInfo] = useState(null);
 
-	const handleShowAlertModal = (selectedAlert) => {
-		setAlertInfo(selectedAlert);
-		setIsOpen((isOpen) => !isOpen);
-	};
+  const handleShowAlertModal = (selectedAlert) => {
+    setAlertInfo(selectedAlert);
+    setIsOpen((isOpen) => !isOpen);
+  };
 
-	const handleCloseModal = () => {
-		setIsOpen(false);
-	};
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
 
-	return (
-		<>
-			<USStateMap>
-				<WarningPolygons
-					// alerts={tor_warn}
-					alerts={fake_tor_warn}
-					showAlertModal={handleShowAlertModal}
-				/>
-				{/* <WarningPolygons
-					// alerts={st_warn}
-					alerts={fake_st_warn}
-					showAlertModal={handleShowAlertModal}
-				/> */}
-				<WatchPolygons
-					// alerts={tor_watch}
-					alerts={fake_tor_watch}
-					showAlertModal={handleShowAlertModal}
-				/>
-				{/* <WatchPolygons
-          // alerts={st_watch}
-          alerts={fake_st_watch}
+  return (
+    <>
+      <USStateMap>
+        <WatchPolygons
+          alerts={tor_watch}
+          // alerts={fake_tor_watch}
           showAlertModal={handleShowAlertModal}
-        /> */}
-			</USStateMap>
+        />
+        <WatchPolygons
+          alerts={st_watch}
+          // alerts={fake_st_watch}
+          showAlertModal={handleShowAlertModal}
+        />
+        <WarningPolygons
+          alerts={tor_warn}
+          // alerts={fake_tor_warn}
+          showAlertModal={handleShowAlertModal}
+        />
+        <WarningPolygons
+          alerts={st_warn}
+          // alerts={fake_st_warn}
+          showAlertModal={handleShowAlertModal}
+        />
+      </USStateMap>
 
-			<AlertModal
-				isOpen={isOpen}
-				alert={alertInfo}
-				closeHandler={handleCloseModal}
-			/>
-		</>
-	);
+      <AlertModal
+        isOpen={isOpen}
+        alert={alertInfo}
+        closeHandler={handleCloseModal}
+      />
+    </>
+  );
 };
