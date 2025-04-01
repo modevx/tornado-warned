531037215c37ac85db2e37047e7bf217e1749565
Ephraim <ephraim@modevx.com>
2025-04-01 00:46:48 -0400

    delete unused files

--- src/custom_hooks/usePublicInfoStatements.js
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "react-query";
-import { getPublicInfoStatements } from "services/nws-alerts";
-
-export const usePublicInfoStatementQuery = () => {
-  return useQuery(["nws-pub-info-stmt"], () => getPublicInfoStatements());
-};
--- src/custom_hooks/useRssFeedQuery.js
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "react-query";
-import { getSpcRssFeed } from "services/storm_prediction_center";
-
-export const useRssFeedQuery = (feedType) => {
-	return useQuery(["spc-rss-feeds", feedType], () => getSpcRssFeed(feedType));
-};
