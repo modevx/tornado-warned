68eee4c073fbd31fc5a23f925caf48d48e4f1217
Ephraim <ephraim@modevx.com>
2026-04-26 13:19:08 -0400

    add layerData CIG_LAYER_ID  checker

--- src/utils/convective-outlooks.js
+++ src/utils/convective-outlooks.js
@@ -1,10 +1,19 @@
 export const convectiveFeatureKey = (feature) => {
-  return `${feature.properties.idp_source}-${feature.id}`;
+  const { idp_source, objectid } = feature.properties;
+
+  return `${idp_source}_${objectid}`;
 };
-export const hasConvectiveFeatures = (features) => {
+export const returnsValidCIGFeatures = (features) => {
+  return features?.length > 0 ?? false;
+};
+export const returnsValidConvectiveFeatures = (features) => {
   return features?.some((feature) => feature?.properties?.dn > 0) ?? false;
 };
+// TODO: depricate
 export const isIntensityFeature = (feature) => {
   // not all feature response payloads include 'label' field
   return feature.properties.label?.includes("CIG");
+};
+export const includesCIGs = (layerData) => {
+  return Object.keys(layerData).includes("CIG_LAYER_ID");
 };
